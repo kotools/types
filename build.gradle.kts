8adb986e337db7ba72ee1ca8d9d3b64c6c62a31b
Loïc Lamarque <loiclamarque777@gmail.com>
2021-12-08 20:53:00 +0100

    chore: update gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,29 @@
 import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
 
-plugins { kotlin(module = "jvm") version "1.5.32" }
+plugins {
+    kotlin(module = "jvm") version "1.5.32"
+    `maven-publish`
+}
 
 group = "org.kotools"
 version = "0.0.0"
 
 repositories(RepositoryHandler::mavenCentral)
 
-kotlin { explicitApi = ExplicitApiMode.Strict }
+publishing {
+    repositories {
+        maven {
+            name = "KotoolsCsvFile"
+            url = uri(path = "https://maven.pkg.github.com/kotools/csv-file")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        register<MavenPublication>(name = "gpr") { from(components["java"]) }
+    }
+}
+
+kotlin.explicitApi = ExplicitApiMode.Strict
