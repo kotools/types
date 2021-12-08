e7d07ea49ce293dc61c97d411d288d88dab89a88
Loïc Lamarque <loiclamarque777@gmail.com>
2021-12-08 20:35:38 +0100

    chore: update gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
+import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
+
 plugins { kotlin(module = "jvm") version "1.5.32" }
 
 group = "org.kotools"
 version = "0.0.0"
 
 repositories(RepositoryHandler::mavenCentral)
+
+kotlin { explicitApi = ExplicitApiMode.Strict }
