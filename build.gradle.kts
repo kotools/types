4c42dfdc494b5e019b57b1d266b6dfd1720d393e
Loïc Lamarque <loiclamarque777@gmail.com>
2021-12-13 20:00:55 +0100

    chore(#11): update gradle config for meeting ossrh requirements

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,13 @@ import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
 
 plugins {
     kotlin(module = "jvm") version Kotlin.version
+    id("org.jetbrains.dokka") version "1.5.31"
     `maven-publish`
+    signing
 }
 
 group = "io.github.kotools"
-version = "0.1.1"
+version = "0.1.2"
 
 repositories(RepositoryHandler::mavenCentral)
 
@@ -17,7 +19,50 @@ dependencies {
     testRuntimeOnly(Library.slf4j)
 }
 
+kotlin.explicitApi = ExplicitApiMode.Strict
+
+val dokkaOutputDir = "$buildDir/dokka"
+tasks.dokkaHtml { outputDirectory.set(file(dokkaOutputDir)) }
+val cleanDokkaOutput: TaskProvider<Delete> =
+    tasks.register(name = "cleanDokkaOutput") { delete(dokkaOutputDir) }
+val javadocJar: TaskProvider<Jar> = tasks.register(name = "javadocJar") {
+    dependsOn(cleanDokkaOutput, tasks.dokkaHtml)
+    archiveClassifier.set("javadoc")
+    from(dokkaOutputDir)
+}
+
 publishing {
+    publications {
+        register<MavenPublication>(name = "gpr") { from(components["java"]) }
+        withType<MavenPublication> {
+            artifact(javadocJar)
+            pom {
+                name.set("Kotools CSV")
+                description.set("Elegant CSV file's manager for Kotlin.")
+                url.set("https://github.com/kotools/csv")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                issueManagement {
+                    system.set("GitHub")
+                    url.set("https://github.com/kotools/csv/issues")
+                }
+                scm {
+                    connection.set("https://github.com/kotools/csv.git")
+                    url.set("https://github.com/kotools/csv")
+                }
+                developers {
+                    developer {
+                        name.set("Loïc Lamarque")
+                        email.set("loiclamarque777@gmail.com")
+                    }
+                }
+            }
+        }
+    }
     repositories {
         maven {
             name = "GitHub"
@@ -29,18 +74,23 @@ publishing {
         }
         maven {
             name = "OSSRH"
-            url = uri(
-                path = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-            )
+            val path: String =
+                if (version.toString().endsWith(suffix = "SNAPSHOT"))
+                    "https://oss.sonatype.org/content/repositories/snapshots/"
+                else "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+            url = uri(path)
             credentials {
                 username = System.getenv("MAVEN_USERNAME")
                 password = System.getenv("MAVEN_PASSWORD")
             }
         }
     }
-    publications {
-        register<MavenPublication>(name = "gpr") { from(components["java"]) }
-    }
 }
 
-kotlin.explicitApi = ExplicitApiMode.Strict
+signing {
+    useInMemoryPgpKeys(
+        System.getenv("GPG_PRIVATE_KEY"),
+        System.getenv("GPG_PASSWORD")
+    )
+    sign(publishing.publications)
+}
