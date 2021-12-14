15ebf830d4a83dea3238eb02ea929162471bc5e7
Loïc Lamarque <loiclamarque777@gmail.com>
2021-12-14 21:29:17 +0100

    docs(#7): document strict file reader

--- src/main/kotlin/io/github/kotools/csv/api/Reader.kt
+++ src/main/kotlin/io/github/kotools/csv/api/Reader.kt
@@ -28,9 +28,29 @@ public suspend fun csvReader(config: Reader.() -> Unit):
 public infix fun CoroutineScope.csvReaderAsync(config: Reader.() -> Unit):
         Deferred<List<Map<String, String>>?> = async(IO) { reader(config) }
 
+/**
+ * Creates a [reader][Reader] with the given [config] and reads the
+ * corresponding [file][Csv.file]'s content.
+ *
+ * Throws a [InvalidPropertyError] if the [file][Csv.file] value is not set or
+ * is invalid.
+ * Throws a [CsvFileNotFoundError] if the [file][Csv.file] doesn't exist on the
+ * system.
+ */
+@Throws(CsvFileNotFoundError::class, InvalidPropertyError::class)
 public suspend fun strictCsvReader(config: Reader.() -> Unit):
         List<Map<String, String>> = withContext(IO) { strictReader(config) }
 
+/**
+ * Creates a [reader][Reader] with the given [config] and reads the
+ * corresponding [file][Csv.file]'s content **asynchronously**.
+ *
+ * Throws a [InvalidPropertyError] if the [file][Csv.file] value is not set or
+ * is invalid.
+ * Throws a [CsvFileNotFoundError] if the [file][Csv.file] doesn't exist on the
+ * system.
+ */
+@Throws(CsvFileNotFoundError::class, InvalidPropertyError::class)
 public infix fun CoroutineScope.strictCsvReaderAsync(config: Reader.() -> Unit):
         Deferred<List<Map<String, String>>> = async(IO) { strictReader(config) }
 
