da730ff9f3f9afcc7dc7ecfe8d28f7eeab46faa1
Loïc Lamarque <loiclamarque777@gmail.com>
2021-12-28 07:52:59 +0100

    feat(#25): remove code of v1.0

--- src/main/kotlin/io/github/kotools/csv/old/Separator.kt
+++
@@ -1,9 +0,0 @@
-package io.github.kotools.csv.old
-
-public val comma: Separator get() = Separator.Comma
-public val semicolon: Separator get() = Separator.Semicolon
-
-public sealed class Separator(internal val value: Char) {
-    internal object Comma : Separator(',')
-    internal object Semicolon : Separator(';')
-}
