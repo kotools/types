6a39fe44a341fc5ddeced4699dc1679dfa44eaed
Loïc Lamarque <loiclamarque777@gmail.com>
2021-12-28 13:25:22 +0100

    refactor: update assertions

--- src/test/kotlin/io/github/kotools/csv/Assertions.kt
+++ src/test/kotlin/io/github/kotools/csv/Assertions.kt
@@ -1,6 +1,5 @@
 package io.github.kotools.csv
 
-import kotlin.test.assertEquals
 import kotlin.test.assertNotEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
@@ -8,19 +7,11 @@ import kotlin.test.assertNull
 internal inline fun <T : Any?> assertNull(block: () -> T?): Unit =
     assertNull(block())
 
-internal infix fun <T : Any> T.assertEquals(other: T): Unit =
-    assertEquals(other, this)
-
 internal infix fun <T : Any> T.assertNotEquals(other: T): Unit =
     assertNotEquals(other, this)
 
 internal fun <T : Any?> T.assertNotNull() {
     assertNotNull(actual = this)
-}
-
-internal infix fun <T : Any?> T.assertNotNullOrEquals(other: T) {
-    assertNotNull()
-    this?.let { assertNotEquals(other, it) }
 }
 
 internal fun <T : Any?> T.assertNull(): Unit = assertNull(actual = this)
