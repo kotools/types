1b89e0efd77436311ed44af7cfa3de225c58e640
Loïc Lamarque <loiclamarque777@gmail.com>
2021-12-28 23:59:49 +0100

    refactor: update writer

--- src/main/kotlin/io/github/kotools/csv/ManagerImpl.kt
+++
@@ -1,9 +0,0 @@
-package io.github.kotools.csv
-
-internal abstract class ManagerImpl : Configurable, Manager {
-    override var file: String by csvFile()
-    override var folder: String by folder()
-    override var separator: Separator = comma
-
-    override fun isValid(): Boolean = file.isNotBlank()
-}
--- src/main/kotlin/io/github/kotools/csv/Writer.kt
+++ src/main/kotlin/io/github/kotools/csv/Writer.kt
@@ -1,10 +1,24 @@
 package io.github.kotools.csv
 
+import com.github.doyaaaaaken.kotlincsv.client.CsvWriter
+import com.github.doyaaaaaken.kotlincsv.dsl.csvWriter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.IOException
+import java.nio.file.Path
+import kotlin.io.path.Path
+import kotlin.io.path.createDirectory
+import kotlin.io.path.notExists
+
+private val writer: WriterConfiguration get() = WriterConfiguration()
+private val Writer.csv: CsvWriter
+    get() = csvWriter { delimiter = separator.value }
+private val Writer.resourceFile: File?
+    get() = loader getResourceFile "$folder$file"
 
 /**
  * Writes records in a CSV file according to the given [configuration], or
@@ -12,7 +26,7 @@ import kotlinx.coroutines.withContext
  */
 @Throws(CsvException::class)
 public suspend fun csvWriter(configuration: Writer.() -> Unit): Unit =
-    withContext(IO) { delegateCsvWriter(configuration) }
+    withContext(IO) { processCsvWriter(configuration) }
 
 /**
  * Writes records in a CSV file **asynchronously** according to the given
@@ -22,14 +36,14 @@ public suspend fun csvWriter(configuration: Writer.() -> Unit): Unit =
 @Throws(CsvException::class)
 public infix fun CoroutineScope.csvWriterAsync(
     configuration: Writer.() -> Unit
-): Deferred<Unit> = async(IO) { delegateCsvWriter(configuration) }
+): Deferred<Unit> = async(IO) { processCsvWriter(configuration) }
 
 /**
  * Writes records in a CSV file according to the given [configuration], or
  * returns `null` when the [configuration] is invalid.
  */
 public suspend fun csvWriterOrNull(configuration: Writer.() -> Unit): Unit? =
-    withContext(IO) { delegateCsvWriterOrNull(configuration) }
+    withContext(IO) { processCsvWriterOrNull(configuration) }
 
 /**
  * Writes records in a CSV file **asynchronously** according to the given
@@ -37,7 +51,30 @@ public suspend fun csvWriterOrNull(configuration: Writer.() -> Unit): Unit? =
  */
 public infix fun CoroutineScope.csvWriterOrNullAsync(
     configuration: Writer.() -> Unit
-): Deferred<Unit?> = async(IO) { delegateCsvWriterOrNull(configuration) }
+): Deferred<Unit?> = async(IO) { processCsvWriterOrNull(configuration) }
+
+private inline fun processCsvWriter(configuration: Writer.() -> Unit): Unit =
+    writer.apply(configuration)
+        .takeIf(WriterConfiguration::isValid)
+        ?.writeInFile()
+        ?: invalidConfigurationException()
+
+private inline fun processCsvWriterOrNull(configuration: Writer.() -> Unit):
+        Unit? = writer.apply(configuration)
+    .takeIf(WriterConfiguration::isValid)
+    ?.writeInFileOrNull()
+
+@Throws(IOException::class)
+private fun Writer.createDirectoryAt(path: Path) {
+    path.createDirectory()
+    overwrite = true
+}
+
+@Throws(IOException::class)
+private fun Writer.getOrCreateSystemFile(): File? = loader.baseUrl
+    ?.let { Path("${it.path}$folder") }
+    ?.apply { takeIf(Path::notExists)?.let(::createDirectoryAt) }
+    ?.let { File("$it/$file") }
 
 /** Configurable object responsible for writing records in a CSV file. */
 public sealed interface Writer : Manager {
@@ -58,5 +95,48 @@ public sealed interface Writer : Manager {
     public sealed interface Records {
         /** Adds the given [Iterable] as a record. */
         public operator fun Iterable<String>.unaryPlus()
+    }
+}
+
+internal class WriterConfiguration : ManagerConfiguration(), Writer {
+    override var header: Set<String> = emptySet()
+    override var overwrite: Boolean = true
+    private val mutableRecords: MutableList<List<String>> = mutableListOf()
+    private val records: List<List<String?>> by lazy {
+        mutableRecords.map { List(header.size, it::getOrNull) }
+    }
+
+    override fun records(configuration: Writer.Records.() -> Unit) {
+        mutableRecords += RecordsConfiguration().apply(configuration).records
+    }
+
+    fun isValid(): Boolean =
+        file.isNotBlank() && header.isNotEmpty() && records.isNotEmpty()
+
+    @Throws(CsvException::class)
+    fun writeInFile(): Unit =
+        writeInFileOrNull() ?: fileNotFoundException("$folder$file")
+
+    fun writeInFileOrNull(): Unit? = try {
+        (resourceFile ?: getOrCreateSystemFile())?.let {
+            if (overwrite) writeWithHeaderIn(it) else writeIn(it)
+        }
+    } catch (exception: IOException) {
+        exception.printStackTrace()
+        null
+    }
+
+    private infix fun writeIn(file: File): Unit = csv.writeAll(records, file)
+
+    private infix fun writeWithHeaderIn(file: File): Unit =
+        mutableListOf(header.toList(), records).let { csv.writeAll(it, file) }
+
+    class RecordsConfiguration : Writer.Records {
+        val records: List<List<String>> get() = mutableRecords
+        private val mutableRecords: MutableList<List<String>> = mutableListOf()
+
+        override fun Iterable<String>.unaryPlus() {
+            mutableRecords += toList()
+        }
     }
 }
