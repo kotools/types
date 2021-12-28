bfc6a40b06ce5d0aece8bc5e19fa23849a0d488e
Loïc Lamarque <loiclamarque777@gmail.com>
2021-12-28 16:48:16 +0100

    refactor: update class loader utilities

--- src/main/kotlin/io/github/kotools/csv/ClassLoader.kt
+++ src/main/kotlin/io/github/kotools/csv/ClassLoader.kt
@@ -7,10 +7,8 @@ import java.net.URL
 internal val loader: ClassLoader get() = ClassLoader.getSystemClassLoader()
 internal val ClassLoader.baseUrl: URL? get() = getResource("")
 
-internal infix fun ClassLoader.getResourceFile(path: String): File? {
-    val url: URL = getResource(path) ?: return null
-    return File(url.path)
-}
+internal infix fun ClassLoader.getResourceFile(path: String): File? =
+    getResource(path)?.let { File(it.path) }
 
 internal infix fun ClassLoader.getStream(path: String): InputStream? =
     getResourceAsStream(path)
