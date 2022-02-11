ebb2d435bed092a882610059cd37200532dd5062
Loïc Lamarque <loiclamarque777@gmail.com>
2022-02-11 23:50:19 +0100

    refactor: update not equals assertions

--- src/main/kotlin/io/github/kotools/assert/NotEquals.kt
+++ src/main/kotlin/io/github/kotools/assert/NotEquals.kt
@@ -7,17 +7,17 @@ public infix fun <T : Any> T.assertNotEquals(other: T): Unit =
     assertNotEquals(other, this)
 
 /**
+ * Asserts that the current value isn't equal to the [other] value, or compute
+ * and report the [lazyMessage]'s result if not.
+ */
+public inline fun <T : Any> T.assertNotEquals(
+    other: T,
+    lazyMessage: () -> String
+): Unit = assertNotEquals(other, this, lazyMessage())
+
+/**
  * Asserts that the current value isn't equal to the [other] value, or report
  * the [message] if not.
  */
 public fun <T : Any> T.assertNotEquals(other: T, message: String): Unit =
     assertNotEquals(other, this, message)
-
-/**
- * Asserts that the current value isn't equal to the [other] value, or compute
- * and report the [lazyMessage]'s result if not.
- */
-public fun <T : Any> T.assertNotEquals(
-    other: T,
-    lazyMessage: () -> String
-): Unit = assertNotEquals(other, this, lazyMessage())
