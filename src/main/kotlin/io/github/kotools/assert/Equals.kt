83133b129a5acf7d163dfe0d88b0b67b5104b531
Loïc Lamarque <loiclamarque777@gmail.com>
2022-02-11 23:50:45 +0100

    refactor: update equals assertions

--- src/main/kotlin/io/github/kotools/assert/Equals.kt
+++ src/main/kotlin/io/github/kotools/assert/Equals.kt
@@ -7,13 +7,6 @@ public infix fun <T : Any> T.assertEquals(other: T): Unit =
     assertEquals(other, this)
 
 /**
- * Asserts that the current value is equal to the [other] value, or report the
- * [message] if not.
- */
-public fun <T : Any> T.assertEquals(other: T, message: String): Unit =
-    assertEquals(other, this, message)
-
-/**
  * Asserts that the current value is equal to the [other] value, or compute and
  * report the [lazyMessage]'s result if not.
  */
@@ -21,3 +14,10 @@ public inline fun <T : Any> T.assertEquals(
     other: T,
     lazyMessage: () -> String
 ): Unit = assertEquals(other, this, lazyMessage())
+
+/**
+ * Asserts that the current value is equal to the [other] value, or report the
+ * [message] if not.
+ */
+public fun <T : Any> T.assertEquals(other: T, message: String): Unit =
+    assertEquals(other, this, message)
