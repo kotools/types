379c0f1d83374b98a418b5882e9eb25ecedbbdcc
Loïc Lamarque <loiclamarque777@gmail.com>
2022-02-11 21:55:40 +0100

    docs: document assertions for equalities

--- src/main/kotlin/io/github/kotools/assert/Equals.kt
+++ src/main/kotlin/io/github/kotools/assert/Equals.kt
@@ -2,12 +2,21 @@ package io.github.kotools.assert
 
 import kotlin.test.assertEquals
 
+/** Asserts that the current value is equal to the [other] value. */
 public infix fun <T : Any> T.assertEquals(other: T): Unit =
     assertEquals(other, this)
 
+/**
+ * Asserts that the current value is equal to the [other] value, or report the
+ * [message] if not.
+ */
 public fun <T : Any> T.assertEquals(other: T, message: String): Unit =
     assertEquals(other, this, message)
 
+/**
+ * Asserts that the current value is equal to the [other] value, or compute and
+ * report the [lazyMessage]'s result if not.
+ */
 public inline fun <T : Any> T.assertEquals(
     other: T,
     lazyMessage: () -> String
