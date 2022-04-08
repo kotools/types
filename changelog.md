337592a3082a55e4298c6400a05edc85d5ce3ba8
Loïc Lamarque <loiclamarque777@gmail.com>
2022-04-08 21:33:07 +0200

    feat(#5): split specific failure assertions from untyped ones

--- changelog.md
+++ changelog.md
@@ -3,7 +3,8 @@
 All notable changes to this project will be documented in this file.
 
 > The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0)
-> and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+> and this project adheres to
+> [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## Types of changes
 
@@ -15,6 +16,11 @@ All notable changes to this project will be documented in this file.
 - `Security` in case of vulnerabilities.
 
 ## Work in progress
+
+### Changed
+
+- Split specific failure assertions `assertFailsWith` from untyped failure
+  assertions `assertFails` ([#5](https://github.com/kotools/assert/issues/5)).
 
 ## Version 1.1.0 - 2022/02/12
 
--- src/main/kotlin/io/github/kotools/assert/Fails.kt
+++ src/main/kotlin/io/github/kotools/assert/Fails.kt
@@ -7,10 +7,6 @@ public inline fun assertFails(block: () -> Unit) {
     kotlin.test.assertFails(block)
 }
 
-/** Asserts that the [block] throws the exception [T]. */
-public inline fun <reified T : Throwable> assertFails(block: () -> Unit): T =
-    assertFailsWith(block = block)
-
 /**
  * Asserts that the [block] throws an exception, or report the [message] if not.
  */
@@ -18,11 +14,16 @@ public inline fun assertFails(message: String, block: () -> Unit) {
     kotlin.test.assertFails(message, block)
 }
 
+/** Asserts that the [block] throws the exception [T]. */
+public inline fun <reified T : Throwable> assertFailsWith(
+    block: () -> Unit
+): T = assertFailsWith(block = block)
+
 /**
  * Asserts that the [block] throws the exception [T], or report the [message] if
  * not.
  */
-public inline fun <reified T : Throwable> assertFails(
+public inline fun <reified T : Throwable> assertFailsWith(
     message: String,
     block: () -> Unit
 ): T = assertFailsWith(message, block)
