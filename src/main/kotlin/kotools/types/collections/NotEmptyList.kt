0962d32f782a985335648c2799844d072b9c4e08
Loïc Lamarque <loiclamarque777@gmail.com>
2022-07-11 22:37:28 +0200

    feat(#6): add more query operations to NotEmptyList type

--- src/main/kotlin/kotools/types/collections/NotEmptyList.kt
+++ src/main/kotlin/kotools/types/collections/NotEmptyList.kt
@@ -1,23 +1,34 @@
 package kotools.types.collections
 
-/** Creates a [NotEmptyList] with a given [head] and an optional [tail]. */
-public fun <E : Any> notEmptyList(head: E, vararg tail: E): NotEmptyList<E> =
-    NotEmptyList.create(head, *tail)
+import kotools.types.number.StrictlyPositiveInt
 
-/** Type representing lists that can't be empty. */
-public class NotEmptyList<out E : Any> private constructor(
+/**
+ * Represents lists that can't be empty.
+ *
+ * @constructor Creates a [NotEmptyList] with a given [head] and an optional
+ * [tail].
+ */
+public class NotEmptyList<out E>(
     /** First element of the current list. */
     public val head: E,
-    private val tail: List<E>
+    vararg tail: E
 ) : AbstractList<E>() {
+    private val tail: List<E>
+
+    init {
+        this.tail = tail.toList()
+    }
+
+    // ---------- Query operations ----------
+
     override val size: Int get() = tail.size + 1
 
-    override fun get(index: Int): E = if (index == 0) head else tail[index - 1]
+    /** Returns the [size] of the collection as a [StrictlyPositiveInt]. */
+    public val typedSize: StrictlyPositiveInt get() = StrictlyPositiveInt(size)
 
     override fun isEmpty(): Boolean = false
 
-    internal companion object {
-        fun <E : Any> create(head: E, vararg tail: E): NotEmptyList<E> =
-            NotEmptyList(head, tail.asList())
-    }
+    // ---------- Positional Access Operations ----------
+
+    override fun get(index: Int): E = if (index == 0) head else tail[index - 1]
 }
