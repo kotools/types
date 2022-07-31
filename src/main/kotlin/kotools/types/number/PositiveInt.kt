da39889d10e6bffd7ce44cf3e0cc8fbce91bebe6
Loïc Lamarque <loiclamarque777@gmail.com>
2022-07-31 11:48:48 +0200

    docs(#51): fix the documentation of comparisons in NonZeroInt and PositiveInt

--- src/main/kotlin/kotools/types/number/PositiveInt.kt
+++ src/main/kotlin/kotools/types/number/PositiveInt.kt
@@ -235,9 +235,9 @@ public value class PositiveInt(
 
     /**
      * Compares this [value] with the [other] value for order.
-     * Returns `0` if this object equals the [other] value, a negative number if
-     * this [value] is less than the [other] value, or a positive number if this
-     * [value] is greater than the [other] value.
+     * Returns `0` if this [value] equals the [other] value, a negative number
+     * if this [value] is less than the [other] value, or a positive number if
+     * this [value] is greater than the [other] value.
      */
     override infix fun compareTo(other: PositiveInt): Int =
         compareTo(other.value)
