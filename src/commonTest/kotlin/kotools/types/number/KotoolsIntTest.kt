3c9bf13df02efb7b6e227f1848c5b8505f82ddae
Loïc Lamarque <loiclamarque777@gmail.com>
2022-09-12 22:27:46 +0200

    test(#83): update tests of KotoolsInt

--- src/commonTest/kotlin/kotools/types/number/KotoolsIntTest.kt
+++ src/commonTest/kotlin/kotools/types/number/KotoolsIntTest.kt
@@ -14,241 +14,172 @@ class KotoolsIntTest {
 
     @Test
     fun plus_should_pass_with_an_Int() {
-        // GIVEN
         val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         val y: Int = Random.nextInt()
-        // WHEN
         val result: Int = x + y
-        // THEN
         result assertEquals x.value + y
     }
 
     @Test
     fun plus_should_pass_when_adding_a_KotoolsInt_to_an_Int() {
-        // GIVEN
         val x: Int = Random.nextInt()
         val y: KotoolsInt = Random.nextInt().toKotoolsIntExample()
-        // WHEN
         val result: Int = x + y
-        // THEN
         result assertEquals x + y.value
     }
 
     @Test
     fun plus_should_pass_with_a_KotoolsInt() {
-        // GIVEN
         val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         val y: KotoolsInt = Random.nextInt().toKotoolsIntExample()
-        // WHEN
         val result: Int = x + y
-        // THEN
         result assertEquals x.value + y.value
     }
 
     @Test
     fun minus_should_pass_with_an_Int() {
-        // GIVEN
         val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         val y: Int = Random.nextInt()
-        // WHEN
         val result: Int = x - y
-        // THEN
         result assertEquals x.value - y
     }
 
     @Test
     fun minus_should_pass_with_a_KotoolsInt() {
-        // GIVEN
         val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         val y: KotoolsInt = Random.nextInt().toKotoolsIntExample()
-        // WHEN
         val result: Int = x - y
-        // THEN
         result assertEquals x.value - y.value
     }
 
     @Test
     fun minus_should_pass_when_subtracting_a_KotoolsInt_from_an_Int() {
-        // GIVEN
         val x: Int = Random.nextInt()
         val y: KotoolsInt = Random.nextInt().toKotoolsIntExample()
-        // WHEN
         val result: Int = x - y
-        // THEN
         result assertEquals x - y.value
     }
 
     @Test
     fun times_should_pass_with_an_Int() {
-        // GIVEN
         val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         val y: Int = Random.nextInt()
-        // WHEN
         val result: Int = x * y
-        // THEN
         result assertEquals x.value * y
     }
 
     @Test
     fun times_should_pass_with_a_KotoolsInt() {
-        // GIVEN
         val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         val y: KotoolsInt = Random.nextInt().toKotoolsIntExample()
-        // WHEN
         val result: Int = x * y
-        // THEN
         result assertEquals x.value * y.value
     }
 
     @Test
     fun times_should_pass_when_multiplying_an_Int_by_a_KotoolsInt() {
-        // GIVEN
         val x: Int = Random.nextInt()
         val y: KotoolsInt = Random.nextInt().toKotoolsIntExample()
-        // WHEN
         val result: Int = x * y
-        // THEN
         result assertEquals x * y.value
     }
 
     @Test
     fun div_should_pass_with_an_Int_other_than_0() {
-        // GIVEN
         val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         var y = 0
         while (y == 0) y = Random.nextInt()
-        // WHEN
         val result: Int = x / y
-        // THEN
         result assertEquals x.value / y
     }
 
     @Test
     fun div_should_pass_when_dividing_an_Int_by_a_KotoolsInt_other_than_0() {
-        // GIVEN
         val x: Int = Random.nextInt()
         var y: KotoolsInt = KotoolsIntExample(0)
         while (y.value == 0) y = Random.nextInt().toKotoolsIntExample()
-        // WHEN
         val result: Int = x / y
-        // THEN
         result assertEquals x / y.value
     }
 
     @Test
     fun div_should_pass_with_a_KotoolsInt_other_than_0() {
-        // GIVEN
         val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         var y: KotoolsInt = KotoolsIntExample(0)
         while (y.value == 0) y = Random.nextInt().toKotoolsIntExample()
-        // WHEN
         val result: Int = x / y
-        // THEN
         result assertEquals x.value / y.value
     }
 
     // ---------- Comparisons ----------
 
     @Test
-    fun compareTo_should_return_0_with_the_same_Int() {
-        // GIVEN
-        val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
-        // WHEN
-        val result: Int = x compareTo x.value
-        // THEN
-        result assertEquals 0
-    }
+    fun compareTo_should_return_0_with_the_same_Int(): Unit = Random.nextInt()
+        .toKotoolsIntExample()
+        .run { this compareTo value assertEquals 0 }
 
     @Test
     fun compareTo_should_return_a_negative_number_with_a_greater_Int() {
-        // GIVEN
         val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         var y: Int = Random.nextInt()
         while (x >= y) y = Random.nextInt()
-        // WHEN
         val result: Int = x compareTo y
-        // THEN
         assertTrue { result < 0 }
     }
 
     @Test
     fun compareTo_should_return_a_positive_number_with_a_less_Int() {
-        // GIVEN
         val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         var y: Int = Random.nextInt()
         while (x <= y) y = Random.nextInt()
-        // WHEN
         val result: Int = x compareTo y
-        // THEN
         assertTrue { result > 0 }
     }
 
     @Test
-    fun compareTo_should_return_0_when_comparing_an_Int_with_the_same_KotoolsInt() {
-        // GIVEN
-        val x: Int = Random.nextInt()
-        val y: KotoolsInt = x.toKotoolsIntExample()
-        // WHEN
-        val result: Int = x compareTo y
-        // THEN
-        result assertEquals 0
-    }
+    fun compareTo_should_return_0_when_comparing_an_Int_with_the_same_KotoolsInt(): Unit =
+        Random.nextInt()
+            .run { this compareTo toKotoolsIntExample() assertEquals 0 }
 
     @Test
     fun compareTo_should_return_a_negative_number_when_comparing_an_Int_with_a_greater_KotoolsInt() {
-        // GIVEN
         val x: Int = Random.nextInt()
         var y: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         while (x >= y) y = Random.nextInt().toKotoolsIntExample()
-        // WHEN
         val result: Int = x compareTo y
-        // THEN
         assertTrue { result < 0 }
     }
 
     @Test
     fun compareTo_should_return_a_positive_number_when_comparing_an_Int_with_a_less_KotoolsInt() {
-        // GIVEN
         val x: Int = Random.nextInt()
         var y: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         while (x <= y) y = Random.nextInt().toKotoolsIntExample()
-        // WHEN
         val result: Int = x compareTo y
-        // THEN
         assertTrue { result > 0 }
     }
 
     @Test
-    fun compareTo_should_return_0_with_the_same_KotoolsInt() {
-        // GIVEN
-        val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
-        // WHEN
-        val result: Int = x compareTo x
-        // THEN
-        result assertEquals 0
-    }
+    fun compareTo_should_return_0_with_the_same_KotoolsInt(): Unit =
+        Random.nextInt()
+            .toKotoolsIntExample()
+            .let { it compareTo it assertEquals 0 }
 
     @Test
     fun compareTo_should_return_a_negative_number_with_a_greater_KotoolsInt() {
-        // GIVEN
         val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         var y: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         while (x >= y) y = Random.nextInt().toKotoolsIntExample()
-        // WHEN
         val result: Int = x compareTo y
-        // THEN
         assertTrue { result < 0 }
     }
 
     @Test
     fun compareTo_should_return_a_positive_number_with_a_less_KotoolsInt() {
-        // GIVEN
         val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         var y: KotoolsInt = Random.nextInt().toKotoolsIntExample()
         while (x <= y) y = Random.nextInt().toKotoolsIntExample()
-        // WHEN
         val result: Int = x compareTo y
-        // THEN
         assertTrue { result > 0 }
     }
 
--- src/nativeTest/kotlin/kotools/types/number/KotoolsIntNativeTest.kt
+++ src/nativeTest/kotlin/kotools/types/number/KotoolsIntNativeTest.kt
@@ -7,28 +7,22 @@ import kotlin.test.Test
 class KotoolsIntNativeTest {
     @Test
     fun `div should throw an ArithmeticException with an Int that equals 0`() {
-        // GIVEN
-        val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
-        val y = 0
-        // WHEN & THEN
-        assertFailsWith<ArithmeticException> { x / y }
+        assertFailsWith<ArithmeticException> {
+            Random.nextInt().toKotoolsIntExample() / 0
+        }
     }
 
     @Test
     fun `div should throw an ArithmeticException when dividing an Int by a KotoolsInt that equals 0`() {
-        // GIVEN
-        val x: Int = Random.nextInt()
-        val y: KotoolsInt = KotoolsIntExample(0)
-        // WHEN & THEN
-        assertFailsWith<ArithmeticException> { x / y }
+        assertFailsWith<ArithmeticException> {
+            Random.nextInt() / KotoolsIntExample(0)
+        }
     }
 
     @Test
     fun `div should throw an ArithmeticException with a KotoolsInt that equals 0`() {
-        // GIVEN
-        val x: KotoolsInt = Random.nextInt().toKotoolsIntExample()
-        val y = KotoolsIntExample(0)
-        // WHEN & THEN
-        assertFailsWith<ArithmeticException> { x / y }
+        assertFailsWith<ArithmeticException> {
+            Random.nextInt().toKotoolsIntExample() / KotoolsIntExample(0)
+        }
     }
 }
