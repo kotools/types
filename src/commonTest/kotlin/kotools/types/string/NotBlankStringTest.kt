9342e1fd38c38bdcf79c66a4fe353fe977a4f172
Loïc Lamarque <loiclamarque777@gmail.com>
2022-09-13 08:37:42 +0200

    test(#89): update tests of NotBlankString

--- src/commonTest/kotlin/kotools/types/string/NotBlankStringTest.kt
+++ src/commonTest/kotlin/kotools/types/string/NotBlankStringTest.kt
@@ -30,25 +30,6 @@ class NotBlankStringTest {
         result.assertNull()
     }
 
-    @Test
-    fun stringToNotBlankString_should_pass_with_a_not_blank_String(): Unit =
-        VALUE.toNotBlankString().value assertEquals VALUE
-
-    @Test
-    fun stringToNotBlankString_should_throw_an_error_with_a_blank_String() {
-        assertFailsWith<IllegalArgumentException>(""::toNotBlankString)
-    }
-
-    @Test
-    fun stringToNotBlankStringOrNull_should_pass_with_a_not_blank_String(): Unit =
-        VALUE.toNotBlankStringOrNull()
-            .assertNotNull()
-            .value assertEquals VALUE
-
-    @Test
-    fun stringToNotBlankStringOrNull_should_return_null_with_a_blank_String(): Unit =
-        "".toNotBlankStringOrNull().assertNull()
-
     private companion object {
         const val VALUE = "hello world"
     }
