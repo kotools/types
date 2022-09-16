a68264f3cb09d5203ae6fee0c4d82d4024e4b9d9
Loïc Lamarque <loiclamarque777@gmail.com>
2022-09-17 00:49:36 +0200

    feat(#100): create the NonZeroNumber type for all platforms

---
+++ src/commonMain/kotlin/kotools/types/number/NonZeroNumber.kt
@@ -0,0 +1,40 @@
+package kotools.types.number
+
+import kotools.types.annotations.SinceKotoolsTypes
+
+/** Represents numeric values that don't equal `0`. */
+@SinceKotoolsTypes("3.0")
+public sealed interface NonZeroNumber<T : kotlin.Number> : Number<T> {
+    public companion object {
+        /**
+         * Returns the [value] as a [NonZeroNumber], or throws an
+         * [IllegalArgumentException] if the [value] equals `0`.
+         */
+        @Throws(IllegalArgumentException::class)
+        public infix operator fun <T : kotlin.Number> invoke(
+            value: T
+        ): NonZeroNumber<T> {
+            require(value != 0) {
+                val type: String = NonZeroNumber::class.simpleName!!
+                "$type doesn't accept 0."
+            }
+            return Implementation(value)
+        }
+
+        /**
+         * Returns the [value] as a [NonZeroNumber], or returns `null` if the
+         * [value] equals `0`.
+         */
+        public infix fun <T : kotlin.Number> orNull(
+            value: T
+        ): NonZeroNumber<T>? = try {
+            invoke(value)
+        } catch (_: IllegalArgumentException) {
+            null
+        }
+    }
+
+    private class Implementation<T : kotlin.Number>(
+        override val value: T
+    ) : NonZeroNumber<T>
+}
