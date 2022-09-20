469af8f02b654d0f3760371aac660dbac784b2c3
Loïc Lamarque <loiclamarque777@gmail.com>
2022-09-20 21:44:14 +0200

    Revert "feat(#99): add a builder to the Number type"
    This reverts commit f0b6c14d987990ec045c2e7781fff769253143ae.

--- src/commonMain/kotlin/kotools/types/number/Number.kt
+++ src/commonMain/kotlin/kotools/types/number/Number.kt
@@ -2,10 +2,6 @@ package kotools.types.number
 
 import kotools.types.annotations.SinceKotoolsTypes
 
-/** Returns this value as a [Number]. */
-@SinceKotoolsTypes("3.0")
-public fun <T : kotlin.Number> T.toKotoolsNumber(): Number<T> = Number(this)
-
 /** Represents numeric values. */
 @SinceKotoolsTypes("3.0")
 public sealed interface Number<T : kotlin.Number> {
@@ -15,10 +11,11 @@ public sealed interface Number<T : kotlin.Number> {
         /** Returns the [value] as a [Number]. */
         public infix operator fun <T : kotlin.Number> invoke(
             value: T
-        ): Number<T> = Implementation(value)
+        ): Number<T> = NumberImplementation(value)
     }
-
-    private class Implementation<T : kotlin.Number>(
-        override val value: T
-    ) : Number<T>
 }
+
+@SinceKotoolsTypes("3.0")
+private class NumberImplementation<T : kotlin.Number>(
+    override val value: T
+) : Number<T>
