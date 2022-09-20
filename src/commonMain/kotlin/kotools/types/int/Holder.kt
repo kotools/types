066b3ebfbf32641f88dc58614965b9bafc46be4e
Loïc Lamarque <loiclamarque777@gmail.com>
2022-09-21 01:42:07 +0200

    docs(#106): update the documentation of IntHolder's comparisons

--- src/commonMain/kotlin/kotools/types/int/Holder.kt
+++ src/commonMain/kotlin/kotools/types/int/Holder.kt
@@ -10,6 +10,14 @@ public sealed interface IntHolder : Comparable<IntHolder> {
 
     /** Optional property responsible for checking the [value]'s validity. */
     public val validator: IntValidator?
+
+    /**
+     * Compares this [value] with the [other] value for order.
+     * Returns `0` if this [value] equals the [other] value, a negative number
+     * if this [value] is less than the [other] value, or a positive number if
+     * this [value] is greater than the [other] value.
+     */
+    override fun compareTo(other: IntHolder): Int
 }
 
 @SinceKotoolsTypes("3.0")
