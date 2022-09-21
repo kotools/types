523854420588a9dc19ff646aa2e5b18dc6014098
Loïc Lamarque <loiclamarque777@gmail.com>
2022-09-21 21:05:47 +0200

    docs(#106): update documentation of IntHolder

--- src/commonMain/kotlin/kotools/types/int/Holder.kt
+++ src/commonMain/kotlin/kotools/types/int/Holder.kt
@@ -31,9 +31,9 @@ public infix operator fun Int.div(other: IntHolder): Int = div(other.value)
 
 /**
  * Compares this value with the [other] value for order.
- * Returns `0` if this value equals the [other] value, a negative number if this
- * value is less than the [other] value, or a positive number if this value is
- * greater than the [other] value.
+ * Returns zero if this value equals the [other] value, a negative number if
+ * this value is less than the [other] value, or a positive number if this value
+ * is greater than the [other] value.
  */
 @SinceKotoolsTypes("3.0")
 public infix operator fun Int.compareTo(other: IntHolder): Int =
@@ -81,7 +81,7 @@ public sealed interface IntHolder : Comparable<IntHolder> {
 
     /**
      * Compares this [value] with the [other] value for order.
-     * Returns `0` if this [value] equals the [other] value, a negative number
+     * Returns zero if this [value] equals the [other] value, a negative number
      * if this [value] is less than the [other] value, or a positive number if
      * this [value] is greater than the [other] value.
      */
@@ -90,7 +90,7 @@ public sealed interface IntHolder : Comparable<IntHolder> {
 
     /**
      * Compares this [value] with the [other] value for order.
-     * Returns `0` if this [value] equals the [other] value, a negative number
+     * Returns zero if this [value] equals the [other] value, a negative number
      * if this [value] is less than the [other] value, or a positive number if
      * this [value] is greater than the [other] value.
      */
