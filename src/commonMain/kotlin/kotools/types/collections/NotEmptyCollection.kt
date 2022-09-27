078a9b3e00dce981776062be5f98223697558bc3
Loïc Lamarque <loiclamarque777@gmail.com>
2022-09-27 23:50:39 +0200

    feat(#91): add query operation to NotEmptyCollection

--- src/commonMain/kotlin/kotools/types/collections/NotEmptyCollection.kt
+++ src/commonMain/kotlin/kotools/types/collections/NotEmptyCollection.kt
@@ -1,6 +1,7 @@
 package kotools.types.collections
 
 import kotools.types.core.SinceKotoolsTypes
+import kotools.types.int.StrictlyPositiveInt
 
 /**
  * Parent of classes representing collections that contain at least one element.
@@ -11,4 +12,9 @@ import kotools.types.core.SinceKotoolsTypes
 public sealed interface NotEmptyCollection<out E> : Collection<E> {
     /** First element of this collection. */
     public val head: E
+
+    // ---------- Query operations ----------
+
+    /** Returns the [size] of this collection as a [StrictlyPositiveInt]. */
+    public val typedSize: StrictlyPositiveInt get() = StrictlyPositiveInt(size)
 }
