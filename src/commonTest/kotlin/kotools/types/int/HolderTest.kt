7708213f7063f8110608a20857105af52a4f3958
Loïc Lamarque <loiclamarque777@gmail.com>
2022-09-29 23:32:01 +0200

    test(#106): add Gherkin statements in tests of IntHolder

--- src/commonTest/kotlin/kotools/types/int/HolderTest.kt
+++ src/commonTest/kotlin/kotools/types/int/HolderTest.kt
@@ -13,113 +13,149 @@ class IntHolderTest {
 
     @Test
     fun plus_should_pass_with_an_Int() {
+        // GIVEN
         val xValue: Int = Random.nextInt()
         val x = IntHolder(xValue)
         val y: Int = Random.nextInt()
+        // WHEN
         val result: Int = x + y
+        // THEN
         result assertEquals xValue + y
     }
 
     @Test
     fun plus_should_pass_with_an_IntHolder() {
+        // GIVEN
         val xValue: Int = Random.nextInt()
         val yValue: Int = Random.nextInt()
         val x = IntHolder(xValue)
         val y = IntHolder(yValue)
+        // WHEN
         val result: Int = x + y
+        // THEN
         result assertEquals xValue + yValue
     }
 
     @Test
     fun plus_should_pass_when_adding_an_IntHolder_to_an_Int() {
+        // GIVEN
         val x: Int = Random.nextInt()
         val yValue: Int = Random.nextInt()
         val y = IntHolder(yValue)
+        // WHEN
         val result: Int = x + y
+        // THEN
         result assertEquals x + yValue
     }
 
     @Test
     fun minus_should_pass_with_an_Int() {
+        // GIVEN
         val xValue: Int = Random.nextInt()
         val x = IntHolder(xValue)
         val y: Int = Random.nextInt()
+        // WHEN
         val result: Int = x - y
+        // THEN
         result assertEquals xValue - y
     }
 
     @Test
     fun minus_should_pass_with_an_IntHolder() {
+        // GIVEN
         val xValue: Int = Random.nextInt()
         val yValue: Int = Random.nextInt()
         val x = IntHolder(xValue)
         val y = IntHolder(yValue)
+        // WHEN
         val result: Int = x - y
+        // THEN
         result assertEquals xValue - yValue
     }
 
     @Test
     fun minus_should_pass_when_subtracting_an_IntHolder_from_an_Int() {
+        // GIVEN
         val x: Int = Random.nextInt()
         val yValue: Int = Random.nextInt()
         val y = IntHolder(yValue)
+        // WHEN
         val result: Int = x - y
+        // THEN
         result assertEquals x - yValue
     }
 
     @Test
     fun times_should_pass_with_an_Int() {
+        // GIVEN
         val xValue: Int = Random.nextInt()
         val x = IntHolder(xValue)
         val y: Int = Random.nextInt()
+        // WHEN
         val result: Int = x * y
+        // THEN
         result assertEquals xValue * y
     }
 
     @Test
     fun times_should_pass_with_an_IntHolder() {
+        // GIVEN
         val xValue: Int = Random.nextInt()
         val yValue: Int = Random.nextInt()
         val x = IntHolder(xValue)
         val y = IntHolder(yValue)
+        // WHEN
         val result: Int = x * y
+        // THEN
         result assertEquals xValue * yValue
     }
 
     @Test
     fun times_should_pass_when_multiplying_an_Int_with_an_IntHolder() {
+        // GIVEN
         val x: Int = Random.nextInt()
         val yValue: Int = Random.nextInt()
         val y = IntHolder(yValue)
+        // WHEN
         val result: Int = x * y
+        // THEN
         result assertEquals x * yValue
     }
 
     @Test
     fun div_should_pass_with_an_Int() {
+        // GIVEN
         val xValue: Int = Random.nextInt()
         val x = IntHolder(xValue)
         val y: Int = Random.nextInt()
+        // WHEN
         val result: Int = x / y
+        // THEN
         result assertEquals xValue / y
     }
 
     @Test
     fun div_should_pass_with_an_IntHolder() {
+        // GIVEN
         val xValue: Int = Random.nextInt()
         val yValue: Int = Random.nextInt()
         val x = IntHolder(xValue)
         val y = IntHolder(yValue)
+        // WHEN
         val result: Int = x / y
+        // THEN
         result assertEquals xValue / yValue
     }
 
     @Test
     fun div_should_pass_when_dividing_an_Int_by_an_IntHolder() {
+        // GIVEN
         val x: Int = Random.nextInt()
         val yValue: Int = Random.nextInt()
         val y = IntHolder(yValue)
+        // WHEN
         val result: Int = x / y
+        // THEN
         result assertEquals x / yValue
     }
 
@@ -127,29 +163,38 @@ class IntHolderTest {
 
     @Test
     fun compareTo_should_pass_with_an_Int() {
+        // GIVEN
         val xValue: Int = Random.nextInt()
         val x = IntHolder(xValue)
         val y: Int = Random.nextInt()
+        // WHEN
         val result: Int = x compareTo y
+        // THEN
         result assertEquals xValue.compareTo(y)
     }
 
     @Test
     fun compareTo_should_pass_with_an_IntHolder() {
+        // GIVEN
         val xValue: Int = Random.nextInt()
         val yValue: Int = Random.nextInt()
         val x = IntHolder(xValue)
         val y = IntHolder(yValue)
+        // WHEN
         val result: Int = x compareTo y
+        // THEN
         result assertEquals xValue.compareTo(yValue)
     }
 
     @Test
     fun compareTo_should_pass_when_comparing_an_Int_with_an_IntHolder() {
+        // GIVEN
         val x: Int = Random.nextInt()
         val yValue: Int = Random.nextInt()
         val y = IntHolder(yValue)
+        // WHEN
         val result: Int = x compareTo y
+        // THEN
         result assertEquals x.compareTo(yValue)
     }
 }
@@ -157,18 +202,22 @@ class IntHolderTest {
 class IntHolderImplementationTest {
     @Test
     fun equals_should_pass_with_the_same_IntHolder() {
+        // GIVEN
         val value: Int = Random.nextInt()
         val x = IntHolder(value)
         val y = IntHolder(value)
+        // WHEN & THEN
         assertTrue { x == y }
     }
 
     @Test
     fun equals_should_return_false_with_different_IntHolders() {
+        // GIVEN
         val xValue: Int = Random.nextInt()
         val yValue: Int = Random.nextInt()
         val x = IntHolder(xValue)
         val y = IntHolder(yValue)
+        // WHEN & THEN
         assertFalse { x == y }
     }
 }
@@ -180,17 +229,23 @@ class IntHolderSerializerTest {
 
     @Test
     fun serialize_should_pass() {
+        // GIVEN
         val value: Int = Random.nextInt()
         val x = IntHolder(value)
+        // WHEN
         val result: String = Json.encodeToString(serializer, x)
+        // THEN
         result assertEquals Json.encodeToString(value)
     }
 
     @Test
     fun deserialize_should_pass() {
+        // GIVEN
         val value: Int = Random.nextInt()
         val encoded: String = Json.encodeToString(value)
+        // WHEN
         val result: IntHolder = Json.decodeFromString(serializer, encoded)
+        // THEN
         result.value assertEquals value
     }
 }
