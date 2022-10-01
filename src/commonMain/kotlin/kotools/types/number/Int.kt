7a945af830d737bc837b9e3d8e55c7feb6be77a3
Loïc Lamarque <loiclamarque777@gmail.com>
2022-10-01 03:29:24 +0200

    feat(#110): create a new type system for integers
    The following types are now available on JVM, JS and Native platforms in the kotools.types.number package: NonZeroInt, PositiveInt, StrictlyPositiveInt, NegativeInt, StrictlyNegativeInt.

---
+++ src/commonMain/kotlin/kotools/types/number/Int.kt
@@ -0,0 +1,145 @@
+package kotools.types.number
+
+import kotools.types.core.SinceKotoolsTypes
+import kotlin.jvm.JvmInline
+
+/** Parent of classes responsible for holding integers. */
+@SinceKotoolsTypes("3.0")
+public sealed interface IntHolder {
+    /** The value to hold. */
+    public val value: Int
+}
+
+/** Parent of classes responsible for creating instances of [IntHolder]. */
+@SinceKotoolsTypes("3.0")
+public sealed interface IntHolderCompanion<out T : IntHolder> {
+    /** The minimum value of the type [T]. */
+    @SinceKotoolsTypes("1.1")
+    public val min: T
+
+    /** The maximum value of the type [T]. */
+    @SinceKotoolsTypes("1.1")
+    public val max: T
+
+    /** Returns an instance of type [T] holding a random value. */
+    public val random: T
+
+    /**
+     * Returns the [value] as a type [T], or throws an
+     * [IllegalArgumentException] if it fails.
+     */
+    @SinceKotoolsTypes("1.1")
+    @Throws(IllegalArgumentException::class)
+    public operator fun invoke(value: Int): T
+}
+
+/** Representation of integers other than zero. */
+@JvmInline
+@SinceKotoolsTypes("1.1")
+public value class NonZeroInt private constructor(override val value: Int) :
+    IntHolder {
+    public companion object : IntHolderCompanion<NonZeroInt> {
+        private val negativeRange: IntRange = StrictlyNegativeInt.range
+        private val positiveRange: IntRange = StrictlyPositiveInt.range
+        private val ranges: Pair<IntRange, IntRange> =
+            negativeRange to positiveRange
+        override val min: NonZeroInt = NonZeroInt(negativeRange.first)
+        override val max: NonZeroInt = NonZeroInt(positiveRange.last)
+        override val random: NonZeroInt
+            get() = ranges.toList()
+                .random()
+                .random()
+                .let(this::invoke)
+
+        override fun invoke(value: Int): NonZeroInt {
+            require(value != 0) { "NonZeroInt doesn't accept 0." }
+            return NonZeroInt(value)
+        }
+    }
+}
+
+/** Representation of positive integers, including zero. */
+@JvmInline
+@SinceKotoolsTypes("1.1")
+public value class PositiveInt private constructor(override val value: Int) :
+    IntHolder {
+    public companion object : IntHolderCompanion<PositiveInt> {
+        private val range: IntRange = 0..Int.MAX_VALUE
+        override val min: PositiveInt = PositiveInt(range.first)
+        override val max: PositiveInt = PositiveInt(range.last)
+        override val random: PositiveInt get() = this(range.random())
+
+        override fun invoke(value: Int): PositiveInt {
+            require(value >= 0) {
+                "PositiveInt doesn't accept strictly negative values (tried " +
+                        "with $value)."
+            }
+            return PositiveInt(value)
+        }
+    }
+}
+
+/** Representation of strictly positive integers, excluding zero. */
+@JvmInline
+@SinceKotoolsTypes("1.1")
+public value class StrictlyPositiveInt private constructor(
+    override val value: Int
+) : IntHolder {
+    public companion object : IntHolderCompanion<StrictlyPositiveInt> {
+        internal val range: IntRange = 1..Int.MAX_VALUE
+        override val min: StrictlyPositiveInt = StrictlyPositiveInt(range.first)
+        override val max: StrictlyPositiveInt = StrictlyPositiveInt(range.last)
+        override val random: StrictlyPositiveInt get() = this(range.random())
+
+        override fun invoke(value: Int): StrictlyPositiveInt {
+            require(value > 0) {
+                "StrictlyPositiveInt doesn't accept negative values (tried " +
+                        "with $value)."
+            }
+            return StrictlyPositiveInt(value)
+        }
+    }
+}
+
+/** Representation of negative integers, including zero. */
+@JvmInline
+@SinceKotoolsTypes("1.1")
+public value class NegativeInt private constructor(override val value: Int) :
+    IntHolder {
+    public companion object : IntHolderCompanion<NegativeInt> {
+        private val range: IntRange = Int.MIN_VALUE..0
+        override val min: NegativeInt = NegativeInt(range.first)
+        override val max: NegativeInt = NegativeInt(range.last)
+        override val random: NegativeInt get() = this(range.random())
+
+        override fun invoke(value: Int): NegativeInt {
+            require(value <= 0) {
+                "NegativeInt doesn't accept strictly positive values (tried " +
+                        "with $value)."
+            }
+            return NegativeInt(value)
+        }
+    }
+}
+
+/** Representation of strictly negative integers, excluding zero. */
+@JvmInline
+@SinceKotoolsTypes("1.1")
+public value class StrictlyNegativeInt private constructor(
+    override val value: Int
+) : IntHolder {
+    public companion object : IntHolderCompanion<StrictlyNegativeInt> {
+        internal val range: IntRange = Int.MIN_VALUE..-1
+        override val min: StrictlyNegativeInt = StrictlyNegativeInt(range.first)
+        override val max: StrictlyNegativeInt = StrictlyNegativeInt(range.last)
+        override val random: StrictlyNegativeInt get() = this(range.random())
+
+        override fun invoke(value: Int): StrictlyNegativeInt {
+            require(value < 0) {
+                "StrictlyNegativeInt doesn't accept positive values (tried " +
+                        "with $value)."
+            }
+            return StrictlyNegativeInt(value)
+        }
+    }
+}
---
+++ src/commonTest/kotlin/kotools/types/number/IntTest.kt
@@ -0,0 +1,153 @@
+package kotools.types.number
+
+import kotools.assert.assertEquals
+import kotools.assert.assertFailsWith
+import kotools.assert.assertNotNull
+import kotlin.test.Test
+
+class NonZeroIntTest {
+    @Test
+    fun companion_min_should_equal_the_minimum_value_of_Int(): Unit =
+        NonZeroInt.min.value assertEquals Int.MIN_VALUE
+
+    @Test
+    fun companion_max_should_equal_the_maximum_value_of_Int(): Unit =
+        NonZeroInt.max.value assertEquals Int.MAX_VALUE
+
+    @Test
+    fun constructor_should_pass_with_an_Int_other_than_zero() {
+        // GIVEN
+        val value: Int = NonZeroInt.random.value
+        // WHEN
+        val result = NonZeroInt(value)
+        // THEN
+        result.value assertEquals value
+    }
+
+    @Test
+    fun constructor_should_throw_an_error_with_an_Int_that_equals_zero() {
+        // GIVEN & WHEN & THEN
+        val error: IllegalArgumentException = assertFailsWith { NonZeroInt(0) }
+        error.message.assertNotNull()
+    }
+}
+
+class PositiveIntTest {
+    @Test
+    fun companion_min_should_equal_zero(): Unit =
+        PositiveInt.min.value assertEquals 0
+
+    @Test
+    fun companion_max_should_equal_the_maximum_value_of_Int(): Unit =
+        PositiveInt.max.value assertEquals Int.MAX_VALUE
+
+    @Test
+    fun constructor_should_pass_with_a_positive_Int() {
+        // GIVEN
+        val value: Int = PositiveInt.random.value
+        // WHEN
+        val result = PositiveInt(value)
+        // THEN
+        result.value assertEquals value
+    }
+
+    @Test
+    fun constructor_should_throw_an_error_with_a_strictly_negative_Int() {
+        // GIVEN
+        val value: Int = StrictlyNegativeInt.random.value
+        // WHEN & THEN
+        val error: IllegalArgumentException =
+            assertFailsWith { PositiveInt(value) }
+        error.message.assertNotNull()
+    }
+}
+
+class StrictlyPositiveIntTest {
+    @Test
+    fun companion_min_should_equal_1(): Unit =
+        StrictlyPositiveInt.min.value assertEquals 1
+
+    @Test
+    fun companion_max_should_equal_the_maximum_value_of_Int(): Unit =
+        StrictlyPositiveInt.max.value assertEquals Int.MAX_VALUE
+
+    @Test
+    fun constructor_should_pass_with_a_strictly_positive_Int() {
+        // GIVEN
+        val value: Int = StrictlyPositiveInt.random.value
+        // WHEN
+        val result = StrictlyPositiveInt(value)
+        // THEN
+        result.value assertEquals value
+    }
+
+    @Test
+    fun constructor_should_throw_an_error_with_a_negative_Int() {
+        // GIVEN
+        val value: Int = NegativeInt.random.value
+        // WHEN & THEN
+        val error: IllegalArgumentException =
+            assertFailsWith { StrictlyPositiveInt(value) }
+        error.message.assertNotNull()
+    }
+}
+
+class NegativeIntTest {
+    @Test
+    fun companion_min_should_equal_the_minimum_value_of_Int(): Unit =
+        NegativeInt.min.value assertEquals Int.MIN_VALUE
+
+    @Test
+    fun companion_max_should_equal_zero(): Unit =
+        NegativeInt.max.value assertEquals 0
+
+    @Test
+    fun constructor_should_pass_with_a_negativeInt() {
+        // GIVEN
+        val value: Int = NegativeInt.random.value
+        // WHEN
+        val result = NegativeInt(value)
+        // THEN
+        result.value assertEquals value
+    }
+
+    @Test
+    fun constructor_should_throw_an_error_with_a_strictly_positive_Int() {
+        // GIVEN
+        val value: Int = StrictlyPositiveInt.random.value
+        // WHEN & THEN
+        val error: IllegalArgumentException =
+            assertFailsWith { NegativeInt(value) }
+        error.message.assertNotNull()
+    }
+}
+
+class StrictlyNegativeIntTest {
+    @Test
+    fun companion_min_should_equal_the_minimum_value_of_Int(): Unit =
+        StrictlyNegativeInt.min.value assertEquals Int.MIN_VALUE
+
+    @Test
+    fun companion_max_should_equal_minus1(): Unit =
+        StrictlyNegativeInt.max.value assertEquals -1
+
+    @Test
+    fun constructor_should_pass_with_a_strictly_negative_Int() {
+        // GIVEN
+        val value: Int = StrictlyNegativeInt.random.value
+        // WHEN
+        val result = StrictlyNegativeInt(value)
+        // THEN
+        result.value assertEquals value
+    }
+
+    @Test
+    fun constructor_should_throw_an_error_with_a_positive_Int() {
+        // GIVEN
+        val value: Int = PositiveInt.random.value
+        // WHEN & THEN
+        val error: IllegalArgumentException =
+            assertFailsWith { StrictlyNegativeInt(value) }
+        error.message.assertNotNull()
+    }
+}
