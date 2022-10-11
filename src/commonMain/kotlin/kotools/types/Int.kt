46cfddd05ca14c49203f09f58850250f8b50258b
Loïc Lamarque <loiclamarque777@gmail.com>
2022-10-11 14:13:03 +0200

    feat(#112): implement a new type system for integers
    For simplifying usages of the library, new types were introduced directly in the "kotools.types" package.

---
+++ src/commonMain/kotlin/kotools/types/Int.kt
@@ -0,0 +1,285 @@
+package kotools.types
+
+import kotools.types.core.SinceKotoolsTypes
+import kotools.types.core.tryOrNull
+
+/**
+ * Compares this value with the [other] value for order.
+ * Returns zero if this value equals the [other] value, a negative number if
+ * this value is less than the [other] value, or a positive number if this value
+ * is greater than the [other] value.
+ */
+@SinceKotoolsTypes("3.0")
+public operator fun Int.compareTo(other: IntHolder): Int =
+    compareTo(other.value)
+
+/** Adds the [other] value to this value. */
+@SinceKotoolsTypes("3.0")
+public operator fun Int.plus(other: IntHolder): Int = plus(other.value)
+
+/** Subtracts the [other] value from this value. */
+@SinceKotoolsTypes("3.0")
+public operator fun Int.minus(other: IntHolder): Int = minus(other.value)
+
+/** Multiplies this value by the [other] value. */
+@SinceKotoolsTypes("3.0")
+public operator fun Int.times(other: IntHolder): Int = times(other.value)
+
+/** Parent of classes responsible for holding integers. */
+@SinceKotoolsTypes("3.0")
+public sealed interface IntHolder : Comparable<IntHolder> {
+    /** The value to hold. */
+    public val value: Int
+
+    /** Returns the negative of this [value]. */
+    public operator fun unaryMinus(): IntHolder
+
+    /**
+     * Compares this [value] with the [other] value for order.
+     * Returns zero if this [value] equals the [other] value, a negative number
+     * if this [value] is less than the [other] value, or a positive number if
+     * this [value] is greater than the [other] value.
+     */
+    public operator fun compareTo(other: Int): Int = value.compareTo(other)
+
+    /**
+     * Compares this [value] with the [other] value for order.
+     * Returns zero if this [value] equals the [other] value, a negative number
+     * if this [value] is less than the [other] value, or a positive number if
+     * this [value] is greater than the [other] value.
+     */
+    override fun compareTo(other: IntHolder): Int = compareTo(other.value)
+
+    /** Adds the [other] value to this [value]. */
+    public operator fun plus(other: Int): Int = value + other
+
+    /** Adds the [other] value to this [value]. */
+    public operator fun plus(other: IntHolder): Int = plus(other.value)
+
+    /** Subtracts the [other] value from this [value]. */
+    public operator fun minus(other: Int): Int = value - other
+
+    /** Subtracts the [other] value from this [value]. */
+    public operator fun minus(other: IntHolder): Int = minus(other.value)
+
+    /** Multiplies this [value] by the [other] value. */
+    public operator fun times(other: Int): Int = value * other
+
+    /** Multiplies this [value] by the [other] value. */
+    public operator fun times(other: IntHolder): Int = times(other.value)
+
+    /**
+     * Divides this [value] by the [other] value, truncating the result to an
+     * integer that is closer to zero.
+     */
+    public operator fun div(other: NonZeroInt): Int = value / other.value
+}
+
+private sealed class AbstractIntHolder : IntHolder {
+    override fun equals(other: Any?): Boolean =
+        other is IntHolder && value == other.value
+
+    override fun hashCode(): Int = 31 * value.hashCode()
+
+    override fun toString(): String = value.toString()
+}
+
+/**
+ * Returns the [value] as a [NonZeroInt], or throws an
+ * [IllegalArgumentException] if the [value] equals zero.
+ */
+@SinceKotoolsTypes("3.0")
+@Throws(IllegalArgumentException::class)
+public fun NonZeroInt(value: Int): NonZeroInt = NonZeroIntImplementation(value)
+
+/**
+ * Returns the [value] as a [NonZeroInt], or returns `null` if the [value]
+ * equals zero.
+ */
+@SinceKotoolsTypes("3.0")
+@Suppress("FunctionName")
+public fun NonZeroIntOrNull(value: Int): NonZeroInt? =
+    tryOrNull { NonZeroInt(value) }
+
+/**
+ * Divides this value by the [other] value, truncating the result to an integer
+ * that is closer to zero.
+ */
+@SinceKotoolsTypes("3.0")
+public operator fun Int.div(other: NonZeroInt): Int = div(other.value)
+
+/** Representation of integers other than zero. */
+@SinceKotoolsTypes("3.0")
+public sealed interface NonZeroInt : IntHolder {
+    override fun unaryMinus(): NonZeroInt = NonZeroInt(-value)
+
+    /** Multiplies this [value] by the [other] value. */
+    public operator fun times(other: NonZeroInt): NonZeroInt =
+        NonZeroInt(value * other.value)
+}
+
+private class NonZeroIntImplementation(override val value: Int) :
+    AbstractIntHolder(),
+    NonZeroInt {
+    init {
+        require(value != 0)
+    }
+}
+
+/**
+ * Returns the [value] as a [PositiveInt], or throws an
+ * [IllegalArgumentException] if the [value] is strictly negative.
+ */
+@SinceKotoolsTypes("3.0")
+@Throws(IllegalArgumentException::class)
+public fun PositiveInt(value: Int): PositiveInt =
+    PositiveIntImplementation(value)
+
+/**
+ * Returns the [value] as a [PositiveInt], or returns `null` if the [value] is
+ * strictly negative.
+ */
+@SinceKotoolsTypes("3.0")
+@Suppress("FunctionName")
+public fun PositiveIntOrNull(value: Int): PositiveInt? =
+    tryOrNull { PositiveInt(value) }
+
+/** Representation of positive integers, including zero. */
+@SinceKotoolsTypes("3.0")
+public sealed interface PositiveInt : IntHolder {
+    override fun unaryMinus(): NegativeInt = NegativeInt(-value)
+
+    /**
+     * Divides this [value] by the [other] value, truncating the result to an
+     * integer that is closer to zero.
+     */
+    public operator fun div(other: StrictlyPositiveInt): PositiveInt =
+        PositiveInt(value / other.value)
+
+    /**
+     * Divides this [value] by the [other] value, truncating the result to an
+     * integer that is closer to zero.
+     */
+    public operator fun div(other: StrictlyNegativeInt): NegativeInt =
+        NegativeInt(value / other.value)
+}
+
+private class PositiveIntImplementation(override val value: Int) :
+    AbstractIntHolder(),
+    PositiveInt {
+    init {
+        require(value >= 0)
+    }
+}
+
+/**
+ * Returns the [value] as a [StrictlyPositiveInt], or throws an
+ * [IllegalArgumentException] if the [value] is negative.
+ */
+@SinceKotoolsTypes("3.0")
+@Throws(IllegalArgumentException::class)
+public fun StrictlyPositiveInt(value: Int): StrictlyPositiveInt =
+    StrictlyPositiveIntImplementation(value)
+
+/**
+ * Returns the [value] as a [StrictlyPositiveInt], or returns `null` if the
+ * [value] is negative.
+ */
+@SinceKotoolsTypes("3.0")
+@Suppress("FunctionName")
+public fun StrictlyPositiveIntOrNull(value: Int): StrictlyPositiveInt? =
+    tryOrNull { StrictlyPositiveInt(value) }
+
+/** Representation of strictly positive integers, excluding zero. */
+@SinceKotoolsTypes("3.0")
+public sealed interface StrictlyPositiveInt : NonZeroInt,
+    PositiveInt {
+    override fun unaryMinus(): StrictlyNegativeInt = StrictlyNegativeInt(-value)
+}
+
+private class StrictlyPositiveIntImplementation(override val value: Int) :
+    AbstractIntHolder(),
+    StrictlyPositiveInt {
+    init {
+        require(value > 0)
+    }
+}
+
+/**
+ * Returns the [value] as a [NegativeInt], or throws an
+ * [IllegalArgumentException] if the [value] is strictly positive.
+ */
+@SinceKotoolsTypes("3.0")
+@Throws(IllegalArgumentException::class)
+public fun NegativeInt(value: Int): NegativeInt =
+    NegativeIntImplementation(value)
+
+/**
+ * Returns the [value] as a [NegativeInt], or returns `null` if the [value] is
+ * strictly positive.
+ */
+@SinceKotoolsTypes("3.0")
+@Suppress("FunctionName")
+public fun NegativeIntOrNull(value: Int): NegativeInt? =
+    tryOrNull { NegativeInt(value) }
+
+/** Representation of negative integers, including zero. */
+@SinceKotoolsTypes("3.0")
+public sealed interface NegativeInt : IntHolder {
+    override fun unaryMinus(): PositiveInt = PositiveInt(-value)
+
+    /**
+     * Divides this [value] by the [other] value, truncating the result to an
+     * integer that is closer to zero.
+     */
+    public operator fun div(other: StrictlyPositiveInt): NegativeInt =
+        NegativeInt(value / other.value)
+
+    /**
+     * Divides this [value] by the [other] value, truncating the result to an
+     * integer that is closer to zero.
+     */
+    public operator fun div(other: StrictlyNegativeInt): PositiveInt =
+        PositiveInt(value / other.value)
+}
+
+private class NegativeIntImplementation(override val value: Int) :
+    AbstractIntHolder(),
+    NegativeInt {
+    init {
+        require(value <= 0)
+    }
+}
+
+/**
+ * Returns the [value] as a [StrictlyNegativeInt], or throws an
+ * [IllegalArgumentException] if the [value] is positive.
+ */
+@SinceKotoolsTypes("3.0")
+@Throws(IllegalArgumentException::class)
+public fun StrictlyNegativeInt(value: Int): StrictlyNegativeInt =
+    StrictlyNegativeIntImplementation(value)
+
+/**
+ * Returns the [value] as a [StrictlyNegativeInt], or returns `null` if the
+ * [value] is positive.
+ */
+@SinceKotoolsTypes("3.0")
+@Suppress("FunctionName")
+public fun StrictlyNegativeIntOrNull(value: Int): StrictlyNegativeInt? =
+    tryOrNull { StrictlyNegativeInt(value) }
+
+/** Representation of strictly negative integers, excluding zero. */
+@SinceKotoolsTypes("3.0")
+public sealed interface StrictlyNegativeInt : NonZeroInt,
+    NegativeInt {
+    override fun unaryMinus(): StrictlyPositiveInt = StrictlyPositiveInt(-value)
+}
+
+private class StrictlyNegativeIntImplementation(override val value: Int) :
+    AbstractIntHolder(),
+    StrictlyNegativeInt {
+    init {
+        require(value < 0)
+    }
+}
---
+++ src/commonTest/kotlin/kotools/types/IntTest.kt
@@ -0,0 +1,372 @@
+package kotools.types
+
+import kotools.assert.assertEquals
+import kotools.assert.assertFailsWith
+import kotools.assert.assertNotNull
+import kotools.assert.assertNull
+import kotools.types.core.RandomValueHolder
+import kotlin.test.Test
+
+private fun randomNonZeroInt(): NonZeroInt =
+    setOf(Int.MIN_VALUE..-1, 1..Int.MAX_VALUE).random() random ::NonZeroInt
+
+private inline infix fun <T : IntHolder> IntRange.random(
+    builder: (Int) -> T
+): T = random().let(builder)
+
+private fun randomPositiveInt(): PositiveInt =
+    0..Int.MAX_VALUE random ::PositiveInt
+
+private fun randomStrictlyPositiveInt(): StrictlyPositiveInt =
+    1..Int.MAX_VALUE random ::StrictlyPositiveInt
+
+private fun randomNegativeInt(): NegativeInt =
+    Int.MIN_VALUE..0 random ::NegativeInt
+
+private fun randomStrictlyNegativeInt(): StrictlyNegativeInt =
+    Int.MIN_VALUE..-1 random ::StrictlyNegativeInt
+
+class IntHolderTest : RandomValueHolder {
+    private val randomHolders: Set<IntHolder> = setOf(
+        randomNonZeroInt(),
+        randomPositiveInt(),
+        randomStrictlyPositiveInt(),
+        randomNegativeInt(),
+        randomStrictlyNegativeInt()
+    )
+
+    @Test
+    fun compareTo_should_pass_when_comparing_an_Int_with_an_IntHolder() {
+        val x: Int = randomInt
+        val y: IntHolder = randomHolders.random()
+        val result: Int = x.compareTo(y)
+        result assertEquals x.compareTo(y.value)
+    }
+
+    @Test
+    fun compareTo_should_pass_with_an_Int() {
+        val x: IntHolder = randomHolders.random()
+        val y: Int = randomInt
+        val result: Int = x.compareTo(y)
+        result assertEquals x.value.compareTo(y)
+    }
+
+    @Test
+    fun compareTo_should_pass_with_an_IntHolder() {
+        val x: IntHolder = randomHolders.random()
+        val y: IntHolder = randomHolders.random()
+        val result: Int = x.compareTo(y)
+        result assertEquals x.value.compareTo(y.value)
+    }
+
+    @Test
+    fun plus_should_pass_when_adding_an_IntHolder_to_an_Int() {
+        val x: Int = randomInt
+        val y: IntHolder = randomHolders.random()
+        val result: Int = x + y
+        result assertEquals x + y.value
+    }
+
+    @Test
+    fun plus_should_pass_with_an_Int() {
+        val x: IntHolder = randomHolders.random()
+        val y: Int = randomInt
+        val result: Int = x + y
+        result assertEquals x.value + y
+    }
+
+    @Test
+    fun plus_should_pass_with_an_IntHolder() {
+        val x: IntHolder = randomHolders.random()
+        val y: IntHolder = randomHolders.random()
+        val result: Int = x + y
+        result assertEquals x.value + y.value
+    }
+
+    @Test
+    fun minus_should_pass_when_subtracting_an_IntHolder_from_an_Int() {
+        val x: Int = randomInt
+        val y: IntHolder = randomHolders.random()
+        val result: Int = x - y
+        result assertEquals x - y.value
+    }
+
+    @Test
+    fun minus_should_pass_with_an_Int() {
+        val x: IntHolder = randomHolders.random()
+        val y: Int = randomInt
+        val result: Int = x - y
+        result assertEquals x.value - y
+    }
+
+    @Test
+    fun minus_should_pass_with_an_IntHolder() {
+        val x: IntHolder = randomHolders.random()
+        val y: IntHolder = randomHolders.random()
+        val result: Int = x - y
+        result assertEquals x.value - y.value
+    }
+
+    @Test
+    fun times_should_pass_when_multiplying_an_Int_by_an_IntHolder() {
+        val x: Int = randomInt
+        val y: IntHolder = randomHolders.random()
+        val result: Int = x * y
+        result assertEquals x * y.value
+    }
+
+    @Test
+    fun times_should_pass_with_an_Int() {
+        val x: IntHolder = randomHolders.random()
+        val y: Int = randomInt
+        val result: Int = x * y
+        result assertEquals x.value * y
+    }
+
+    @Test
+    fun times_should_pass_with_an_IntHolder() {
+        val x: IntHolder = randomHolders.random()
+        val y: IntHolder = randomHolders.random()
+        val result: Int = x * y
+        result assertEquals x.value * y.value
+    }
+
+    @Test
+    fun div_should_pass_with_a_NonZeroInt() {
+        val x: IntHolder = randomHolders.random()
+        val y: NonZeroInt = randomNonZeroInt()
+        val result: Int = x / y
+        result assertEquals x.value / y.value
+    }
+}
+
+@Suppress("TestFunctionName")
+class NonZeroIntTest : RandomValueHolder {
+    @Test
+    fun NonZeroInt_should_pass_with_a_non_zero_Int() {
+        val value: Int = randomNonZeroInt().value
+        val result = NonZeroInt(value)
+        result.value assertEquals value
+    }
+
+    @Test
+    fun NonZeroInt_should_throw_an_error_with_an_Int_that_equals_zero() {
+        assertFailsWith<IllegalArgumentException> { NonZeroInt(0) }
+    }
+
+    @Test
+    fun NonZeroIntOrNull_should_pass_with_a_non_zero_Int() {
+        val value: Int = randomNonZeroInt().value
+        val result: NonZeroInt? = NonZeroIntOrNull(value)
+        result.assertNotNull().value assertEquals value
+    }
+
+    @Test
+    fun NonZeroIntOrNull_should_return_null_with_an_Int_that_equals_zero() {
+        val result: NonZeroInt? = NonZeroIntOrNull(0)
+        result.assertNull()
+    }
+
+    @Test
+    fun unaryMinus_should_pass() {
+        val x: NonZeroInt = randomNonZeroInt()
+        val result: NonZeroInt = -x
+        result.value assertEquals -x.value
+    }
+
+    @Test
+    fun times_should_pass_with_a_NonZeroInt() {
+        val x: NonZeroInt = randomNonZeroInt()
+        val y: NonZeroInt = randomNonZeroInt()
+        val result: NonZeroInt = x * y
+        result.value assertEquals x.value * y.value
+    }
+
+    @Test
+    fun div_should_pass_when_dividing_an_Int_by_a_NonZeroInt() {
+        val x: Int = randomInt
+        val y: NonZeroInt = randomNonZeroInt()
+        val result: Int = x / y
+        result assertEquals x / y.value
+    }
+}
+
+@Suppress("TestFunctionName")
+class PositiveIntTest {
+    @Test
+    fun PositiveInt_should_pass_with_a_positive_Int() {
+        val value: Int = randomPositiveInt().value
+        val result = PositiveInt(value)
+        result.value assertEquals value
+    }
+
+    @Test
+    fun PositiveInt_should_throw_an_error_with_a_strictly_negative_Int() {
+        val value: Int = randomStrictlyNegativeInt().value
+        assertFailsWith<IllegalArgumentException> { PositiveInt(value) }
+    }
+
+    @Test
+    fun PositiveIntOrNull_should_pass_with_a_positive_Int() {
+        val value: Int = randomPositiveInt().value
+        val result: PositiveInt? = PositiveIntOrNull(value)
+        result.assertNotNull().value assertEquals value
+    }
+
+    @Test
+    fun PositiveIntOrNull_should_return_null_with_a_strictly_negative_Int() {
+        val value: Int = randomStrictlyNegativeInt().value
+        val result: PositiveInt? = PositiveIntOrNull(value)
+        result.assertNull()
+    }
+
+    @Test
+    fun unaryMinus_should_pass() {
+        val x: PositiveInt = randomPositiveInt()
+        val result: NegativeInt = -x
+        result.value assertEquals -x.value
+    }
+
+    @Test
+    fun div_should_pass_with_a_StrictlyPositiveInt() {
+        val x: PositiveInt = randomPositiveInt()
+        val y: StrictlyPositiveInt = randomStrictlyPositiveInt()
+        val result: PositiveInt = x / y
+        result.value assertEquals x.value / y.value
+    }
+
+    @Test
+    fun div_should_pass_with_a_StrictlyNegativeInt() {
+        val x: PositiveInt = randomPositiveInt()
+        val y: StrictlyNegativeInt = randomStrictlyNegativeInt()
+        val result: NegativeInt = x / y
+        result.value assertEquals x.value / y.value
+    }
+}
+
+@Suppress("TestFunctionName")
+class StrictlyPositiveIntTest {
+    @Test
+    fun StrictlyPositiveInt_should_pass_with_a_strictly_positive_Int() {
+        val value: Int = randomStrictlyPositiveInt().value
+        val result = StrictlyPositiveInt(value)
+        result.value assertEquals value
+    }
+
+    @Test
+    fun StrictlyPositiveInt_should_throw_an_error_with_a_negative_Int() {
+        val value: Int = randomNegativeInt().value
+        assertFailsWith<IllegalArgumentException> { StrictlyPositiveInt(value) }
+    }
+
+    @Test
+    fun StrictlyPositiveIntOrNull_should_pass_with_a_strictly_positive_Int() {
+        val value: Int = randomStrictlyPositiveInt().value
+        val result: StrictlyPositiveInt? = StrictlyPositiveIntOrNull(value)
+        result.assertNotNull().value assertEquals value
+    }
+
+    @Test
+    fun StrictlyPositiveIntOrNull_should_return_null_with_a_negative_Int() {
+        val value: Int = randomNegativeInt().value
+        val result: StrictlyPositiveInt? = StrictlyPositiveIntOrNull(value)
+        result.assertNull()
+    }
+
+    @Test
+    fun unaryMinus_should_pass() {
+        val x: StrictlyPositiveInt = randomStrictlyPositiveInt()
+        val result: StrictlyNegativeInt = -x
+        result.value assertEquals -x.value
+    }
+}
+
+@Suppress("TestFunctionName")
+class NegativeIntTest {
+    @Test
+    fun NegativeInt_should_pass_with_a_negative_Int() {
+        val value: Int = randomNegativeInt().value
+        val result = NegativeInt(value)
+        result.value assertEquals value
+    }
+
+    @Test
+    fun NegativeInt_should_throw_an_error_with_a_strictly_positive_Int() {
+        val value: Int = randomStrictlyPositiveInt().value
+        assertFailsWith<IllegalArgumentException> { NegativeInt(value) }
+    }
+
+    @Test
+    fun NegativeIntOrNull_should_pass_with_a_negative_Int() {
+        val value: Int = randomNegativeInt().value
+        val result: NegativeInt? = NegativeIntOrNull(value)
+        result.assertNotNull().value assertEquals value
+    }
+
+    @Test
+    fun NegativeIntOrNull_should_return_null_with_a_strictly_positive_Int() {
+        val value: Int = randomStrictlyPositiveInt().value
+        val result: NegativeInt? = NegativeIntOrNull(value)
+        result.assertNull()
+    }
+
+    @Test
+    fun unaryMinus_should_pass() {
+        val x: NegativeInt = randomNegativeInt()
+        val result: PositiveInt = -x
+        result.value assertEquals -x.value
+    }
+
+    @Test
+    fun div_should_pass_with_a_StrictlyPositiveInt() {
+        val x: NegativeInt = randomNegativeInt()
+        val y: StrictlyPositiveInt = randomStrictlyPositiveInt()
+        val result: NegativeInt = x / y
+        result.value assertEquals x.value / y.value
+    }
+
+    @Test
+    fun div_should_pass_with_a_StrictlyNegativeInt() {
+        val x: NegativeInt = randomNegativeInt()
+        val y: StrictlyNegativeInt = randomStrictlyNegativeInt()
+        val result: PositiveInt = x / y
+        result.value assertEquals x.value / y.value
+    }
+}
+
+@Suppress("TestFunctionName")
+class StrictlyNegativeIntTest {
+    @Test
+    fun StrictlyNegativeInt_should_pass_with_a_strictly_negative_Int() {
+        val value: Int = randomStrictlyNegativeInt().value
+        val result = StrictlyNegativeInt(value)
+        result.value assertEquals value
+    }
+
+    @Test
+    fun StrictlyNegativeInt_should_throw_an_error_with_a_positive_Int() {
+        val value: Int = randomPositiveInt().value
+        assertFailsWith<IllegalArgumentException> { StrictlyNegativeInt(value) }
+    }
+
+    @Test
+    fun StrictlyNegativeIntOrNull_should_pass_with_a_strictly_negative_Int() {
+        val value: Int = randomStrictlyNegativeInt().value
+        val result: StrictlyNegativeInt? = StrictlyNegativeIntOrNull(value)
+        result.assertNotNull().value assertEquals value
+    }
+
+    @Test
+    fun StrictlyNegativeIntOrNull_should_return_null_with_a_positive_Int() {
+        val value: Int = randomPositiveInt().value
+        val result: StrictlyNegativeInt? = StrictlyNegativeIntOrNull(value)
+        result.assertNull()
+    }
+
+    @Test
+    fun unaryMinus_should_pass() {
+        val x: StrictlyNegativeInt = randomStrictlyNegativeInt()
+        val result: StrictlyPositiveInt = -x
+        result.value assertEquals -x.value
+    }
+}
