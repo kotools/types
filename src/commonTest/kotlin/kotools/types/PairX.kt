96e3b9a511dddf26143be649ed06e381097c4a36
Loïc Lamarque <loiclamarque777@gmail.com>
2022-10-21 12:05:22 +0200

    test(#125): refactor tests of NotEmptyList's builders

--- src/commonTest/kotlin/kotools/types/PairX.kt
+++ src/commonTest/kotlin/kotools/types/PairX.kt
@@ -3,11 +3,15 @@ package kotools.types
 import kotools.assert.assertEquals
 import kotools.assert.assertNotNull
 import kotools.assert.assertNull
+import kotools.types.number.PositiveInt
 
 // ---------- Builders ----------
 
 inline fun <A, B> A.pairBy(block: (A) -> B): Pair<B, A> = block(this) to this
 inline fun <A, B> A.runPairBy(block: A.() -> B): Pair<B, A> = pairBy(block)
+
+inline fun <A, B> A.pairWith(block: (A) -> B): Pair<A, B> = this to block(this)
+inline fun <A, B> A.runPairWith(block: A.() -> B): Pair<A, B> = pairWith(block)
 
 // ---------- Assertions ----------
 
@@ -41,3 +45,32 @@ inline fun <A, B, C> Pair<A, B>.mapSecond(block: (B) -> C): Pair<A, C> =
 
 inline fun <A, B, C> Pair<A, B>.runMapSecond(block: B.() -> C): Pair<A, C> =
     mapSecond(block)
+
+// ---------- Reducers ----------
+
+fun <A, B> Pair<Collection<A>, Collection<B>>.toPairs(): List<Pair<A?, B?>> {
+    val firstCollection: Collection<A?> = first
+        .takeIf { it.size >= second.size }
+        ?: mutableListOf<A?>().apply {
+            addAll(first)
+            repeat(second.size - first.size) { add(null) }
+        }
+    val secondCollection: Collection<B?> = second
+        .takeIf { it.size >= firstCollection.size }
+        ?: mutableListOf<B?>().apply {
+            addAll(second)
+            repeat(firstCollection.size - second.size) { add(null) }
+        }
+    return firstCollection.mapIndexed { index: Int, element: A? ->
+        element to secondCollection.elementAt(index)
+    }
+}
+
+// ---------- Iterations ----------
+
+inline fun <A, B, C : Collection<B>> Pair<A, C>.forEachSecondIndexed(
+    action: (PositiveInt, B, A) -> Unit
+): Unit = second.forEachIndexed { index: Int, element: B ->
+    val positiveIndex = PositiveInt(index)
+    action(positiveIndex, element, first)
+}
--- src/commonTest/kotlin/kotools/types/collections/NotEmptyListTest.kt
+++ src/commonTest/kotlin/kotools/types/collections/NotEmptyListTest.kt
@@ -4,33 +4,28 @@ import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotools.assert.*
+import kotools.types.*
 import kotools.types.core.RandomValueHolder
+import kotools.types.number.PositiveInt
 import kotools.types.number.toPositiveInt
 import kotlin.test.Test
 
 class NotEmptyListTest : RandomValueHolder {
     // ---------- Builders ----------
+    @Test
+    fun notEmptyListOf_should_pass(): Unit = randomInt
+        .pairWith { arrayOf(randomInt) }
+        .runPairWith { notEmptyListOf(first, *second) }
+        .runMapFirst { listOf(first) + second }
+        .toPairs()
+        .forEach(Pair<Int?, Int?>::assertEquals)
 
     @Test
-    fun notEmptyListOf_should_pass() {
-        val head = 1
-        val tail: Array<Int> = arrayOf(2, 3)
-        val result: NotEmptyList<Int> = notEmptyListOf(head, *tail)
-        result.head assertEquals head
-        val expectedResult: List<Int> = listOf(head) + tail
-        result.forEachIndexed { index: Int, element: Int ->
-            element assertEquals expectedResult[index]
-        }
-    }
-
-    @Test
-    fun collection_toNotEmptyList_should_pass_with_a_not_empty_Collection() {
-        val collection: Collection<Int> = listOf(1, 2, 3)
-        val result: NotEmptyList<Int> = collection.toNotEmptyList()
-        result.forEachIndexed { index: Int, element: Int ->
-            element assertEquals collection.elementAt(index)
-        }
-    }
+    fun collection_toNotEmptyList_should_pass_with_a_not_empty_Collection() =
+        setOf(randomInt, randomInt, randomInt)
+            .pairWith(Collection<Int>::toNotEmptyList)
+            .toPairs()
+            .forEach(Pair<Int?, Int?>::assertEquals)
 
     @Test
     fun collection_toNotEmptyList_should_throw_an_error_with_an_empty_Collection() {
@@ -39,33 +34,34 @@ class NotEmptyListTest : RandomValueHolder {
     }
 
     @Test
-    fun collection_toNotEmptyListOrNull_should_pass_with_a_not_empty_Collection() {
-        val collection: Collection<Int> = listOf(1, 2, 3)
-        val result: NotEmptyList<Int>? = collection.toNotEmptyListOrNull()
-        result.assertNotNull().forEachIndexed { index: Int, element: Int ->
-            element assertEquals collection.elementAt(index)
-        }
-    }
+    fun collection_toNotEmptyListOrNull_should_pass_with_a_not_empty_Collection() =
+        setOf(randomString, randomString, randomString)
+            .pairWith(Collection<String>::toNotEmptyListOrNull)
+            .mapSecond(NotEmptyList<String>?::assertNotNull)
+            .toPairs()
+            .forEach(Pair<String?, String?>::assertEquals)
 
     @Test
-    fun collection_toNotEmptyListOrNull_should_return_null_with_an_empty_Collection() {
-        val collection: Collection<Int> = emptyList()
-        collection.toNotEmptyListOrNull().assertNull()
-    }
+    fun collection_toNotEmptyListOrNull_should_return_null_with_an_empty_Collection() =
+        emptyList<String>()
+            .toNotEmptyListOrNull()
+            .assertNull()
 
     @Test
-    fun collection_toNotEmptyListOrElse_should_pass_with_a_not_empty_Collection() {
-        val collection: Collection<Int> = listOf(1, 2, 3)
-        val defaultValue: NotEmptyList<Int> = notEmptyListOf(-1, -2, -3)
-        val result: NotEmptyList<Int> =
-            collection toNotEmptyListOrElse { defaultValue }
-        result.forEachIndexed { index: Int, element: Int ->
-            element assertEquals collection.elementAt(index)
-            index.toPositiveInt()
-                .let(defaultValue::get)
-                .assertNotEquals(element)
-        }
-    }
+    fun collection_toNotEmptyListOrElse_should_pass_with_a_not_empty_Collection() =
+        setOf(randomInt, randomInt, randomInt)
+            .runPairWith { map(Int::unaryMinus) }
+            .mapSecond(List<Int>::toNotEmptyList)
+            .runPairWith {
+                first toNotEmptyListOrElse { second }
+            }
+            .forEachSecondIndexed { index: PositiveInt,
+                                    element: Int,
+                                    (initialCollection: Collection<Int>,
+                                        defaultValue: NotEmptyList<Int>) ->
+                element assertEquals initialCollection.elementAt(index.value)
+                element assertNotEquals defaultValue[index]
+            }
 
     @Test
     fun collection_toNotEmptyListOrElse_should_return_the_default_value_with_an_empty_Collection() {
