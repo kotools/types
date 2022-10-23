cde4c02896040e8fd9d7d658f22d427bf2828333
Loïc Lamarque <loiclamarque777@gmail.com>
2022-10-23 18:32:30 +0200

    feat(#130): update the DSL for IntHolder types

--- src/commonMain/kotlin/kotools/types/experimental/IntHolderDsl.kt
+++ src/commonMain/kotlin/kotools/types/experimental/IntHolderDsl.kt
@@ -5,79 +5,71 @@ import kotools.types.StabilityLevel
 import kotools.types.number.*
 import kotools.types.tryOrNull
 
+/** Context responsible for building an [IntHolder]. */
 @ExperimentalKotoolsTypesApi
-internal sealed interface IntHolderDsl<out T : IntHolder> {
+@SinceKotoolsTypes("3.1", StabilityLevel.Experimental)
+public sealed interface IntHolderDsl<out T : IntHolder> {
     /**
      * Returns the [value] as a type [T], or throws an
      * [IllegalArgumentException] if it fails.
      */
     @Throws(IllegalArgumentException::class)
-    infix fun int(value: Int): T
+    public infix fun int(value: Int): T
 
     /** Returns the [value] as a type [T], or returns `null` if it fails. */
-    infix fun intOrNull(value: Int): T? = tryOrNull { int(value) }
+    public infix fun intOrNull(value: Int): T? = tryOrNull { int(value) }
 }
 
-/** Holds the context responsible for building a non-zero number. */
+/** Context responsible for building a [NonZeroInt]. */
 @ExperimentalKotoolsTypesApi
 @SinceKotoolsTypes("3.0", StabilityLevel.Experimental)
-public val nonZero: NonZeroHolderDsl = NonZeroHolderDsl
+public val nonZero: IntHolderDsl<NonZeroInt> = NonZeroHolderDsl
 
-/** Context responsible for building a non-zero number. */
 @ExperimentalKotoolsTypesApi
-@SinceKotoolsTypes("3.0", StabilityLevel.Experimental)
-public object NonZeroHolderDsl : IntHolderDsl<NonZeroInt> {
+private object NonZeroHolderDsl : IntHolderDsl<NonZeroInt> {
     override fun int(value: Int): NonZeroInt = value.toNonZeroInt()
 }
 
-/** Context responsible for building a positive number. */
+/** Context responsible for building a [PositiveInt]. */
 @ExperimentalKotoolsTypesApi
 @SinceKotoolsTypes("3.0", StabilityLevel.Experimental)
-public val positive: PositiveHolderDsl = PositiveHolderDsl
+public val positive: IntHolderDsl<PositiveInt> = PositiveHolderDsl
 
-/** Context responsible for building a positive number. */
 @ExperimentalKotoolsTypesApi
-@SinceKotoolsTypes("3.0", StabilityLevel.Experimental)
-public object PositiveHolderDsl : IntHolderDsl<PositiveInt> {
+private object PositiveHolderDsl : IntHolderDsl<PositiveInt> {
     override fun int(value: Int): PositiveInt = value.toPositiveInt()
 }
 
-/** Context responsible for building a strictly positive number. */
+/** Context responsible for building a [StrictlyPositiveInt]. */
 @ExperimentalKotoolsTypesApi
 @SinceKotoolsTypes("3.0", StabilityLevel.Experimental)
-public val strictlyPositive: StrictlyPositiveHolderDsl =
+public val strictlyPositive: IntHolderDsl<StrictlyPositiveInt> =
     StrictlyPositiveHolderDsl
 
-/** Context responsible for building a strictly positive number. */
 @ExperimentalKotoolsTypesApi
-@SinceKotoolsTypes("3.0", StabilityLevel.Experimental)
-public object StrictlyPositiveHolderDsl : IntHolderDsl<StrictlyPositiveInt> {
+private object StrictlyPositiveHolderDsl : IntHolderDsl<StrictlyPositiveInt> {
     override fun int(value: Int): StrictlyPositiveInt =
         value.toStrictlyPositiveInt()
 }
 
-/** Context responsible for building a negative number. */
+/** Context responsible for building a [NegativeInt]. */
 @ExperimentalKotoolsTypesApi
 @SinceKotoolsTypes("3.0", StabilityLevel.Experimental)
-public val negative: NegativeHolderDsl = NegativeHolderDsl
+public val negative: IntHolderDsl<NegativeInt> = NegativeHolderDsl
 
-/** Context responsible for building a negative number. */
 @ExperimentalKotoolsTypesApi
-@SinceKotoolsTypes("3.0", StabilityLevel.Experimental)
-public object NegativeHolderDsl : IntHolderDsl<NegativeInt> {
+private object NegativeHolderDsl : IntHolderDsl<NegativeInt> {
     override fun int(value: Int): NegativeInt = value.toNegativeInt()
 }
 
-/** Context responsible for building a strictly negative number. */
+/** Context responsible for building a [StrictlyNegativeInt]. */
 @ExperimentalKotoolsTypesApi
 @SinceKotoolsTypes("3.0", StabilityLevel.Experimental)
-public val strictlyNegative: StrictlyNegativeHolderDsl =
+public val strictlyNegative: IntHolderDsl<StrictlyNegativeInt> =
     StrictlyNegativeHolderDsl
 
-/** Context responsible for building a strictly negative number. */
 @ExperimentalKotoolsTypesApi
-@SinceKotoolsTypes("3.0", StabilityLevel.Experimental)
-public object StrictlyNegativeHolderDsl : IntHolderDsl<StrictlyNegativeInt> {
+private object StrictlyNegativeHolderDsl : IntHolderDsl<StrictlyNegativeInt> {
     override fun int(value: Int): StrictlyNegativeInt =
         value.toStrictlyNegativeInt()
 }
