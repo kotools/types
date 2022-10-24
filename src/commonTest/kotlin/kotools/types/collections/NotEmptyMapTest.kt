88e558cdee8b5448013eaea5e25019050b92f8b6
Loïc Lamarque <loiclamarque777@gmail.com>
2022-10-24 21:25:54 +0200

    feat(#120): introduce a new NotEmptyMap type

---
+++ src/commonTest/kotlin/kotools/types/collections/NotEmptyMapTest.kt
@@ -0,0 +1,52 @@
+package kotools.types.collections
+
+import kotools.assert.assertEquals
+import kotools.assert.assertFailsWith
+import kotools.types.core.RandomValueHolder
+import kotlin.test.Test
+
+class NotEmptyMapTest : RandomValueHolder {
+    // ---------- Builders ----------
+
+    @Test
+    fun notEmptyMapOf_should_pass() {
+        // GIVEN
+        val head: Pair<Int, String> = randomInt to randomString
+        val tail: Array<Pair<Int, String>> = arrayOf(randomInt to randomString)
+        // WHEN
+        val result: NotEmptyMap<Int, String> = notEmptyMapOf(head, *tail)
+        // THEN
+        val expectedResult: Map<Int, String> = mutableMapOf(head)
+            .apply { putAll(tail) }
+        result.size assertEquals expectedResult.size
+        result.head assertEquals head
+        val expectedEntries: Set<Map.Entry<Int, String>> =
+            expectedResult.entries
+        result.entries.forEachIndexed { index: Int,
+                                        entry: Map.Entry<Int, String> ->
+            val expectedEntry: Map.Entry<Int, String> =
+                expectedEntries.elementAt(index)
+            entry.key assertEquals expectedEntry.key
+            entry.value assertEquals expectedEntry.value
+        }
+    }
+
+    @Test
+    fun map_toNotEmptyMap_should_pass_with_a_not_empty_Map() {
+        // GIVEN
+        val map: Map<Int, String> = mapOf(randomInt to randomString)
+        // WHEN
+        val result: NotEmptyMap<Int, String> = map.toNotEmptyMap()
+        // THEN
+        result.size assertEquals map.size
+        result.head assertEquals map.entries.first().toPair()
+    }
+
+    @Test
+    fun map_toNotEmptyMap_should_throw_an_error_with_an_empty_Map() {
+        // GIVEN
+        val map: Map<Int, String> = emptyMap()
+        // WHEN & THEN
+        assertFailsWith<NotEmptyMap.ConstructionError>(map::toNotEmptyMap)
+    }
+}
