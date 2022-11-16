177936dd965d629d86a5994190db5c1587fec822
Loïc Lamarque <loiclamarque777@gmail.com>
2022-11-16 20:11:10 +0100

    feat(csv-#47)!: update NotBlankString.csv function

--- csv/src/main/kotlin/kotools/csv/Path.kt
+++ csv/src/main/kotlin/kotools/csv/Path.kt
@@ -3,17 +3,62 @@ package kotools.csv
 import kotools.types.string.NotBlankString
 import kotools.types.string.toNotBlankString
 
-/** Returns this string as a [CsvPath] suffixed with a `.csv` extension. */
-@SinceKotoolsCsv("2.3")
-public fun NotBlankString.csv(): CsvPath {
-    val extension = ".csv"
-    val path: NotBlankString = takeIf { it.value.endsWith(extension) }
-        ?: "$this$extension".toNotBlankString()
-    return CsvPath(path)
-}
+internal const val CSV_EXTENSION: String = ".csv"
 
-/** Representation of a path pointing to a CSV file. */
-@JvmInline
+/**
+ * Returns this string as a [CSV path][CsvPathResult.Success] suffixed with the
+ * `.csv` extension, or returns a [CsvPathResult.Exception.CsvExtensionAsPath]
+ * if this string equals the `.csv` extension.
+ */
 @SinceKotoolsCsv("2.3")
-public value class CsvPath
-internal constructor(internal val path: NotBlankString)
+public fun NotBlankString.csv(): CsvPathResult.FromNotBlankString =
+    if (value == CSV_EXTENSION) CsvPathResult.Exception.CsvExtensionAsPath
+    else {
+        val path: NotBlankString = takeIf { it.value.endsWith(CSV_EXTENSION) }
+            ?: "$this$CSV_EXTENSION".toNotBlankString()
+        CsvPathResult.Success(path)
+    }
+
+/** Object returned when trying to build a [CSV path][CsvPathResult.Success]. */
+@SinceKotoolsCsv("2.3")
+public sealed interface CsvPathResult {
+    /**
+     * Object returned when trying to build a [CSV path][Success] from a
+     * [NotBlankString].
+     */
+    public sealed interface FromNotBlankString : CsvPathResult
+
+    /**
+     * Object returned when trying to build a [CSV path][Success] from a
+     * [String].
+     */
+    public sealed interface FromString : CsvPathResult
+
+    /** Representation of a path pointing to a CSV file. */
+    @JvmInline
+    public value class Success
+    internal constructor(internal val path: NotBlankString) :
+        FromNotBlankString,
+        FromString
+
+    /** Object returned when building a [CSV path][Success] fails. */
+    public sealed class Exception(reason: String) :
+        IllegalArgumentException("CSV path shouldn't $reason."),
+        CsvPathResult {
+        /**
+         * Exception returned when trying to build a [CSV path][Success] from a
+         * blank string.
+         */
+        public object BlankString : Exception("be blank"),
+            FromString
+
+        /**
+         * Exception returned when trying to build a [CSV path][Success] from a
+         * string that equals the `.csv` extension.
+         */
+        public object CsvExtensionAsPath :
+            Exception("equal the \"$CSV_EXTENSION\" extension"),
+            FromNotBlankString,
+            FromString
+    }
+}
--- csv/src/test/kotlin/kotools/csv/PathTest.kt
+++ csv/src/test/kotlin/kotools/csv/PathTest.kt
@@ -1,27 +1,52 @@
 package kotools.csv
 
 import kotools.assert.assertEquals
+import kotools.assert.assertNotNull
+import kotools.assert.assertTrue
 import kotools.types.string.NotBlankString
 import kotlin.test.Test
+import kotlin.test.fail
+
+private inline fun CsvPathResult.onException(
+    action: (CsvPathResult.Exception) -> CsvPathResult.Success
+): CsvPathResult.Success = when (this) {
+    is CsvPathResult.Success -> this
+    is CsvPathResult.Exception -> action(this)
+}
 
 class CsvPathTest {
     @Test
-    fun notBlankString_csv_should_pass_with_a_string_not_suffixed_by_the_CSV_extension() {
+    fun csv_should_pass_with_a_NotBlankString_not_suffixed_by_the_CSV_extension() {
         // GIVEN
         val fileName = NotBlankString("file")
-        // WHEN
-        val result: CsvPath = fileName.csv()
-        // THEN
-        result.path.value assertEquals "$fileName.csv"
+        // WHEN & THEN
+        fileName.csv()
+            .onException { fail(it.message, it) }
+            .path.value assertEquals "$fileName$CSV_EXTENSION"
     }
 
     @Test
-    fun notBlankString_csv_should_pass_with_a_string_suffixed_by_the_CSV_extension() {
+    fun csv_should_pass_with_a_NotBlankString_suffixed_by_the_CSV_extension() {
         // GIVEN
-        val fileName = NotBlankString("file.csv")
-        // WHEN
-        val result: CsvPath = fileName.csv()
-        // THEN
-        result.path.value assertEquals fileName.value
+        val fileName = NotBlankString("file$CSV_EXTENSION")
+        // WHEN & THEN
+        fileName.csv()
+            .onException { fail(it.message, it) }
+            .path.value assertEquals fileName.value
+    }
+
+    @Test
+    fun csv_should_fail_with_a_NotBlankString_that_equals_the_CSV_extension() {
+        // GIVEN
+        val fileName = NotBlankString(CSV_EXTENSION)
+        // WHEN & THEN
+        when (val result: CsvPathResult.FromNotBlankString = fileName.csv()) {
+            is CsvPathResult.Exception.CsvExtensionAsPath -> result.message
+                .assertNotNull()
+                .isNotBlank()
+                .assertTrue()
+            is CsvPathResult.Exception -> fail(result.message, result)
+            else -> fail()
+        }
     }
 }
