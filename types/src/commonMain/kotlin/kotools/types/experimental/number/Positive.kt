d6244a3f75d8c500869570d2c2b1eeb30042de8a
Loïc Lamarque <loiclamarque777@gmail.com>
2022-11-23 23:53:10 +0100

    refactor(types-#67): reorganize the experimental number API

--- types/src/commonMain/kotlin/kotools/types/experimental/number/Positive.kt
+++ types/src/commonMain/kotlin/kotools/types/experimental/number/Positive.kt
@@ -4,6 +4,10 @@ import kotools.shared.Project
 import kotools.shared.SinceKotools
 import kotools.shared.StabilityLevel
 import kotools.types.experimental.ExperimentalKotoolsTypesApi
+import kotools.types.toSuccessfulResult
+import kotlin.jvm.JvmInline
+
+// ---------- Number ----------
 
 /**
  * Representation of positive numbers.
@@ -18,3 +22,32 @@ public interface PositiveNumber<out N : Number> : ExplicitNumber<N> {
         "Given value should be positive (tried with $value)."
     )
 }
+
+// ---------- Int ----------
+
+/** Representation of positive integers, including zero. */
+@ExperimentalKotoolsTypesApi
+@JvmInline
+@SinceKotools(Project.Types, "1.1", StabilityLevel.Experimental)
+public value class PositiveInt private constructor(override val value: Int) :
+    PositiveNumber<Int> {
+    /** Contains declarations for holding or building a [PositiveInt]. */
+    public companion object {
+        internal infix fun of(value: Int): Result<PositiveInt> = value
+            .takeIf { it >= 0 }
+            ?.let(::PositiveInt)
+            ?.toSuccessfulResult()
+            ?: Result.failure(PositiveNumber.Exception(value))
+    }
+}
+
+@ExperimentalKotoolsTypesApi
+internal val PositiveInt.Companion.range: IntRange by lazy { 0..Int.MAX_VALUE }
+
+/**
+ * Returns this value as a [PositiveInt], or a [PositiveNumber.Exception] if
+ * this value is strictly negative.
+ */
+@ExperimentalKotoolsTypesApi
+@SinceKotools(Project.Types, "3.2", StabilityLevel.Experimental)
+public val Int.positive: Result<PositiveInt> get() = PositiveInt of this
