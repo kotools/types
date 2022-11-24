b0985ceeb03518de76ac9cc9b038aa6b29a9f9cb
Loïc Lamarque <loiclamarque777@gmail.com>
2022-11-24 16:20:44 +0100

    feat(types-#67): design an experimental API for explicit numbers

---
+++ types/src/commonMain/kotlin/kotools/types/experimental/ExplicitNumber.kt
@@ -0,0 +1,65 @@
+package kotools.types.experimental
+
+import kotools.shared.Project.Types
+import kotools.shared.SinceKotools
+import kotools.shared.StabilityLevel.Experimental
+
+/**
+ * Representation of explicit numbers.
+ *
+ * @param N The type of [Number] to hold.
+ */
+@ExperimentalKotoolsTypesApi
+@SinceKotools(Types, "3.2", Experimental)
+public sealed interface ExplicitNumber<out N : Number> {
+    public val value: N
+}
+
+/**
+ * Representation of numbers other than zero.
+ *
+ * @param N The type of [Number] to hold.
+ */
+@ExperimentalKotoolsTypesApi
+@SinceKotools(Types, "3.2", Experimental)
+public sealed interface NonZeroNumber<out N : Number> : ExplicitNumber<N>
+
+/**
+ * Representation of positive numbers (including zero).
+ *
+ * @param N The type of [Number] to hold.
+ */
+@ExperimentalKotoolsTypesApi
+@SinceKotools(Types, "3.2", Experimental)
+public sealed interface PositiveNumber<out N : Number> : ExplicitNumber<N>
+
+/**
+ * Representation of negative numbers (including zero).
+ *
+ * @param N The type of [Number] to hold.
+ */
+@ExperimentalKotoolsTypesApi
+@SinceKotools(Types, "3.2", Experimental)
+public sealed interface NegativeNumber<out N : Number> : ExplicitNumber<N>
+
+/**
+ * Representation of strictly positive numbers (excluding zero).
+ *
+ * @param N The type of [Number] to hold.
+ */
+@ExperimentalKotoolsTypesApi
+@SinceKotools(Types, "3.2", Experimental)
+public sealed interface StrictlyPositiveNumber<out N : Number> :
+    NonZeroNumber<N>,
+    PositiveNumber<N>
+
+/**
+ * Representation of strictly negative numbers (excluding zero).
+ *
+ * @param N The type of [Number] to hold.
+ */
+@ExperimentalKotoolsTypesApi
+@SinceKotools(Types, "3.2", Experimental)
+public sealed interface StrictlyNegativeNumber<out N : Number> :
+    NonZeroNumber<N>,
+    NegativeNumber<N>
