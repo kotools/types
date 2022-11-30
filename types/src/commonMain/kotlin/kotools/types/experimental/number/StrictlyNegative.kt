2546c43a9dfea45b8b232e12ee8b8a83e653a70a
Loïc Lamarque <loiclamarque777@gmail.com>
2022-11-30 14:19:37 +0100

    Revert "refactor(types-#67): reorganize the experimental number API"
    This reverts commit d6244a3f75d8c500869570d2c2b1eeb30042de8a.

--- types/src/commonMain/kotlin/kotools/types/experimental/number/StrictlyNegative.kt
+++ types/src/commonMain/kotlin/kotools/types/experimental/number/StrictlyNegative.kt
@@ -4,9 +4,6 @@ import kotools.shared.Project
 import kotools.shared.SinceKotools
 import kotools.shared.StabilityLevel
 import kotools.types.experimental.ExperimentalKotoolsTypesApi
-import kotlin.jvm.JvmInline
-
-// ---------- Number ----------
 
 /**
  * Representation of strictly negative numbers.
@@ -17,22 +14,3 @@ import kotlin.jvm.JvmInline
 @SinceKotools(Project.Types, "3.2", StabilityLevel.Experimental)
 public interface StrictlyNegativeNumber<out N : Number> : NonZeroNumber<N>,
     NegativeNumber<N>
-
-// ---------- Int ----------
-
-/** Representation of strictly negative integers, excluding zero. */
-@ExperimentalKotoolsTypesApi
-@JvmInline
-@SinceKotools(Project.Types, "1.1", StabilityLevel.Experimental)
-public value class StrictlyNegativeInt
-private constructor(override val value: Int) : StrictlyNegativeNumber<Int> {
-    /**
-     * Contains declarations for holding or building a [StrictlyNegativeInt].
-     */
-    public companion object
-}
-
-@ExperimentalKotoolsTypesApi
-internal val StrictlyNegativeInt.Companion.range: IntRange by lazy {
-    Int.MIN_VALUE..-1
-}
