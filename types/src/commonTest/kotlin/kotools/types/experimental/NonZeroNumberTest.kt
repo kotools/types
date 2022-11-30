7a41e49edd8f605d04dbd44b08d191f7483c67e6
Loïc Lamarque <loiclamarque777@gmail.com>
2022-11-30 14:18:55 +0100

    Revert "feat(types-#67)!: replace experimental nonZero properties by toNonZeroNumber function"
    This reverts commit 4781d54582e6875e286520f5abf58fa9a37d7971.

--- types/src/commonTest/kotlin/kotools/types/experimental/NonZeroNumberTest.kt
+++ types/src/commonTest/kotlin/kotools/types/experimental/NonZeroNumberTest.kt
@@ -9,32 +9,49 @@ import kotlin.test.Test
 
 @ExperimentalKotoolsTypesApi
 class NonZeroNumberTest {
-    // ---------- Number.toNonZeroNumber() ----------
+    // ---------- Byte.nonZero ----------
 
     @Test
-    fun toNonZeroNumber_should_pass_with_a_Number_other_than_zero() {
-        val value: Int = randomNonZeroInt().value
-        value.toNonZeroNumber()
-            .getOrThrow()
-            .value assertEquals value
-    }
+    fun nonZero_should_pass_with_a_Byte_other_than_zero(): Unit =
+        randomNonZeroInt()
+            .value
+            .toByte()
+            .let { it.nonZero.getOrThrow().value assertEquals it }
 
     @Test
-    fun toNonZeroNumber_should_fail_with_a_Number_that_equals_zero() {
-        val result: Result<NonZeroNumber<Int>> = 0.toNonZeroNumber()
-        assertFailsWith<IllegalArgumentException>(result::getOrThrow)
-            .message
-            .assertNotNull()
-            .isNotBlank()
-            .assertTrue()
-    }
+    fun nonZero_should_fail_with_a_Byte_that_equals_zero(): Unit = 0.toByte()
+        .nonZero
+        .let { assertFailsWith<IllegalArgumentException>(it::getOrThrow) }
+        .message
+        .assertNotNull()
+        .isNotBlank()
+        .assertTrue()
+
+    // ---------- Short.nonZero ----------
+
+    @Test
+    fun nonZero_should_pass_with_a_Short_other_than_zero(): Unit =
+        randomNonZeroInt()
+            .value
+            .toShort()
+            .let { it.nonZero.getOrThrow().value assertEquals it }
+
+    @Test
+    fun nonZero_should_fail_with_a_Short_that_equals_zero(): Unit = 0.toShort()
+        .nonZero
+        .let { assertFailsWith<IllegalArgumentException>(it::getOrThrow) }
+        .message
+        .assertNotNull()
+        .isNotBlank()
+        .assertTrue()
 
     // ---------- NonZeroNumber.toString() ----------
 
     @Test
     fun toString_should_behave_like_its_value(): Unit = randomNonZeroInt()
         .value
-        .toNonZeroNumber()
+        .toByte()
+        .nonZero
         .getOrThrow()
         .run { toString() assertEquals value.toString() }
 }
