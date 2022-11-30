9ace3f23e9be4dc7bd1ac5f2571bb4ca614b5cd0
Loïc Lamarque <loiclamarque777@gmail.com>
2022-11-30 16:03:52 +0100

    feat(types-#81): add the NotBlankString.first property

--- types/src/commonMain/kotlin/kotools/types/NotBlankString.kt
+++ types/src/commonMain/kotlin/kotools/types/NotBlankString.kt
@@ -23,6 +23,9 @@ private constructor(private val value: String) : Comparable<NotBlankString> {
             )
     }
 
+    /** The first character of this string. */
+    public val first: Char get() = value[0]
+
     /**
      * Compares this string lexicographically with the [other] one for order.
      * Returns zero if this string equals the [other] one, a negative number if
