232c11875c2e76b3a4b02280e632f2a16e53e1d0
Loïc Lamarque <loiclamarque777@gmail.com>
2022-12-02 12:25:37 +0100

    feat(types-#85): override the StrictlyNegativeInt.toString function

--- types/src/commonMain/kotlin/kotools/types/StrictlyNegativeInt.kt
+++ types/src/commonMain/kotlin/kotools/types/StrictlyNegativeInt.kt
@@ -43,10 +43,13 @@ private constructor(private val value: Int) : Comparable<StrictlyNegativeInt>,
     override fun compareTo(other: StrictlyNegativeInt): Int =
         value.compareTo(other.value)
 
+    override fun toInt(): Int = value
+
     internal fun toNonZeroInt(): NonZeroInt = value.toNonZeroInt()
         .getOrThrow()
 
-    override fun toInt(): Int = value
+    /** Returns this integer as a [String]. */
+    override fun toString(): String = "$value"
 }
 
 /**
