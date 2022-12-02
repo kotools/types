d674f0e207dc4d5c37f6917c74e5b133da414754
Loïc Lamarque <loiclamarque777@gmail.com>
2022-12-02 01:07:30 +0100

    feat(types-#82): add operations to the PositiveInt.Companion object

--- types/src/commonTest/kotlin/kotools/types/PositiveIntTest.kt
+++ types/src/commonTest/kotlin/kotools/types/PositiveIntTest.kt
@@ -4,8 +4,6 @@ import kotools.assert.assertEquals
 import kotools.assert.assertFailsWith
 import kotools.assert.assertNotNull
 import kotools.assert.assertTrue
-import kotlin.random.Random
-import kotlin.random.nextInt
 import kotlin.test.Test
 
 class PositiveIntTest {
@@ -13,7 +11,7 @@ class PositiveIntTest {
 
     @Test
     fun of_should_pass_with_a_positive_Int() {
-        val value: Int = Random.nextInt(0..Int.MAX_VALUE)
+        val value: Int = PositiveInt.random().toInt()
         PositiveInt.of(value)
             .getOrThrow()
             .toInt() assertEquals value
