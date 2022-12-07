5f7714a16b170c61ef077fc9a6ba231ebafc7f56
Loïc Lamarque <loiclamarque777@gmail.com>
2022-12-07 21:31:35 +0100

    chore(types-#85): stabilize the StrictlyNegativeInt type

--- types/src/commonMain/kotlin/kotools/types/StrictlyNegativeInt.kt
+++ types/src/commonMain/kotlin/kotools/types/StrictlyNegativeInt.kt
@@ -2,14 +2,13 @@ package kotools.types
 
 import kotools.shared.Project.Types
 import kotools.shared.SinceKotools
-import kotools.shared.StabilityLevel
 import kotools.types.number.aStrictlyNegativeNumber
 import kotools.types.number.shouldBe
 import kotlin.jvm.JvmInline
 
 /** Representation of strictly negative integers, excluding zero. */
 @JvmInline
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
+@SinceKotools(Types, "3.2")
 public value class StrictlyNegativeInt
 private constructor(private val value: Int) : Comparable<StrictlyNegativeInt>,
     ExplicitInt {
@@ -60,7 +59,7 @@ private constructor(private val value: Int) : Comparable<StrictlyNegativeInt>,
  * Divides this integer by the [other] one, truncating the result to an integer
  * that is closer to zero.
  */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
+@SinceKotools(Types, "3.2")
 public operator fun Int.div(other: StrictlyNegativeInt): Int =
     this / other.toInt()
 
@@ -68,6 +67,6 @@ public operator fun Int.div(other: StrictlyNegativeInt): Int =
  * Returns this integer as a [StrictlyNegativeInt], or an
  * [IllegalArgumentException] if this integer is positive.
  */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
+@SinceKotools(Types, "3.2")
 public fun Int.toStrictlyNegativeInt(): Result<StrictlyNegativeInt> =
     StrictlyNegativeInt of this
