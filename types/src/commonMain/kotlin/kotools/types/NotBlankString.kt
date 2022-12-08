2218b90b3999d5d99dbc9a3bb5291de3a98784f7
Loïc Lamarque <loiclamarque777@gmail.com>
2022-12-08 16:44:14 +0100

    feat(types-#103): improve deserialization of kotools.types.NotBlankString

--- types/src/commonMain/kotlin/kotools/types/NotBlankString.kt
+++ types/src/commonMain/kotlin/kotools/types/NotBlankString.kt
@@ -1,28 +1,32 @@
 package kotools.types
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotools.shared.Project.Types
 import kotools.shared.SinceKotools
 import kotlin.jvm.JvmInline
+
+/**
+ * Returns this string as a [NotBlankString], or [IllegalArgumentException] if
+ * this string is blank.
+ */
+@SinceKotools(Types, "3.2")
+public fun String.toNotBlankString(): Result<NotBlankString> =
+    NotBlankString of this
 
 /**
  * Representation of strings that have at least one character, excluding
  * whitespaces.
  */
 @JvmInline
-@Serializable
+@Serializable(NotBlankStringSerializer::class)
 @SinceKotools(Types, "3.2")
 public value class NotBlankString
 private constructor(private val value: String) : Comparable<NotBlankString> {
-    internal companion object {
-        infix fun of(value: String): Result<NotBlankString> = value
-            .takeIf(String::isNotBlank)
-            ?.toSuccessfulResult(::NotBlankString)
-            ?: Result.failure(
-                IllegalArgumentException("Given string shouldn't be blank.")
-            )
-    }
-
     /**
      * Compares this string lexicographically with the [other] one for order.
      * Returns zero if this string equals the [other] one, a negative number if
@@ -34,12 +38,28 @@ private constructor(private val value: String) : Comparable<NotBlankString> {
 
     /** Returns this value as a [String]. */
     override fun toString(): String = value
+
+    internal companion object {
+        infix fun of(value: String): Result<NotBlankString> = value
+            .takeIf(String::isNotBlank)
+            ?.toSuccessfulResult(::NotBlankString)
+            ?: Result.failure(
+                IllegalArgumentException("Given string shouldn't be blank.")
+            )
+    }
 }
 
-/**
- * Returns this string as a [NotBlankString], or [IllegalArgumentException] if
- * this string is blank.
- */
-@SinceKotools(Types, "3.2")
-public fun String.toNotBlankString(): Result<NotBlankString> =
-    NotBlankString of this
+internal object NotBlankStringSerializer : KSerializer<NotBlankString> {
+    private val delegate: KSerializer<String> = String.serializer()
+    override val descriptor: SerialDescriptor = delegate.descriptor
+
+    override fun serialize(encoder: Encoder, value: NotBlankString) {
+        val stringValue: String = value.toString()
+        delegate.serialize(encoder, stringValue)
+    }
+
+    override fun deserialize(decoder: Decoder): NotBlankString = delegate
+        .deserialize(decoder)
+        .toNotBlankString()
+        .getOrThrow()
+}
--- types/src/commonTest/kotlin/kotools/types/NotBlankStringTest.kt
+++ types/src/commonTest/kotlin/kotools/types/NotBlankStringTest.kt
@@ -3,22 +3,10 @@ package kotools.types
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import kotools.assert.assertEquals
-import kotools.assert.assertFailsWith
-import kotools.assert.assertNotNull
-import kotools.assert.assertTrue
+import kotools.assert.*
 import kotlin.test.Test
 
 class NotBlankStringTest {
-    @Test
-    fun compareTo_should_pass() {
-        val x: NotBlankString = "hello".toNotBlankStringOrThrow()
-        val y: NotBlankString = "world".toNotBlankStringOrThrow()
-        val result: Int = x.compareTo(y)
-        val expectedResult: Int = "$x".compareTo("$y")
-        result assertEquals expectedResult
-    }
-
     @Test
     fun string_toNotBlankString_should_pass_with_a_not_blank_String() {
         val value = "hello world"
@@ -38,6 +26,15 @@ class NotBlankStringTest {
     }
 
     @Test
+    fun compareTo_should_pass() {
+        val x: NotBlankString = "hello".toNotBlankStringOrThrow()
+        val y: NotBlankString = "world".toNotBlankStringOrThrow()
+        val result: Int = x.compareTo(y)
+        val expectedResult: Int = "$x".compareTo("$y")
+        result assertEquals expectedResult
+    }
+
+    @Test
     fun serialization_should_behave_like_a_String() {
         val value: NotBlankString = "hello world".toNotBlankStringOrThrow()
         val result: String = Json.encodeToString(value)
@@ -46,11 +43,22 @@ class NotBlankStringTest {
     }
 
     @Test
-    fun deserialization_should_pass() {
+    fun deserialization_should_pass_with_a_not_blank_String() {
         val value = "hello world"
         val encoded: String = Json.encodeToString(value)
         val result: NotBlankString = Json.decodeFromString(encoded)
         "$result" assertEquals value
+    }
+
+    @Test
+    fun deserialization_should_fail_with_a_blank_String() {
+        val value = " "
+        val encoded: String = Json.encodeToString(value)
+        val error: IllegalArgumentException =
+            assertFailsWith { Json.decodeFromString<NotBlankString>(encoded) }
+        error.message.assertNotNull()
+            .isNotBlank()
+            .assertTrue()
     }
 }
 
