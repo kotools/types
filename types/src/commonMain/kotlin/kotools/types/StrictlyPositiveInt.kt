892301691a061bd85e7f0e578767240846bdc8b9
Loïc Lamarque <loiclamarque777@gmail.com>
2022-12-08 14:05:03 +0100

    Revert "chore(types-#83): stabilize the StrictlyPositiveInt type"
    This reverts commit e3ef29388d6d9e3aa1498f88f783caf6291b6320.

--- types/src/commonMain/kotlin/kotools/types/StrictlyPositiveInt.kt
+++ types/src/commonMain/kotlin/kotools/types/StrictlyPositiveInt.kt
@@ -2,13 +2,14 @@ package kotools.types
 
 import kotools.shared.Project.Types
 import kotools.shared.SinceKotools
+import kotools.shared.StabilityLevel
 import kotools.types.number.aStrictlyPositiveNumber
 import kotools.types.number.shouldBe
 import kotlin.jvm.JvmInline
 
 /** Representation of strictly positive integers, excluding zero. */
 @JvmInline
-@SinceKotools(Types, "3.2")
+@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
 public value class StrictlyPositiveInt
 private constructor(private val value: Int) : Comparable<StrictlyPositiveInt>,
     ExplicitInt {
@@ -90,7 +91,7 @@ private constructor(private val value: Int) : Comparable<StrictlyPositiveInt>,
  * Divides this integer by the [other] one, truncating the result to an integer
  * that is closer to zero.
  */
-@SinceKotools(Types, "3.2")
+@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
 public operator fun Int.div(other: StrictlyPositiveInt): Int =
     this / other.toInt()
 
@@ -98,6 +99,6 @@ public operator fun Int.div(other: StrictlyPositiveInt): Int =
  * Returns this integer as a [StrictlyPositiveInt], or an
  * [IllegalArgumentException] if this integer is negative.
  */
-@SinceKotools(Types, "3.2")
+@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
 public fun Int.toStrictlyPositiveInt(): Result<StrictlyPositiveInt> =
     StrictlyPositiveInt of this
