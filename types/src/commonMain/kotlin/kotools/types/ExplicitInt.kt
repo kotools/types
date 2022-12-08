7c83d6e7b551eaf40b2ea4639443b432d4d572de
Loïc Lamarque <loiclamarque777@gmail.com>
2022-12-08 14:05:18 +0100

    Revert "refactor(types-#86)!: convert member functions of ExplicitInt to extensions"
    This reverts commit 3b74b87698d811f7164e0aafbae7f25bc350bf59.

--- types/src/commonMain/kotlin/kotools/types/ExplicitInt.kt
+++ types/src/commonMain/kotlin/kotools/types/ExplicitInt.kt
@@ -10,11 +10,89 @@ public sealed interface ExplicitInt {
     /** Returns the negative of this integer. */
     public operator fun unaryMinus(): ExplicitInt
 
+    /**
+     * Compares this integer with the [other] one for order.
+     * Returns zero if this integer equals the [other] one, a negative number if
+     * this integer is less than the [other] one, or a positive number if
+     * this integer is greater than the [other] one.
+     */
+    public operator fun compareTo(other: Int): Int = toInt().compareTo(other)
+
+    /**
+     * Compares this integer with the [other] one for order.
+     * Returns zero if this integer equals the [other] one, a negative number if
+     * this integer is less than the [other] one, or a positive number if
+     * this integer is greater than the [other] one.
+     */
+    public operator fun compareTo(other: ExplicitInt): Int =
+        compareTo(other.toInt())
+
+    /** Adds the [other] integer to this one. */
+    public operator fun plus(other: Int): Int = toInt() + other
+
+    /** Adds the [other] integer to this one. */
+    public operator fun plus(other: ExplicitInt): Int = this + other.toInt()
+
+    /** Subtracts the [other] integer from this one. */
+    public operator fun minus(other: Int): Int = toInt() - other
+
+    /** Subtracts the [other] integer from this one. */
+    public operator fun minus(other: ExplicitInt): Int = this - other.toInt()
+
+    /** Multiplies this integer by the [other] one. */
+    public operator fun times(other: Int): Int = toInt() * other
+
+    /** Multiplies this integer by the [other] one. */
+    public operator fun times(other: ExplicitInt): Int = this * other.toInt()
+
+    /**
+     * Divides this integer by the [other] one, truncating the result to an integer
+     * that is closer to zero.
+     */
+    public operator fun div(other: NonZeroInt): Int = toInt() / other
+
+    /**
+     * Divides this integer by the [other] one, truncating the result to an integer
+     * that is closer to zero.
+     */
+    public operator fun div(other: StrictlyPositiveInt): Int = toInt() / other
+
+    /**
+     * Divides this integer by the [other] one, truncating the result to an integer
+     * that is closer to zero.
+     */
+    public operator fun div(other: StrictlyNegativeInt): Int = toInt() / other
+
+    /**
+     * Returns the remainder of truncating division of this integer by the
+     * [other] one.
+     */
+    public operator fun rem(other: NonZeroInt): Int = toInt() % other.toInt()
+
+    /**
+     * Returns the remainder of truncating division of this integer by the
+     * [other] one.
+     */
+    public operator fun rem(other: StrictlyPositiveInt): Int =
+        toInt() % other.toInt()
+
+    /**
+     * Returns the remainder of truncating division of this integer by the
+     * [other] one.
+     */
+    public operator fun rem(other: StrictlyNegativeInt): Int =
+        toInt() % other.toInt()
+
     /** Returns this value as an [Int]. */
     public fun toInt(): Int
 
     /** Returns this integer as a [String]. */
     override fun toString(): String
+
+    /** Returns this integer as a [NotBlankString]. */
+    public fun toNotBlankString(): NotBlankString = toString()
+        .toNotBlankString()
+        .getOrThrow()
 }
 
 /**
@@ -27,114 +105,14 @@ public sealed interface ExplicitInt {
 public operator fun Int.compareTo(other: ExplicitInt): Int =
     compareTo(other.toInt())
 
-/**
- * Compares this integer with the [other] one for order.
- * Returns zero if this integer equals the [other] one, a negative number if
- * this integer is less than the [other] one, or a positive number if this
- * integer is greater than the [other] one.
- */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
-public operator fun ExplicitInt.compareTo(other: Int): Int = toInt()
-    .compareTo(other)
-
-/**
- * Compares this integer with the [other] one for order.
- * Returns zero if this integer equals the [other] one, a negative number if
- * this integer is less than the [other] one, or a positive number if this
- * integer is greater than the [other] one.
- */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
-public operator fun ExplicitInt.compareTo(other: ExplicitInt): Int =
-    compareTo(other.toInt())
-
 /** Adds the [other] integer to this one. */
 @SinceKotools(Types, "3.2", StabilityLevel.Alpha)
 public operator fun Int.plus(other: ExplicitInt): Int = this + other.toInt()
-
-/** Adds the [other] integer to this one. */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
-public operator fun ExplicitInt.plus(other: Int): Int = toInt() + other
-
-/** Adds the [other] integer to this one. */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
-public operator fun ExplicitInt.plus(other: ExplicitInt): Int =
-    this + other.toInt()
 
 /** Subtracts the [other] integer from this one. */
 @SinceKotools(Types, "3.2", StabilityLevel.Alpha)
 public operator fun Int.minus(other: ExplicitInt): Int = this - other.toInt()
 
-/** Subtracts the [other] integer from this one. */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
-public operator fun ExplicitInt.minus(other: Int): Int = toInt() - other
-
-/** Subtracts the [other] integer from this one. */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
-public operator fun ExplicitInt.minus(other: ExplicitInt): Int =
-    this - other.toInt()
-
 /** Multiplies this integer by the [other] one. */
 @SinceKotools(Types, "3.2", StabilityLevel.Alpha)
 public operator fun Int.times(other: ExplicitInt): Int = this * other.toInt()
-
-/** Multiplies this integer by the [other] one. */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
-public operator fun ExplicitInt.times(other: Int): Int = toInt() * other
-
-/** Multiplies this integer by the [other] one. */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
-public operator fun ExplicitInt.times(other: ExplicitInt): Int =
-    this * other.toInt()
-
-/**
- * Divides this integer by the [other] one, truncating the result to an integer
- * that is closer to zero.
- */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
-public operator fun ExplicitInt.div(other: NonZeroInt): Int = toInt() / other
-
-/**
- * Divides this integer by the [other] one, truncating the result to an integer
- * that is closer to zero.
- */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
-public operator fun ExplicitInt.div(other: StrictlyPositiveInt): Int =
-    toInt() / other
-
-/**
- * Divides this integer by the [other] one, truncating the result to an integer
- * that is closer to zero.
- */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
-public operator fun ExplicitInt.div(other: StrictlyNegativeInt): Int =
-    toInt() / other
-
-/**
- * Returns the remainder of truncating division of this integer by the [other]
- * one.
- */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
-public operator fun ExplicitInt.rem(other: NonZeroInt): Int =
-    toInt() % other.toInt()
-
-/**
- * Returns the remainder of truncating division of this integer by the [other]
- * one.
- */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
-public operator fun ExplicitInt.rem(other: StrictlyPositiveInt): Int =
-    toInt() % other.toInt()
-
-/**
- * Returns the remainder of truncating division of this integer by the [other]
- * one.
- */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
-public operator fun ExplicitInt.rem(other: StrictlyNegativeInt): Int =
-    toInt() % other.toInt()
-
-/** Returns this integer as a [NotBlankString]. */
-@SinceKotools(Types, "3.2", StabilityLevel.Alpha)
-public fun ExplicitInt.toNotBlankString(): NotBlankString = toString()
-    .toNotBlankString()
-    .getOrThrow()
