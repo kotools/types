5c3e8ed91b344f681b0fca2034fe0f877ae89e28
Loïc Lamarque <loiclamarque777@gmail.com>
2022-12-08 18:08:24 +0100

    refactor(types-#105): reorder declarations of PositiveInt

--- types/src/commonMain/kotlin/kotools/types/PositiveInt.kt
+++ types/src/commonMain/kotlin/kotools/types/PositiveInt.kt
@@ -6,25 +6,24 @@ import kotools.types.number.aPositiveNumber
 import kotools.types.number.shouldBe
 import kotlin.jvm.JvmInline
 
+/** Representation of positive integers, including zero. */
+@JvmInline
+@SinceKotools(Types, "3.2")
+public value class PositiveInt
+private constructor(private val value: Int) : ExplicitInt {
+    internal companion object {
+        infix fun of(value: Int): Result<PositiveInt> = value.takeIf { it >= 0 }
+            ?.toSuccessfulResult(::PositiveInt)
+            ?: Result.failure(value shouldBe aPositiveNumber)
+    }
+
+    override fun toInt(): Int = value
+    override fun toString(): String = "$value"
+}
+
 /**
  * Returns this integer as a [PositiveInt], or [IllegalArgumentException] if
  * this integer is strictly negative.
  */
 @SinceKotools(Types, "3.2")
 public fun Int.toPositiveInt(): Result<PositiveInt> = PositiveInt of this
-
-/** Representation of positive integers, including zero. */
-@JvmInline
-@SinceKotools(Types, "3.2")
-public value class PositiveInt
-private constructor(private val value: Int) : ExplicitInt {
-    override fun toInt(): Int = value
-
-    override fun toString(): String = "$value"
-
-    internal companion object {
-        infix fun of(value: Int): Result<PositiveInt> = value.takeIf { it >= 0 }
-            ?.toSuccessfulResult(::PositiveInt)
-            ?: Result.failure(value shouldBe aPositiveNumber)
-    }
-}
--- types/src/commonTest/kotlin/kotools/types/PositiveIntTest.kt
+++ types/src/commonTest/kotlin/kotools/types/PositiveIntTest.kt
@@ -8,11 +8,14 @@ import kotlin.random.Random
 import kotlin.random.nextInt
 import kotlin.test.Test
 
-@Throws(IllegalArgumentException::class)
-private fun Int.toPositiveIntOrThrow(): PositiveInt = toPositiveInt()
-    .getOrThrow()
-
 class PositiveIntTest {
+    @Test
+    fun toString_should_behave_like_an_Int() {
+        val value: Int = Random.nextInt(0..Int.MAX_VALUE)
+        value.toPositiveIntOrThrow()
+            .toString() assertEquals "$value"
+    }
+
     @Test
     fun int_toPositiveInt_should_pass_with_a_positive_Int() {
         val value: Int = Random.nextInt(0..Int.MAX_VALUE)
@@ -31,11 +34,8 @@ class PositiveIntTest {
             .isNotBlank()
             .assertTrue()
     }
-
-    @Test
-    fun toString_should_behave_like_an_Int() {
-        val value: Int = Random.nextInt(0..Int.MAX_VALUE)
-        value.toPositiveIntOrThrow()
-            .toString() assertEquals "$value"
-    }
 }
+
+@Throws(IllegalArgumentException::class)
+private fun Int.toPositiveIntOrThrow(): PositiveInt = toPositiveInt()
+    .getOrThrow()
