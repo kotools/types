24406b1116bb8f837f63286fbac28b2e87828d4f
Loïc Lamarque <loiclamarque777@gmail.com>
2022-12-08 12:52:29 +0100

    feat(types-#83): make the kotools.types.StrictlyPositiveInt type serializable

--- types/src/commonMain/kotlin/kotools/types/StrictlyPositiveInt.kt
+++ types/src/commonMain/kotlin/kotools/types/StrictlyPositiveInt.kt
@@ -1,5 +1,6 @@
 package kotools.types
 
+import kotlinx.serialization.Serializable
 import kotools.shared.Project.Types
 import kotools.shared.SinceKotools
 import kotools.types.number.aStrictlyPositiveNumber
@@ -8,6 +9,7 @@ import kotlin.jvm.JvmInline
 
 /** Representation of strictly positive integers, excluding zero. */
 @JvmInline
+@Serializable
 @SinceKotools(Types, "3.2")
 public value class StrictlyPositiveInt
 private constructor(private val value: Int) : Comparable<StrictlyPositiveInt>,
