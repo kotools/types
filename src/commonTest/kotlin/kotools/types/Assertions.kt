63e124f53363f9366708ebeaa11eea79d2b2998a
Loïc Lamarque <loiclamarque777@gmail.com>
2022-12-29 00:12:49 +0100

    refactor(#11): update implementation of NotBlankString

--- src/commonTest/kotlin/kotools/types/Assertions.kt
+++ src/commonTest/kotlin/kotools/types/Assertions.kt
@@ -1,7 +1,10 @@
 package kotools.types
 
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
-internal fun Throwable.assertHasAMessage(): Unit =
+infix fun <T> T.shouldEqual(expected: T): Unit = assertEquals(expected, this)
+
+fun Throwable.assertHasAMessage(): Unit =
     assertTrue(block = assertNotNull(message)::isNotBlank)
