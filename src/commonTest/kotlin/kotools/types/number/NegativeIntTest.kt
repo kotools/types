e6ced9422dbf9091d803b4b20323b5b7dd061f67
Loïc Lamarque <loiclamarque777@gmail.com>
2022-12-29 16:34:31 +0100

    feat(#15): add min, max and random declarations to NegativeInt

--- src/commonTest/kotlin/kotools/types/number/NegativeIntTest.kt
+++ src/commonTest/kotlin/kotools/types/number/NegativeIntTest.kt
@@ -6,22 +6,41 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotools.types.Package
 import kotools.types.assertHasAMessage
+import kotools.types.shouldEqual
+import kotools.types.shouldNotEqual
 import kotlin.test.Test
-import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
-internal val negativeIntRange: IntRange = Int.MIN_VALUE..ZeroInt.asInt
-
-class NegativeIntTest {
+class NegativeIntCompanionTest {
     @Test
-    fun int_toNegativeInt_should_pass_with_a_negative_Int() {
-        val value: Int = negativeIntRange.random()
-        val result: Int = value.asNegativeInt.getOrThrow().asInt
-        assertEquals(value, result)
+    fun min_should_equal_the_minimum_value_of_Int() {
+        val result: StrictlyNegativeInt = NegativeInt.min
+        result.asInt shouldEqual Int.MIN_VALUE
     }
 
     @Test
-    fun int_toNegativeInt_should_fail_with_a_strictly_positive_Int() {
+    fun max_should_equal_zero() {
+        val result: ZeroInt = NegativeInt.max
+        result shouldEqual ZeroInt
+    }
+
+    @Test
+    fun random_should_return_different_values() {
+        val result: NegativeInt = NegativeInt.random()
+        result.asInt shouldNotEqual NegativeInt.random().asInt
+    }
+}
+
+class NegativeIntTest {
+    @Test
+    fun int_asNegativeInt_should_pass_with_a_negative_Int() {
+        val value: Int = NegativeInt.random().asInt
+        val result: Int = value.asNegativeInt.getOrThrow().asInt
+        result shouldEqual value
+    }
+
+    @Test
+    fun int_asNegativeInt_should_fail_with_a_strictly_positive_Int() {
         val result: Result<NegativeInt> = strictlyPositiveIntRange.random()
             .asNegativeInt
         assertFailsWith<IllegalArgumentException>(block = result::getOrThrow)
@@ -34,26 +53,23 @@ class NegativeIntSerializerTest {
     @Test
     fun descriptor_should_have_the_qualified_name_of_NegativeInt_as_serial_name() {
         val result: String = NegativeIntSerializer.descriptor.serialName
-        assertEquals("${Package.number}.NegativeInt", result)
+        result shouldEqual "${Package.number}.NegativeInt"
     }
 
     @Test
     fun serialization_should_behave_like_an_Int() {
-        val x: NegativeInt = negativeIntRange.random()
-            .asNegativeInt
-            .getOrThrow()
+        val x: NegativeInt = NegativeInt.random()
         val result: String = Json.encodeToString(NegativeIntSerializer, x)
-        val expected: String = Json.encodeToString(x.asInt)
-        assertEquals(expected, result)
+        result shouldEqual Json.encodeToString(x.asInt)
     }
 
     @Test
     fun deserialization_should_pass_with_a_negative_Int() {
-        val value: Int = negativeIntRange.random()
+        val value: Int = NegativeInt.random().asInt
         val encoded: String = Json.encodeToString(value)
         val result: NegativeInt =
             Json.decodeFromString(NegativeIntSerializer, encoded)
-        assertEquals(value, result.asInt)
+        result.asInt shouldEqual value
     }
 
     @Test
