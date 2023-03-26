15a008ee179903812d20d8f7c69befee3ce2bb41
Loïc Lamarque <loiclamarque777@gmail.com>
2023-03-26 16:27:31 +0200

    feat(#37): add toNotBlankString builder in ResultContext

--- src/commonMain/kotlin/kotools/types/result/Context.kt
+++ src/commonMain/kotlin/kotools/types/result/Context.kt
@@ -1,12 +1,25 @@
 package kotools.types.result
 
 import kotools.types.SinceKotoolsTypes
+import kotools.types.text.NotBlankString
+import kotools.types.text.toNotBlankString as delegateToNotBlankString
 
 /** Context available when calling the [resultOf] function. */
 @SinceKotoolsTypes("4.1")
-public sealed interface ResultContext
+public sealed interface ResultContext {
+    /**
+     * Returns this string as a [NotBlankString], or throws an
+     * [IllegalArgumentException] if this string is [blank][String.isBlank].
+     */
+    @Throws(IllegalArgumentException::class)
+    public fun String.toNotBlankString(): NotBlankString
+}
 
-private object ResultContextImplementation : ResultContext
+private object ResultContextImplementation : ResultContext {
+    override fun String.toNotBlankString(): NotBlankString =
+        delegateToNotBlankString()
+            .getOrThrow()
+}
 
 /**
  * Returns an encapsulated result of calling the [block] function in the
--- src/commonTest/kotlin/kotools/types/result/ContextTest.kt
+++ src/commonTest/kotlin/kotools/types/result/ContextTest.kt
@@ -1,10 +1,29 @@
 package kotools.types.result
 
 import kotools.types.shouldEqual
+import kotools.types.shouldHaveAMessage
+import kotools.types.text.NotBlankString
 import kotlin.random.Random
 import kotlin.test.Test
+import kotlin.test.assertFailsWith
 
 class ResultContextTest {
+    @Test
+    fun string_toNotBlankString_should_pass_with_a_not_blank_String() {
+        val string = "hello world"
+        val result: Result<NotBlankString> = resultOf {
+            string.toNotBlankString()
+        }
+        "${result.getOrThrow()}" shouldEqual string
+    }
+
+    @Test
+    fun string_toNotBlankString_should_fail_with_a_blank_String() {
+        val result: Result<NotBlankString> = resultOf { "".toNotBlankString() }
+        assertFailsWith<IllegalArgumentException>(block = result::getOrThrow)
+            .shouldHaveAMessage()
+    }
+
     @Test
     fun resultOf_should_pass() {
         val value: Int = Random.nextInt()
