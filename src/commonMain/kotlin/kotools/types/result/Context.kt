518168cf44b1d638c0d5e2f3c8cf31624e865f38
Loïc Lamarque <loiclamarque777@gmail.com>
2023-03-27 08:10:12 +0200

    feat(#37): add toNonZeroInt builder in ResultContext

--- src/commonMain/kotlin/kotools/types/result/Context.kt
+++ src/commonMain/kotlin/kotools/types/result/Context.kt
@@ -1,12 +1,23 @@
 package kotools.types.result
 
 import kotools.types.SinceKotoolsTypes
+import kotools.types.number.NonZeroInt
+import kotools.types.number.ZeroInt
 import kotools.types.text.NotBlankString
+import kotools.types.number.toNonZeroInt as delegateToNonZeroInt
 import kotools.types.text.toNotBlankString as delegateToNotBlankString
 
 /** Context available when calling the [resultOf] function. */
 @SinceKotoolsTypes("4.1")
 public sealed interface ResultContext {
+    /**
+     * Returns this number as a [NonZeroInt], which may involve rounding or
+     * truncation, or throws an [IllegalArgumentException] if this number
+     * equals [zero][ZeroInt].
+     */
+    @Throws(IllegalArgumentException::class)
+    public fun Number.toNonZeroInt(): NonZeroInt
+
     /**
      * Returns this string as a [NotBlankString], or throws an
      * [IllegalArgumentException] if this string is [blank][String.isBlank].
@@ -16,6 +27,9 @@ public sealed interface ResultContext {
 }
 
 private object ResultContextImplementation : ResultContext {
+    override fun Number.toNonZeroInt(): NonZeroInt = delegateToNonZeroInt()
+        .getOrThrow()
+
     override fun String.toNotBlankString(): NotBlankString =
         delegateToNotBlankString()
             .getOrThrow()
--- src/commonTest/kotlin/kotools/types/result/ContextTest.kt
+++ src/commonTest/kotlin/kotools/types/result/ContextTest.kt
@@ -1,5 +1,7 @@
 package kotools.types.result
 
+import kotools.types.number.NonZeroInt
+import kotools.types.number.ZeroInt
 import kotools.types.shouldEqual
 import kotools.types.shouldHaveAMessage
 import kotools.types.text.NotBlankString
@@ -8,6 +10,24 @@ import kotlin.test.Test
 import kotlin.test.assertFailsWith
 
 class ResultContextTest {
+    @Test
+    fun number_toNonZeroInt_should_pass_with_an_Int_other_than_zero() {
+        val value: Number = NonZeroInt.random()
+            .toInt()
+        val result: Result<NonZeroInt> = resultOf { value.toNonZeroInt() }
+        result.getOrThrow()
+            .toInt() shouldEqual value
+    }
+
+    @Test
+    fun number_toNonZeroInt_should_fail_with_an_Int_that_equals_zero() {
+        val value: Number = ZeroInt.toInt()
+        val result: Result<NonZeroInt> = resultOf { value.toNonZeroInt() }
+        val exception: IllegalArgumentException =
+            assertFailsWith(block = result::getOrThrow)
+        exception.shouldHaveAMessage()
+    }
+
     @Test
     fun string_toNotBlankString_should_pass_with_a_not_blank_String() {
         val string = "hello world"
