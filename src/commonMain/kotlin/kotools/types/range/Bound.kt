86470c8535605a4f92f3f4a3c0e1aa6be0267980
Loïc Lamarque <loiclamarque777@gmail.com>
2023-04-19 11:56:27 +0200

    refactor(#56)!: convert InclusiveBound as interface

--- src/commonMain/kotlin/kotools/types/range/Bound.kt
+++ src/commonMain/kotlin/kotools/types/range/Bound.kt
@@ -14,15 +14,18 @@ public sealed interface Bound<T : Comparable<T>> {
 
 /** Representation of an inclusive bound in a [range][NotEmptyRange]. */
 @SinceKotoolsTypes("4.2")
-public data class InclusiveBound<T : Comparable<T>>
-internal constructor(override val value: T) : Bound<T> {
+public sealed interface InclusiveBound<T : Comparable<T>> : Bound<T>
+
+private data class InclusiveBoundImplementation<T : Comparable<T>>(
+    override val value: T
+) : InclusiveBound<T> {
     override fun toString(): String = "$value"
 }
 
 /** Returns this comparable value as an [InclusiveBound]. */
 @SinceKotoolsTypes("4.2")
 public fun <T : Comparable<T>> T.toInclusiveBound(): InclusiveBound<T> =
-    InclusiveBound(this)
+    InclusiveBoundImplementation(this)
 
 /** Representation of an exclusive bound in a [range][NotEmptyRange]. */
 @SinceKotoolsTypes("4.2")
