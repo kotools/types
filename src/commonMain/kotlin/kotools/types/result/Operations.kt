a427393500a91e835f8083cf5737ba1eae63923f
Loïc Lamarque <loiclamarque777@gmail.com>
2023-04-27 17:40:34 +0200

    refactor(#47): rename generics in Result.flatMap

--- src/commonMain/kotlin/kotools/types/result/Operations.kt
+++ src/commonMain/kotlin/kotools/types/result/Operations.kt
@@ -3,14 +3,14 @@ package kotools.types.result
 import kotools.types.SinceKotoolsTypes
 
 /**
- * Transforms the encapsulated value from [A] to [B] if this result is a
+ * Transforms the encapsulated value from [T] to [R] if this result is a
  * [success][Result.isSuccess], or returns the original exception if this result
  * or the one returned by the [transform] function is a
  * [failure][Result.isFailure].
  */
 @SinceKotoolsTypes("4.2")
-public inline fun <A, B> Result<A>.flatMap(
-    transform: (A) -> Result<B>
-): Result<B> = exceptionOrNull()
+public inline fun <T, R> Result<T>.flatMap(
+    transform: (T) -> Result<R>
+): Result<R> = exceptionOrNull()
     ?.let(Result.Companion::failure)
     ?: transform(getOrThrow())
