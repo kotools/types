85329584328044788e67547ceed7021e72b3f887
Loïc Lamarque <43500570+LVMVRQUXL@users.noreply.github.com>
2023-05-19 16:21:55 +0200

    feat(#47)!: mark Result.flatMap as experimental

--- src/commonTest/kotlin/kotools/types/result/OperationsTest.kt
+++ src/commonTest/kotlin/kotools/types/result/OperationsTest.kt
@@ -1,5 +1,6 @@
 package kotools.types.result
 
+import kotools.types.experimental.ExperimentalResultApi
 import kotools.types.number.*
 import kotools.types.shouldEqual
 import kotlin.test.Test
@@ -7,20 +8,27 @@ import kotlin.test.assertFails
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
+@ExperimentalResultApi
 class FlatMapTest {
     @Test
     fun should_pass_with_a_successful_receiver_and_result_of_transformation() {
+        // GIVEN
         val receiver: Result<NonZeroInt> = 1.toNonZeroInt()
+        // WHEN
         val result: Result<StrictlyPositiveInt> =
             receiver.flatMap { it.plus(2).toStrictlyPositiveInt() }
+        // THEN
         result.getOrThrow().toInt() shouldEqual 3
     }
 
     @Test
     fun should_fail_with_a_failure_as_receiver() {
+        // GIVEN
         val receiver: Result<NonZeroInt> = 0.toNonZeroInt()
+        // WHEN
         val result: Result<StrictlyPositiveInt> =
             receiver.flatMap { it.minus(2).toStrictlyPositiveInt() }
+        // THEN
         val originalException: Throwable =
             assertNotNull(receiver.exceptionOrNull())
         val resultException: Throwable = assertNotNull(result.exceptionOrNull())
@@ -29,9 +37,12 @@ class FlatMapTest {
 
     @Test
     fun should_fail_with_a_failure_as_result_of_transformation() {
+        // GIVEN
         val receiver: Result<NonZeroInt> = 1.toNonZeroInt()
+        // WHEN
         val result: Result<StrictlyPositiveInt> =
             receiver.flatMap { it.minus(2).toStrictlyPositiveInt() }
+        // THEN
         assertNull(receiver.exceptionOrNull())
         assertFails(result::getOrThrow)
     }
