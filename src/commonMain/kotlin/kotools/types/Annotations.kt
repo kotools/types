982df23435975d0780b3bcf1fb95dbe7886d1191
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-01 16:03:14 +0200

    feat(#84): add the 'DeprecatedSinceKotoolsTypes' annotation
    This annotation is only available internally.

--- src/commonMain/kotlin/kotools/types/Annotations.kt
+++ src/commonMain/kotlin/kotools/types/Annotations.kt
@@ -31,3 +31,27 @@ internal annotation class ExperimentalSinceKotoolsTypes(val version: String)
 @Retention(BINARY)
 @Target(CLASS, FUNCTION, PROPERTY, TYPEALIAS)
 internal annotation class SinceKotoolsTypes(val version: String)
+
+/**
+ * Specifies the first versions of Kotools Types where a declaration has
+ * appeared as a **deprecated** feature.
+ *
+ * The specified [warningSince] should match the version where the declaration
+ * was deprecated with a [warning][DeprecationLevel.WARNING] level.
+ * The specified [errorSince] should match the version where it was deprecated
+ * with an [error][DeprecationLevel.ERROR] level.
+ * The specified [hiddenSince] should match the version where the declaration
+ * was deprecated with a [hidden][DeprecationLevel.HIDDEN] level.
+ *
+ * The versions should be in the following formats: `<major>.<minor>` or
+ * `<major>.<minor>.<patch>`, where _major_, _minor_ and _patch_ are positive
+ * integers without leading zeros.
+ */
+@MustBeDocumented
+@Retention(BINARY)
+@Target(CLASS, FUNCTION, PROPERTY, TYPEALIAS)
+internal annotation class DeprecatedSinceKotoolsTypes(
+    val warningSince: String,
+    val errorSince: String = "",
+    val hiddenSince: String = ""
+)
