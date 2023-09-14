696c83c0380b13f1a6111e263571f1f961cf17fe
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-14 04:21:00 +0200

    chore: add 'build-logic' and 'library' to Gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
-rootProject.name = "Kotools Types"
+rootProject.name = "kotools-types"
+
+val modulesDir: File = rootDir.resolve("modules")
+includeBuild(modulesDir.resolve("build-logic"))
+"library".let {
+    include(it)
+    project(":$it").projectDir = modulesDir.resolve(it)
+}
