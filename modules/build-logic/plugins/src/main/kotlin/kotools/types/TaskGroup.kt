c205a189c49578037a878875c4a2bdf86af4eb74
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-15 12:49:01 +0200

    chore: add 'other' Gradle task group

--- modules/build-logic/plugins/src/main/kotlin/kotools/types/TaskGroup.kt
+++ modules/build-logic/plugins/src/main/kotlin/kotools/types/TaskGroup.kt
@@ -3,12 +3,14 @@ package kotools.types
 import org.gradle.api.Task
 
 internal enum class TaskGroup {
-    BUILD, INFORMATION, VERIFICATION;
+    BUILD, INFORMATION, OTHER, VERIFICATION;
 
     override fun toString(): String = name.toLowerCase()
 
     companion object {
-        val all: List<TaskGroup> by lazy { values().toList() }
+        val all: List<TaskGroup> by lazy {
+            values().filterNot { it == OTHER }
+        }
     }
 }
 
