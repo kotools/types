f8f81e2807ec16b284c4eed71e2a9158e9ad2ac0
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-17 01:16:39 +0200

    chore: update the 'kotools.types.multiplatform' Gradle plugin

--- gradle/plugins/src/main/kotlin/kotools/types/multiplatform.gradle.kts
+++ gradle/plugins/src/main/kotlin/kotools/types/multiplatform.gradle.kts
@@ -15,27 +15,26 @@ kotlin {
     js(IR) { browser() }
     jvm {
         compilations.configureEach { kotlinOptions.jvmTarget = "17" }
-        testRuns["test"].executionTask { useJUnitPlatform() }
+        testRuns["test"].executionTask.configure { useJUnitPlatform() }
     }
     linuxX64("linux")
     macosX64("macos")
     mingwX64("windows")
 }
 
-rootProject.plugins.withType<YarnPlugin> {
+rootProject.plugins.withType<YarnPlugin>().configureEach {
     rootProject.the<YarnRootExtension>().lockFileDirectory = projectDir
 }
 
-tasks.withType<Jar> {
+tasks.withType<Jar>().configureEach {
     fun key(suffix: String): String = "Implementation-$suffix"
     val name: Pair<String, String> = key("Title") to project.name
     val version: Pair<String, Any> = key("Version") to project.version
     manifest.attributes(name, version)
 }
 
-val test = tasks.register<Task>("test") {
+tasks.register<Task>("test") {
     group(TaskGroup.RECOMMENDED)
-    description = "Tests this project on the JVM platform."
+    description("Tests this project on the JVM platform.")
     dependsOn += tasks.named<KotlinJvmTest>("jvmTest")
 }
-tasks.check { dependsOn += test }
