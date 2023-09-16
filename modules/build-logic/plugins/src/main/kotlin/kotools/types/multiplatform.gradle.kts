f30a928e2fe35a6420438fb962b2a7cb54d39e2c
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-16 15:35:21 +0200

    chore: add platforms to 'kotools.types.multiplatform' Gradle plugin
    This commit adds the following platform targets: JS, Linux x64, macOS x64 and Windows x64.

--- modules/build-logic/plugins/src/main/kotlin/kotools/types/multiplatform.gradle.kts
+++ modules/build-logic/plugins/src/main/kotlin/kotools/types/multiplatform.gradle.kts
@@ -5,7 +5,16 @@ plugins {
     kotlin("multiplatform")
 }
 
-kotlin.jvm {
-    compilations.configureEach { kotlinOptions.jvmTarget = "17" }
-    testRuns["test"].executionTask { useJUnitPlatform() }
+kotlin {
+    explicitApi()
+    js(IR) { browser() }
+    jvm {
+        compilations.configureEach { kotlinOptions.jvmTarget = "17" }
+        testRuns["test"].executionTask { useJUnitPlatform() }
+    }
+    linuxX64("linux")
+    macosX64("macos")
+    mingwX64("windows")
 }
+
+dependencies { commonMainImplementation(platform(kotlin("bom"))) }
