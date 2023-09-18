7f5d6f2b3ab3d82567ec1ad40b9010d9466e0b92
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-19 00:00:10 +0200

    Revert "chore: use version catalog in build script of 'plugins' project"
    This reverts commit 323ee343c0d9d8ab18dec32f26d023d1e11705bd.

--- gradle/plugins/build.gradle.kts
+++ gradle/plugins/build.gradle.kts
@@ -1,14 +1,18 @@
 plugins { `kotlin-dsl` }
 
 buildscript {
-    dependencies { classpath(libs.dokka.versioning) }
+    dependencies { classpath("org.jetbrains.dokka:versioning-plugin:1.7.20") }
 }
 
 repositories.mavenCentral()
 
 dependencies {
-    implementation(libs.kotlin.gradle)
-    implementation(libs.kotlin.serialization)
-    implementation(libs.dokka.gradle)
-    implementation(libs.dokka.versioning)
+    val kotlinVersion = "1.7.21"
+    implementation(kotlin("gradle-plugin", kotlinVersion))
+    implementation(kotlin("serialization", kotlinVersion))
+    val dokkaVersion = "1.7.20"
+    implementation(
+        "org.jetbrains.dokka:org.jetbrains.dokka.gradle.plugin:$dokkaVersion"
+    )
+    implementation("org.jetbrains.dokka:versioning-plugin:$dokkaVersion")
 }
--- gradle/plugins/settings.gradle.kts
+++
@@ -1,4 +0,0 @@
-dependencyResolutionManagement.versionCatalogs.create("libs") {
-    val files: ConfigurableFileCollection = files("../libs.versions.toml")
-    from(files)
-}
