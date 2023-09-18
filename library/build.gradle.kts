8c0da1729f13c516afdc0d3b903bd0c3d1ebee44
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-19 00:00:10 +0200

    Revert "chore: add 'kotools.types.publication' Gradle plugin"
    This reverts commit e866553a9fced13e8f0615a476ed11ea69735b63.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,51 +1,11 @@
 plugins {
     id("kotools.types.multiplatform")
     id("kotools.types.documentation")
-    id("kotools.types.publication")
 }
 
 version = "4.3.1-SNAPSHOT"
 
 repositories.mavenCentral()
-
-publishing.publications.named<MavenPublication>("kotlinMultiplatform")
-    .configure {
-        groupId = "${project.group}"
-        artifactId = rootProject.name
-        version = "${project.version}"
-    }
-
-publishing.publications.withType<MavenPublication>().configureEach {
-    artifact(tasks.javadocJar)
-    pom {
-        name.set("Kotools Types")
-        description.set(
-            "Kotlin multiplatform library providing explicit types."
-        )
-        val gitRepository = "https://github.com/kotools/types"
-        url.set(gitRepository)
-        licenses {
-            license {
-                name.set("MIT")
-                url.set("https://opensource.org/licenses/MIT")
-            }
-        }
-        issueManagement {
-            system.set("GitHub")
-            url.set("$gitRepository/issues")
-        }
-        scm {
-            connection.set("$gitRepository.git")
-            url.set(gitRepository)
-        }
-        developers {
-            developer {
-                name.set(System.getenv("GIT_USER"))
-                email.set(System.getenv("GIT_EMAIL"))
-            }
-        }
-    }
-}
 
 dependencies {
     // Kotlin
