3849b84ed93a3ad99ecd4de8d6dc95b2d3a47708
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-18 18:08:26 +0200

    chore: remove 'old' Gradle project

--- old/src/commonMain/kotlin/kotools/types/Package.kt
+++
@@ -1,9 +0,0 @@
-package kotools.types
-
-internal object Package {
-    private const val root: String = "kotools.types"
-    const val collection: String = "$root.collection"
-    const val experimental: String = "$root.experimental"
-    const val number: String = "$root.number"
-    const val text: String = "$root.text"
-}
