624ef1bb599a3adf20f7c28b88aca3fa091aae8a
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-19 00:00:18 +0200

    Revert "chore: add 'build-logic' and 'library' to Gradle settings"
    This reverts commit 696c83c0380b13f1a6111e263571f1f961cf17fe.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,1 @@
-rootProject.name = "kotools-types"
-
-val modulesDir: File = rootDir.resolve("modules")
-includeBuild(modulesDir.resolve("build-logic"))
-"library".let {
-    include(it)
-    project(":$it").projectDir = modulesDir.resolve(it)
-}
+rootProject.name = "Kotools Types"
