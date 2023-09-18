b293001e1dec2e978d0ecc6968eda9dc538bad6d
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-19 00:00:17 +0200

    Revert "chore: add 'kotools-types-base' Gradle plugin"
    This reverts commit 3dcf1cea73d0c78b984147071b6d8378d07a2593.

--- modules/build-logic/plugins/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins { `kotlin-dsl` }
-
-repositories.gradlePluginPortal()
-
-dependencies { implementation(kotlin("gradle-plugin", embeddedKotlinVersion)) }
--- modules/library/build.gradle.kts
+++
@@ -1,4 +0,0 @@
-plugins { id("kotools-types-base") }
-
-group = "org.kotools"
-version = "4.3.1-SNAPSHOT"
