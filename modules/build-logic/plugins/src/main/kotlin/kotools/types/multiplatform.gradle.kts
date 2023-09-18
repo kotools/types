21fa4754b41eef3a86555fc92621345552d1dc27
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-19 00:00:13 +0200

    Revert "chore: update the ':library:test' Gradle task"
    This reverts commit 916de71fdc5103251f9dca0b153e97def3d5690f.

--- modules/build-logic/plugins/src/main/kotlin/kotools/types/multiplatform.gradle.kts
+++ modules/build-logic/plugins/src/main/kotlin/kotools/types/multiplatform.gradle.kts
@@ -2,7 +2,6 @@ package kotools.types
 
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
-import org.jetbrains.kotlin.gradle.targets.jvm.tasks.KotlinJvmTest
 
 plugins {
     id("kotools.types.base")
@@ -26,16 +25,13 @@ rootProject.plugins.withType<YarnPlugin> {
     rootProject.the<YarnRootExtension>().lockFileDirectory = projectDir
 }
 
-tasks.withType<Jar> {
-    fun key(suffix: String): String = "Implementation-$suffix"
-    val name: Pair<String, String> = key("Title") to project.name
-    val version: Pair<String, Any> = key("Version") to project.version
-    manifest.attributes(name, version)
+tasks {
+    val test: TaskProvider<Task> = register("test") {
+        group(TaskGroup.LIFECYCLE)
+        description = "Tests this project on the JVM platform."
+        val jvmTest: Task = findByName("jvmTest")
+            ?: error("Can't find the 'jvmTest' task.")
+        dependsOn(jvmTest)
+    }
+    check { dependsOn += test }
 }
-
-val test = tasks.register<Task>("test") {
-    group(TaskGroup.LIFECYCLE)
-    description = "Tests this project on the JVM platform."
-    dependsOn += tasks.named<KotlinJvmTest>("jvmTest")
-}
-tasks.check { dependsOn += test }
