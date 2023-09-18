38edf8bed2daa22b2a5eb7d009dc9b4c060a2c10
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-19 00:00:11 +0200

    Revert "chore: update the 'kotools.types.multiplatform' Gradle plugin"
    This reverts commit f8f81e2807ec16b284c4eed71e2a9158e9ad2ac0.

--- gradle/plugins/src/main/kotlin/kotools/types/multiplatform.gradle.kts
+++ gradle/plugins/src/main/kotlin/kotools/types/multiplatform.gradle.kts
@@ -15,26 +15,27 @@ kotlin {
     js(IR) { browser() }
     jvm {
         compilations.configureEach { kotlinOptions.jvmTarget = "17" }
-        testRuns["test"].executionTask.configure { useJUnitPlatform() }
+        testRuns["test"].executionTask { useJUnitPlatform() }
     }
     linuxX64("linux")
     macosX64("macos")
     mingwX64("windows")
 }
 
-rootProject.plugins.withType<YarnPlugin>().configureEach {
+rootProject.plugins.withType<YarnPlugin> {
     rootProject.the<YarnRootExtension>().lockFileDirectory = projectDir
 }
 
-tasks.withType<Jar>().configureEach {
+tasks.withType<Jar> {
     fun key(suffix: String): String = "Implementation-$suffix"
     val name: Pair<String, String> = key("Title") to project.name
     val version: Pair<String, Any> = key("Version") to project.version
     manifest.attributes(name, version)
 }
 
-tasks.register<Task>("test") {
+val test = tasks.register<Task>("test") {
     group(TaskGroup.RECOMMENDED)
-    description("Tests this project on the JVM platform.")
+    description = "Tests this project on the JVM platform."
     dependsOn += tasks.named<KotlinJvmTest>("jvmTest")
 }
+tasks.check { dependsOn += test }
