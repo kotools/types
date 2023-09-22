55094b06e53b396fbf4b0cbb335d5e75ca5748ac
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-22 23:50:48 +0200

    chore(#237): add script for publishing API reference locally

---
+++ library/publishApiReference.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# This script prepares the publication of the API reference of Kotools Types.
+# It should be called from the root project!
+
+BLUE='\033[0;36m'
+GREEN='\033[0;32m'
+NO_COLOR='\033[0m'
+
+echo "${BLUE}> Synchronize Gradle${NO_COLOR}"
+./gradlew -q :projects --dry-run
+
+PROJECT_VERSION=$(./gradlew -q :library:version)
+echo "${BLUE}> Project version:${NO_COLOR} $PROJECT_VERSION"
+
+echo "${BLUE}> Get archived versions of API reference${NO_COLOR}"
+BRANCH_TARGET="api-reference"
+SOURCE_FOLDER="versions"
+git checkout -q $BRANCH_TARGET -- $SOURCE_FOLDER
+
+echo "${BLUE}> Prepare generation of latest API reference${NO_COLOR}"
+OLD_VERSIONS_DIR="library/api/references"
+mkdir $OLD_VERSIONS_DIR
+git mv $SOURCE_FOLDER/* $OLD_VERSIONS_DIR
+rm -rf $SOURCE_FOLDER
+
+echo "${BLUE}> Generate latest API reference${NO_COLOR}"
+./gradlew -q :library:dokkaHtml
+
+echo "${BLUE}> Archive latest API reference${NO_COLOR}"
+git checkout -q $BRANCH_TARGET
+git rm -qr $SOURCE_FOLDER
+mkdir $SOURCE_FOLDER
+git mv $OLD_VERSIONS_DIR/* $SOURCE_FOLDER
+rm -r $OLD_VERSIONS_DIR
+git add $SOURCE_FOLDER
+git diff-index --quiet HEAD || \
+git commit -qS -m "docs: archive API reference of v$PROJECT_VERSION"
+
+echo "${BLUE}> Publish latest API reference${NO_COLOR}"
+mkdir docs-backup
+mv docs/CNAME docs-backup
+git rm -qr docs
+mkdir docs
+cp -a library/build/dokka/. docs
+mv docs-backup/CNAME docs
+rm -r docs-backup
+git add docs/
+git diff-index --quiet HEAD || \
+git commit -qS -m "docs: publish API reference of v$PROJECT_VERSION"
+git checkout -q -
+
+echo "${GREEN}> SUCCESS${NO_COLOR}"
