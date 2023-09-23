256fe36dd26f5377f47ee9246c99a64b5f272be4
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-23 16:36:37 +0200

    chore: add 'plugins' Gradle build project

---
+++ gradle/plugins/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins { `kotlin-dsl` }
+
+repositories.mavenCentral()
+
+dependencies {
+    implementation(kotlin("gradle-plugin", "1.7.21"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,6 @@
 rootProject.name = "types"
 
+val gradlePluginsDir: File = rootDir.resolve("gradle/plugins")
+includeBuild(gradlePluginsDir)
+
 include("library")
