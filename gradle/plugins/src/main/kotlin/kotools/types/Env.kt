8e9bbccd22ebb50a8f6ddf9720e5f5e713ae6006
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-23 22:36:18 +0200

    chore: refactor 'kotools.types.publication' Gradle plugin

---
+++ gradle/plugins/src/main/kotlin/kotools/types/Env.kt
@@ -0,0 +1,12 @@
+package kotools.types
+
+internal object Env {
+    internal val gitEmail: String? by lazy { env("GIT_EMAIL") }
+    internal val gitUser: String? by lazy { env("GIT_USER") }
+    internal val gpgPassword: String? by lazy { env("GPG_PASSWORD") }
+    internal val gpgPrivateKey: String? by lazy { env("GPG_PRIVATE_KEY") }
+    internal val mavenPassword: String? by lazy { env("MAVEN_PASSWORD") }
+    internal val mavenUsername: String? by lazy { env("MAVEN_USERNAME") }
+
+    private fun env(name: String): String? = System.getenv(name)
+}
--- gradle/plugins/src/main/kotlin/kotools/types/plugins/Publication.kt
+++ gradle/plugins/src/main/kotlin/kotools/types/plugins/Publication.kt
@@ -1,7 +1,9 @@
 package kotools.types.plugins
 
+import kotools.types.Env
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.artifacts.dsl.RepositoryHandler
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.kotlin.dsl.getByType
@@ -13,54 +15,54 @@ public class PublicationPlugin : Plugin<Project> {
     /** Applies this plugin to the given [project]. */
     override fun apply(project: Project) {
         val publishing: PublishingExtension = project.extensions.getByType()
-        publishing.repositories.maven {
-            name = "OSSRH"
-            url = project.uri(
-                "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-            )
-            credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
-            }
-        }
+        publishing.repositories.ossrh(project)
         val signing: SigningExtension = project.extensions.getByType()
-        signing.run {
-            val secretKey: String? = System.getenv("GPG_PRIVATE_KEY")
-            val password: String? = System.getenv("GPG_PASSWORD")
-            useInMemoryPgpKeys(secretKey, password)
-        }
+        signing.useInMemoryPgpKeys(Env.gpgPrivateKey, Env.gpgPassword)
         publishing.publications.withType<MavenPublication>().configureEach {
             signing.sign(this)
-            pom {
-                name.set("Kotools Types")
-                description.set(
-                    "Multiplatform library providing explicit types for Kotlin."
-                )
-                val gitRepository = "https://github.com/kotools/types"
-                url.set(gitRepository)
-                licenses {
-                    license {
-                        name.set("MIT")
-                        url.set("https://opensource.org/licenses/MIT")
-                    }
-                }
-                issueManagement {
-                    system.set("GitHub")
-                    url.set("$gitRepository/issues")
-                }
-                scm {
-                    connection.set("$gitRepository.git")
-                    url.set(gitRepository)
-                }
-                developers {
-                    developer {
-                        val nameValue: String? = System.getenv("GIT_USER")
-                        name.set(nameValue)
-                        val emailValue: String? = System.getenv("GIT_EMAIL")
-                        email.set(emailValue)
-                    }
-                }
-            }
+            configurePom()
+        }
+    }
+}
+
+private fun MavenPublication.configurePom(): Unit = pom {
+    name.set("Kotools Types")
+    description.set(
+        "Multiplatform library providing explicit types for Kotlin."
+    )
+    val gitRepository = "https://github.com/kotools/types"
+    url.set(gitRepository)
+    licenses {
+        license {
+            name.set("MIT")
+            url.set("https://opensource.org/licenses/MIT")
+        }
+    }
+    issueManagement {
+        system.set("GitHub")
+        url.set("$gitRepository/issues")
+    }
+    scm {
+        connection.set("$gitRepository.git")
+        url.set(gitRepository)
+    }
+    developers {
+        developer {
+            name.set(Env.gitUser)
+            email.set(Env.gitEmail)
+        }
+    }
+}
+
+private fun RepositoryHandler.ossrh(project: Project) {
+    maven {
+        name = "OSSRH"
+        url = project.uri(
+            "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+        )
+        credentials {
+            username = Env.mavenUsername
+            password = Env.mavenPassword
         }
     }
 }
