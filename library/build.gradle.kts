789697421f289313c03300721d77e3d1fb33afab
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-23 17:10:22 +0200

    chore: upgrade Gradle wrapper to 8.3
    This reverts commit 078b82ab94efe98f6b13322e046ad6ebd2b5e306.

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -10,7 +10,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript { dependencies.classpath(libs.dokka.versioning) }
 
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlinx.binary.compatibility.validator)
@@ -118,7 +117,7 @@ dependencies {
 enum class TaskGroup {
     DOCUMENTATION, HELP;
 
-    override fun toString(): String = name.toLowerCase()
+    override fun toString(): String = name.lowercase()
 }
 
 fun Task.group(value: TaskGroup) {
