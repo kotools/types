1025243bcdcb08c032b8e0bd8092a37d73ff7b20
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-25 22:37:29 +0200

    chore: update 'gitTag' script

--- scripts/gitTag.sh
+++ scripts/gitTag.sh
@@ -3,6 +3,5 @@
 # This scripts tags a stable release of Kotools Types.
 
 TAG_NAME=$1
-git checkout $TAG_NAME && \
-git tag $TAG_NAME -fs -m "Stable release of Kotools Types $TAG_NAME." && \
-git push origin $TAG_NAME -f
+git tag "$TAG_NAME" -s -m "Stable release of Kotools Types $TAG_NAME." && \
+git push origin "$TAG_NAME" -f
