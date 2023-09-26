25bddf5ab3ec48674f900cebf58e109004710424
Loïc Lamarque <loiclamarque777@gmail.com>
2023-09-26 23:15:48 +0200

    refactor(#234): update internal 'ZeroIntSerializer'

--- src/commonMain/kotlin/kotools/types/number/ZeroInt.kt
+++ src/commonMain/kotlin/kotools/types/number/ZeroInt.kt
@@ -2,10 +2,10 @@ package kotools.types.number
 
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerializationException
-import kotools.types.NUMBER_PACKAGE
 import kotools.types.SinceKotoolsTypes
 import kotools.types.text.NotBlankString
 import kotools.types.text.toNotBlankString
+import kotlin.jvm.JvmSynthetic
 
 /** Representation of the zero integer. */
 @Serializable(ZeroIntSerializer::class)
@@ -17,11 +17,18 @@ public object ZeroInt : PositiveInt, NegativeInt {
 
 internal object ZeroIntSerializer : AnyIntSerializer<ZeroInt> {
     override val serialName: Result<NotBlankString> by lazy {
-        "$NUMBER_PACKAGE.ZeroInt".toNotBlankString()
+        val value: String = ZeroInt::class.qualifiedName
+            ?: error("Unable to get qualified name of 'ZeroInt'.")
+        value.toNotBlankString()
     }
 
     override fun deserialize(value: Int): ZeroInt = if (value == 0) ZeroInt
-    else throw SerializationException(
-        "Unable to deserialize $value to ZeroInt."
-    )
+    else {
+        val message: String = deserializationErrorMessage(value)
+        throw SerializationException(message)
+    }
+
+    @JvmSynthetic
+    internal fun deserializationErrorMessage(value: Int): String =
+        "Unable to deserialize '$value' to ${ZeroInt::class.simpleName}."
 }
--- src/commonTest/kotlin/kotools/types/number/ZeroIntTest.kt
+++ src/commonTest/kotlin/kotools/types/number/ZeroIntTest.kt
@@ -5,11 +5,11 @@ import kotlinx.serialization.SerializationException
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-import kotools.types.NUMBER_PACKAGE
 import kotools.types.shouldEqual
-import kotools.types.shouldHaveAMessage
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+import kotlin.test.assertNotNull
 
 class ZeroIntTest {
     @Test
@@ -25,20 +25,23 @@ class ZeroIntSerializerTest {
     @ExperimentalSerializationApi
     @Test
     fun descriptor_should_have_the_qualified_name_of_ZeroInt_as_serial_name() {
-        val result: String = ZeroInt.serializer().descriptor.serialName
-        result shouldEqual "$NUMBER_PACKAGE.ZeroInt"
+        val actual: String = ZeroInt.serializer().descriptor.serialName
+        val expected: String = assertNotNull(ZeroInt::class.qualifiedName)
+        assertEquals(expected, actual)
     }
 
     @Test
     fun serialize_should_behave_like_the_zero_integer() {
-        val result: String = Json.encodeToString(ZeroInt)
-        result shouldEqual Json.encodeToString(0)
+        val actual: String = Json.encodeToString(ZeroInt)
+        val expected: String = Json.encodeToString(0)
+        assertEquals(expected, actual)
     }
 
     @Test
     fun deserialize_should_pass_with_the_zero_integer() {
-        val result: ZeroInt = Json.decodeFromString("0")
-        result shouldEqual ZeroInt
+        val actual: ZeroInt = Json.decodeFromString("0")
+        val expected = ZeroInt
+        assertEquals(expected, actual)
     }
 
     @Test
@@ -48,6 +51,9 @@ class ZeroIntSerializerTest {
         val encoded: String = Json.encodeToString(value)
         val exception: SerializationException =
             assertFailsWith { Json.decodeFromString<ZeroInt>(encoded) }
-        exception.shouldHaveAMessage()
+        val actualMessage: String = assertNotNull(exception.message)
+        val expectedMessage: String =
+            ZeroIntSerializer.deserializationErrorMessage(value)
+        assertEquals(expectedMessage, actualMessage)
     }
 }
