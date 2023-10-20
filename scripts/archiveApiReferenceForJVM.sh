9d55473ea686bab690bb04052c870080e7501b22
Loïc Lamarque <loiclamarque777@gmail.com>
2023-10-20 23:51:41 +0200

    chore(#261): add script for archiving API reference for JVM

---
+++ scripts/archiveApiReferenceForJVM.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Copyright 2023 Loïc Lamarque.
+# Use of this source code is governed by the MIT license.
+
+# This script regenerates the API reference of an old version and archives it.
+# It should be called from the root project!
+
+PROJECT_VERSION=$1
+
+BLUE='\033[0;36m'
+GREEN='\033[0;32m'
+NO_COLOR='\033[0m'
+
+echo "${BLUE}> Get source code of v$PROJECT_VERSION.${NO_COLOR}"
+rm -r src
+git checkout "$PROJECT_VERSION" -- src
+mkdir src/jvmMain
+cp -a src/main/. src/jvmMain
+rm -r src/main
+rm -r src/test
+
+echo "${BLUE}> Generate API reference.${NO_COLOR}"
+./gradlew -Pversion="$PROJECT_VERSION" -q :clean :dokkaHtml
+git reset --hard -q
+rm -r src/jvmMain
+
+echo "${BLUE}> Archive API reference.${NO_COLOR}"
+git checkout -q api-reference
+cp -a "api/references/$PROJECT_VERSION" versions
+git add "versions/$PROJECT_VERSION"
+git diff-index --quiet HEAD || \
+git commit -qS -m "docs: archive API reference of v$PROJECT_VERSION"
+git checkout -q -
+
+echo "${BLUE}> Clean project.${NO_COLOR}"
+rm -r "api/references/$PROJECT_VERSION"
+
+echo "${GREEN}> SUCCESS${NO_COLOR}"
