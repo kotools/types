788165540d8e705adcc4f9fc02285d1887917d99
Loïc Lamarque <loiclamarque777@gmail.com>
2023-10-20 20:30:20 +0200

    chore(#261): add script for updating published API reference

---
+++ scripts/republishApiReference.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Copyright 2023 Loïc Lamarque.
+# Use of this source code is governed by the MIT license.
+
+# This script republishes the API reference of Kotools Types corresponding to
+# the specified version.
+# It should be called from the root project!
+
+PROJECT_VERSION=$1
+
+BLUE='\033[0;36m'
+GREEN='\033[0;32m'
+NO_COLOR='\033[0m'
+
+echo "${BLUE}> Synchronize Gradle${NO_COLOR}"
+./gradlew -q :unit
+
+echo "${BLUE}> Get source code of version $PROJECT_VERSION${NO_COLOR}"
+git checkout "$PROJECT_VERSION" -- src
+
+echo "${BLUE}> Get archived versions of API reference${NO_COLOR}"
+git checkout -q api-reference -- versions
+mkdir api/references
+git mv versions/* api/references
+rm -rf versions
+
+echo "${BLUE}> Generate API reference for v$PROJECT_VERSION${NO_COLOR}"
+./gradlew -Pversion="$PROJECT_VERSION" -q :clean :dokkaHtml
+git reset --hard -q
+
+echo "${BLUE}> Republish API reference of v$PROJECT_VERSION${NO_COLOR}"
+git checkout -q api-reference
+mkdir docs-backup
+mv docs/CNAME docs-backup
+git rm -qr docs
+mkdir docs
+cp -a build/dokka/. docs
+mv docs-backup/CNAME docs
+rm -r docs-backup
+git add docs/
+git diff-index --quiet HEAD || \
+git commit -qS -m "docs: update API reference of v$PROJECT_VERSION"
+git checkout -q -
+
+echo "${GREEN}> SUCCESS${NO_COLOR}"
