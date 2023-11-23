f64506d0fe4ab2b5479008465754df725526afe0
Loïc Lamarque <loiclamarque777@gmail.com>
2023-11-23 22:17:06 +0100

    chore: add 'java-compatibility' Gradle subproject

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022-2023 Loïc Lamarque.
+ * Copyright 2022-2023 Loïc Lamarque, Kotools S.A.S.U.
  * Use of this source code is governed by the MIT license.
  */
 
@@ -7,3 +7,17 @@ rootProject.name = "types"
 
 val gradlePluginsDir: File = rootDir.resolve("gradle/plugins")
 includeBuild(gradlePluginsDir)
+
+include(KotoolsTypesSubproject.JavaCompatibility)
+
+fun include(subproject: KotoolsTypesSubproject) {
+    include("$subproject")
+    project(":$subproject").projectDir =
+        rootDir.resolve("subprojects/$subproject")
+}
+
+enum class KotoolsTypesSubproject(private val value: String) {
+    JavaCompatibility("java-compatibility");
+
+    override fun toString(): String = value
+}
