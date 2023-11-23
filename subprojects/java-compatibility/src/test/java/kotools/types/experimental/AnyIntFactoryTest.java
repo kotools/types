3b965086cb41c16138dcdcb3bb31d5ae76debf31
Loïc Lamarque <loiclamarque777@gmail.com>
2023-11-23 22:57:15 +0100

    test: add Java compatibility test for 'AnyInt(Int)' function

---
+++ subprojects/java-compatibility/src/test/java/kotools/types/experimental/AnyIntFactoryTest.java
@@ -0,0 +1,18 @@
+package kotools.types.experimental;
+
+import kotools.types.number.AnyInt;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+public class AnyIntFactoryTest {
+    @Test
+    public void createShouldPassWithInt() {
+        final int expected = new Random()
+                .nextInt();
+        final AnyInt number = AnyIntFactory.create(expected);
+        final int actual = number.toInt();
+        Assertions.assertEquals(expected, actual);
+    }
+}
