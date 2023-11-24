78607a15bcdbe4d0a00bc740a3f31b2cd007dfa8
Loïc Lamarque <loiclamarque777@gmail.com>
2023-11-24 11:31:02 +0100

    feat: add 'ExperimentalSince' and 'Since' internal annotations

---
+++ subprojects/internal/src/commonMain/kotlin/Annotations.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2023 Kotools S.A.S.U.
+ * Use of this source code is governed by the MIT license.
+ */
+
+package kotools.types.internal
+
+/**
+ * Specifies the first [version] of Kotools Types where a declaration has
+ * appeared as an **experimental** feature.
+ *
+ * The [version] should be in the following formats: `<major>.<minor>` or
+ * `<major>.<minor>.<patch>`, where _major_, _minor_ and _patch_ are positive
+ * integers without leading zeros.
+ */
+@Retention(AnnotationRetention.BINARY)
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.TYPEALIAS
+)
+public annotation class ExperimentalSince(val version: KotoolsTypesVersion)
+
+/**
+ * Specifies the first [version] of Kotools Types where a declaration has
+ * appeared as a **stable** feature.
+ *
+ * The [version] should be in the following formats: `<major>.<minor>` or
+ * `<major>.<minor>.<patch>`, where _major_, _minor_ and _patch_ are positive
+ * integers without leading zeros.
+ */
+@Retention(AnnotationRetention.BINARY)
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY,
+    AnnotationTarget.TYPEALIAS
+)
+public annotation class Since(val version: KotoolsTypesVersion)
--- subprojects/internal/src/commonMain/kotlin/KotoolsTypesVersion.kt
+++ subprojects/internal/src/commonMain/kotlin/KotoolsTypesVersion.kt
@@ -7,7 +7,8 @@ package kotools.types.internal
 
 /** Represents a version of Kotools Types. */
 public enum class KotoolsTypesVersion {
-    V4_3_2;
+    V4_3_2,
+    V4_2_0;
 
     /** Returns the string representation of this version. */
     override fun toString(): String = name.drop(1)
