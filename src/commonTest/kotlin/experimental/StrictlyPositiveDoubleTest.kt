ffc14096bcf5a8e8c81cc1c10edeac9cbf6dee17
Loïc Lamarque <loiclamarque777@gmail.com>
2023-12-05 02:06:14 +0100

    test: update tests of 'StrictlyPositiveDouble' type

--- src/commonTest/kotlin/experimental/StrictlyPositiveDoubleTest.kt
+++ src/commonTest/kotlin/experimental/StrictlyPositiveDoubleTest.kt
@@ -19,6 +19,7 @@ import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
+import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 @ExperimentalKotoolsTypesApi
@@ -29,7 +30,9 @@ class StrictlyPositiveDoubleTest {
             Random.nextDouble(from = 0.1, until = Double.MAX_VALUE)
         val result: Result<StrictlyPositiveDouble> =
             value.toStrictlyPositiveDouble()
-        result.getOrThrow().toDouble() shouldEqual value
+        val actualValue: Double = result.getOrThrow()
+            .toDouble()
+        assertEquals(expected = value, actualValue)
     }
 
     @Test
@@ -39,8 +42,12 @@ class StrictlyPositiveDoubleTest {
             .unaryMinus()
         val result: Result<StrictlyPositiveDouble> =
             value.toStrictlyPositiveDouble()
-        assertFailsWith<IllegalArgumentException> { result.getOrThrow() }
-            .shouldHaveAMessage()
+        val exception: IllegalArgumentException =
+            assertFailsWith { result.getOrThrow() }
+        val actualMessage: String = assertNotNull(exception.message)
+        val expectedMessage: String = StrictlyPositiveDoubleException(value)
+            .message
+        assertEquals(expectedMessage, actualMessage)
     }
 
     @Test
@@ -51,8 +58,9 @@ class StrictlyPositiveDoubleTest {
         val y: StrictlyPositiveDouble = x.toDouble()
             .toStrictlyPositiveDouble()
             .getOrThrow()
-        val result: Int = x.compareTo(y)
-        result shouldEqual 0
+        val actual: Int = x.compareTo(y)
+        val expected = 0
+        assertEquals(expected, actual)
     }
 
     @Test
@@ -64,8 +72,8 @@ class StrictlyPositiveDoubleTest {
             .nextDouble(from = 0.6, until = Double.MAX_VALUE)
             .toStrictlyPositiveDouble()
             .getOrThrow()
-        val result: Int = x.compareTo(y)
-        assertTrue(result < 0)
+        val actual: Int = x.compareTo(y)
+        assertTrue(actual < 0)
     }
 
     @Test
@@ -77,17 +85,18 @@ class StrictlyPositiveDoubleTest {
         val y: StrictlyPositiveDouble = Random.nextDouble(until = 0.5)
             .toStrictlyPositiveDouble()
             .getOrThrow()
-        val result: Int = x.compareTo(y)
-        assertTrue(result > 0)
+        val actual: Int = x.compareTo(y)
+        assertTrue(actual > 0)
     }
 
     @Test
     fun toString_should_return_the_string_representation_of_its_value() {
         val value: Double = Random.nextDouble()
-        value.toStrictlyPositiveDouble()
+        val actual: String = value.toStrictlyPositiveDouble()
             .getOrThrow()
             .toString()
-            .shouldEqual("$value")
+        val expected: String = value.toString()
+        assertEquals(expected, actual)
     }
 }
 
