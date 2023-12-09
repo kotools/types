0a479f9f157405635cdb696b9403e7dc9aa65a60
Loïc Lamarque <loiclamarque777@gmail.com>
2023-12-09 21:32:32 +0100

    chore: add 'native' source set in 'internal' subproject

--- subprojects/internal/build.gradle.kts
+++ subprojects/internal/build.gradle.kts
@@ -4,6 +4,7 @@
  */
 
 import kotools.types.KotoolsTypesPackage
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
@@ -13,6 +14,16 @@ plugins {
 }
 
 repositories.mavenCentral()
+
+kotlin.sourceSets {
+    val nativeMain: KotlinSourceSet by creating {
+        val commonMain: KotlinSourceSet = commonMain.get()
+        dependsOn(commonMain)
+    }
+    linuxMain.get().dependsOn(nativeMain)
+    macosMain.get().dependsOn(nativeMain)
+    windowsMain.get().dependsOn(nativeMain)
+}
 
 kotlinCompile.javaPackagePrefix.set(KotoolsTypesPackage.Internal)
 
