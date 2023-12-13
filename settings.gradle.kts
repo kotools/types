20be63e8ba20212a235235ce9eae5ddda41bed8b
Loïc Lamarque <loiclamarque777@gmail.com>
2023-12-13 23:40:03 +0100

    chore: remove 'core' Gradle subproject

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ rootProject.name = "types"
 val gradlePluginsDir: File = rootDir.resolve("gradle/plugins")
 includeBuild(gradlePluginsDir)
 
-subprojects("core", "internal", "java-compatibility")
+subprojects("internal", "java-compatibility")
 
 fun subprojects(first: String, vararg others: String) {
     val values: Set<String> = setOf(first) + others
