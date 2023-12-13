f73f8868f16ee01457df5f29384b4392f1bce480
Loïc Lamarque <loiclamarque777@gmail.com>
2023-12-13 23:24:48 +0100

    refactor: update Gradle settings script of root project

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,24 +8,13 @@ rootProject.name = "types"
 val gradlePluginsDir: File = rootDir.resolve("gradle/plugins")
 includeBuild(gradlePluginsDir)
 
-include(KotoolsTypesSubproject.Core)
-include(KotoolsTypesSubproject.Internal)
-include(KotoolsTypesSubproject.JavaCompatibility)
+subprojects("core", "internal", "java-compatibility")
 
-fun include(subproject: KotoolsTypesSubproject) {
-    include("$subproject")
-    project(":$subproject").projectDir =
-        rootDir.resolve("subprojects/$subproject")
-}
-
-enum class KotoolsTypesSubproject(private val value: String) {
-    Core("core"),
-    Internal("internal"),
-    JavaCompatibility("java-compatibility");
-
-    init {
-        require(value.isNotBlank()) { "Subproject name shouldn't be blank." }
+fun subprojects(first: String, vararg others: String) {
+    val values: Set<String> = setOf(first) + others
+    values.forEach {
+        require(it.isNotBlank()) { "Subproject name shouldn't be blank." }
+        include(it)
+        project(":$it").projectDir = rootDir.resolve("subprojects/$it")
     }
-
-    override fun toString(): String = value
 }
