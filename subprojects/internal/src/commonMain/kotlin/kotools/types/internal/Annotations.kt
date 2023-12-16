4edcebd1fa8b360ba1b1e238f209522e36f742d7
Loïc Lamarque <loiclamarque777@gmail.com>
2023-12-16 16:12:47 +0100

    feat: remove DeprecatedSinceKotoolsTypes internal annotation
    This commit reverts 5381636d4c016e812d32ad1cfff1e3f180ea5821.
    
    Refs: #251.

--- subprojects/internal/src/commonMain/kotlin/kotools/types/internal/Annotations.kt
+++ subprojects/internal/src/commonMain/kotlin/kotools/types/internal/Annotations.kt
@@ -36,27 +36,3 @@ public annotation class ExperimentalSince(val version: KotoolsTypesVersion)
 @Retention(SOURCE)
 @Target(CLASS, FUNCTION, PROPERTY, TYPEALIAS)
 public annotation class Since(val version: KotoolsTypesVersion)
-
-/**
- * Specifies the first versions of Kotools Types where a declaration has
- * appeared as a **deprecated** feature.
- *
- * The specified [warningSince] should match the version where the declaration
- * was deprecated with a [warning][DeprecationLevel.WARNING] level.
- * The specified [errorSince] should match the version where it was deprecated
- * with an [error][DeprecationLevel.ERROR] level.
- * The specified [hiddenSince] should match the version where the declaration
- * was deprecated with a [hidden][DeprecationLevel.HIDDEN] level.
- *
- * The versions should be in the following formats: `<major>.<minor>` or
- * `<major>.<minor>.<patch>`, where _major_, _minor_ and _patch_ are positive
- * integers without leading zeros.
- */
-@MustBeDocumented
-@Retention(SOURCE)
-@Target(CLASS, FUNCTION, PROPERTY, TYPEALIAS)
-public annotation class DeprecatedSinceKotoolsTypes(
-    val warningSince: KotoolsTypesVersion,
-    val errorSince: KotoolsTypesVersion = KotoolsTypesVersion.Unreleased,
-    val hiddenSince: KotoolsTypesVersion = KotoolsTypesVersion.Unreleased
-)
