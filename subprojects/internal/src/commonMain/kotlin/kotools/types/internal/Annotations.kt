6b5ee9f1bcbba1a05b788f3b1b137a67f0789eb8
Loïc Lamarque <loiclamarque777@gmail.com>
2023-12-16 16:15:01 +0100

    feat: add DeprecatedSince internal annotation
    Refs: #251.

--- subprojects/internal/src/commonMain/kotlin/kotools/types/internal/Annotations.kt
+++ subprojects/internal/src/commonMain/kotlin/kotools/types/internal/Annotations.kt
@@ -36,3 +36,23 @@ public annotation class ExperimentalSince(val version: KotoolsTypesVersion)
 @Retention(SOURCE)
 @Target(CLASS, FUNCTION, PROPERTY, TYPEALIAS)
 public annotation class Since(val version: KotoolsTypesVersion)
+
+/**
+ * Specifies the first versions of Kotools Types where a declaration has
+ * appeared as a **deprecated** feature.
+ *
+ * The specified [warningSince] should match the version where the declaration
+ * was deprecated with a [warning][DeprecationLevel.WARNING] level.
+ * The specified [errorSince] should match the version where it was deprecated
+ * with an [error][DeprecationLevel.ERROR] level.
+ * The specified [hiddenSince] should match the version where the declaration
+ * was deprecated with a [hidden][DeprecationLevel.HIDDEN] level.
+ */
+@MustBeDocumented
+@Retention(SOURCE)
+@Target(CLASS, FUNCTION, PROPERTY, TYPEALIAS)
+public annotation class DeprecatedSince(
+    val warningSince: KotoolsTypesVersion,
+    val errorSince: KotoolsTypesVersion = KotoolsTypesVersion.Unreleased,
+    val hiddenSince: KotoolsTypesVersion = KotoolsTypesVersion.Unreleased
+)
