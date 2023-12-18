b75d70c1479043579b069d986a90bb9a9d29e34b
Loïc Lamarque <loiclamarque777@gmail.com>
2023-12-18 13:47:34 +0100

    feat: add requirement to internal 'KotoolsTypesPackage'

--- subprojects/internal/src/commonMain/kotlin/kotools/types/internal/KotoolsTypesPackage.kt
+++ subprojects/internal/src/commonMain/kotlin/kotools/types/internal/KotoolsTypesPackage.kt
@@ -5,10 +5,18 @@
 
 package kotools.types.internal
 
+private val regex: Regex by lazy { Regex("^[a-z]+(\\.[a-z]+)*\$") }
+
 /** Represents a package of Kotools Types. */
 public enum class KotoolsTypesPackage(private val value: String) {
     Number("kotools.types.number"),
     Text("kotools.types.text");
+
+    init {
+        require(value matches regex) {
+            "Package should match the regular expression $regex"
+        }
+    }
 
     /**
      * Returns the string representation of this package.
