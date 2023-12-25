a7c9c83091f3154f8c1de5f4b07b91697b001c44
Loïc Lamarque <loiclamarque777@gmail.com>
2023-12-26 00:11:57 +0100

    refactor: reorder declarations of 'EmailAddress'
    Refs: #339

--- src/commonTest/kotlin/kotools/types/experimental/EmailAddressTest.kt
+++ src/commonTest/kotlin/kotools/types/experimental/EmailAddressTest.kt
@@ -12,6 +12,51 @@ import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
 @ExperimentalKotoolsTypesApi
+class EmailAddressCompanionTest {
+    @Test
+    fun regex_should_pass() {
+        val actual: Regex = EmailAddress.regex
+        val expected = Regex("^\\S+@\\S+\\.\\S+\$")
+        assertEquals(expected.pattern, actual.pattern)
+    }
+
+    @Test
+    fun from_should_pass_with_a_valid_CharSequence() {
+        val text: CharSequence = "contact@kotools.org"
+        val actual: EmailAddress? = EmailAddress from text
+        assertNotNull(actual)
+    }
+
+    @Test
+    fun from_should_fail_with_a_CharSequence_having_an_invalid_local_part() {
+        val text: CharSequence = " contact@kotools.org"
+        val actual: EmailAddress? = EmailAddress from text
+        assertNull(actual)
+    }
+
+    @Test
+    fun from_should_fail_with_a_CharSequence_that_does_not_have_an_at_sign() {
+        val text: CharSequence = "contact-kotools.org"
+        val actual: EmailAddress? = EmailAddress from text
+        assertNull(actual)
+    }
+
+    @Test
+    fun from_should_fail_with_a_CharSequence_having_an_invalid_domain() {
+        val text: CharSequence = "contact@ko tools. org"
+        val actual: EmailAddress? = EmailAddress from text
+        assertNull(actual)
+    }
+
+    @Test
+    fun from_should_fail_with_a_CharSequence_that_does_not_have_a_dot() {
+        val text: CharSequence = "contact@kotools_org"
+        val actual: EmailAddress? = EmailAddress from text
+        assertNull(actual)
+    }
+}
+
+@ExperimentalKotoolsTypesApi
 class EmailAddressTest {
     @Test
     fun structural_equality_should_pass_with_the_same_object() {
@@ -63,50 +108,5 @@ class EmailAddressTest {
         val text = "contact@kotools.org"
         val emailAddress: EmailAddress = checkNotNull(EmailAddress from text)
         assertEquals(expected = text, actual = "$emailAddress")
-    }
-}
-
-@ExperimentalKotoolsTypesApi
-class EmailAddressCompanionTest {
-    @Test
-    fun regex_should_pass() {
-        val actual: Regex = EmailAddress.regex
-        val expected = Regex("^\\S+@\\S+\\.\\S+\$")
-        assertEquals(expected.pattern, actual.pattern)
-    }
-
-    @Test
-    fun from_should_pass_with_a_valid_CharSequence() {
-        val text: CharSequence = "contact@kotools.org"
-        val actual: EmailAddress? = EmailAddress from text
-        assertNotNull(actual)
-    }
-
-    @Test
-    fun from_should_fail_with_a_CharSequence_having_an_invalid_local_part() {
-        val text: CharSequence = " contact@kotools.org"
-        val actual: EmailAddress? = EmailAddress from text
-        assertNull(actual)
-    }
-
-    @Test
-    fun from_should_fail_with_a_CharSequence_that_does_not_have_an_at_sign() {
-        val text: CharSequence = "contact-kotools.org"
-        val actual: EmailAddress? = EmailAddress from text
-        assertNull(actual)
-    }
-
-    @Test
-    fun from_should_fail_with_a_CharSequence_having_an_invalid_domain() {
-        val text: CharSequence = "contact@ko tools. org"
-        val actual: EmailAddress? = EmailAddress from text
-        assertNull(actual)
-    }
-
-    @Test
-    fun from_should_fail_with_a_CharSequence_that_does_not_have_a_dot() {
-        val text: CharSequence = "contact@kotools_org"
-        val actual: EmailAddress? = EmailAddress from text
-        assertNull(actual)
     }
 }
