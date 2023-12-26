0d06735171486d7c8695e4fd87f3ffb058e38203
Loïc Lamarque <loiclamarque777@gmail.com>
2023-12-26 17:31:08 +0100

    refactor: update function 'KotoolsTypesPackage.toString()'

--- subprojects/internal/src/commonMain/kotlin/kotools/types/internal/KotoolsTypesPackage.kt
+++ subprojects/internal/src/commonMain/kotlin/kotools/types/internal/KotoolsTypesPackage.kt
@@ -10,5 +10,5 @@ public enum class KotoolsTypesPackage {
     Experimental, Number, Text;
 
     /** Returns the string representation of this package. */
-    override fun toString(): String = "kotools.types.${name.lowercase()}"
+    override fun toString(): String = "kotools.types.${this.name.lowercase()}"
 }
