f339080e8aa34b29e5d31589efe6d88d03e9bdab
Loïc Lamarque <loiclamarque777@gmail.com>
2024-01-29 21:58:19 +0100

    docs: add RFC-1035 in KDoc of DomainName type
    Refs: #340

--- src/commonMain/kotlin/org/kotools/types/web/DomainName.kt
+++ src/commonMain/kotlin/org/kotools/types/web/DomainName.kt
@@ -6,7 +6,9 @@ import kotools.types.internal.KotoolsTypesVersion
 
 /**
  * Represents a
- * [domain name in the Internet](https://en.wikipedia.org/wiki/Domain_name).
+ * [domain name in the Internet](https://en.wikipedia.org/wiki/Domain_name),
+ * with a syntax based on the
+ * [Request For Comments number 1035 (RFC-1035)](https://datatracker.ietf.org/doc/html/rfc1035#section-2.3.1).
  */
 @ExperimentalKotoolsTypesApi
 @ExperimentalSince(KotoolsTypesVersion.Unreleased)
