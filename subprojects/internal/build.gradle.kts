38aa12458ab79dbbd10c3da0af8e95310b4baa12
Loïc Lamarque <loiclamarque777@gmail.com>
2024-02-02 00:24:21 +0100

    chore: configure 'internal' Gradle subproject for publication
    Refs: #406

--- subprojects/internal/build.gradle.kts
+++ subprojects/internal/build.gradle.kts
@@ -1,11 +1,21 @@
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    `maven-publish`
+    signing
     id("kotools.types.base")
     id("kotools.types.multiplatform")
+    id("kotools.types.publication")
 }
 
 repositories.mavenCentral()
+
+publishing.publications.named<MavenPublication>("kotlinMultiplatform")
+    .configure {
+        groupId = "${project.group}"
+        artifactId = project.name
+        version = "${project.version}"
+    }
 
 dependencies {
     commonMainImplementation(libs.kotlinx.serialization.core)
