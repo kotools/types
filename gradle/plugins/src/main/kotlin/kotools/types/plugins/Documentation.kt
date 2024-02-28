82d237cc35037de39d456ef9dcf35027b4665e6c
Loïc Lamarque <loiclamarque777@gmail.com>
2024-02-28 23:16:20 +0100

    ♻️ Refactor documentation Gradle plugin (#379)
    Removes useless 'this' keywords.

--- gradle/plugins/src/main/kotlin/kotools/types/plugins/Documentation.kt
+++ gradle/plugins/src/main/kotlin/kotools/types/plugins/Documentation.kt
@@ -5,7 +5,9 @@ import kotools.types.tasks.description
 import kotools.types.tasks.group
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.file.Directory
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.provider.Provider
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.tasks.Copy
@@ -36,83 +38,79 @@ public class DocumentationPlugin : Plugin<Project> {
 }
 
 private fun TaskContainer.dokkaTasks(project: Project): Unit =
-    this.withType<DokkaTask>().configureEach {
-        project.layout.buildDirectory.dir("dokka")
-            .let { this.outputDirectory.set(it) }
-        this.dokkaSourceSets.configureEach {
-            this.skipEmptyPackages.set(true)
+    withType<DokkaTask>().configureEach {
+        val dokkaDirectory: Provider<Directory> =
+            project.layout.buildDirectory.dir("dokka")
+        outputDirectory.set(dokkaDirectory)
+        dokkaSourceSets.configureEach { skipEmptyPackages.set(true) }
+        pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
+            customAssets = listOf(project.logoIcon)
+            footerMessage = project.copyrightNotice
         }
-        this.pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
-            this.customAssets = listOf(project.logoIcon)
-            this.footerMessage = project.copyrightNotice
-        }
-        this.pluginConfiguration<VersioningPlugin, VersioningConfiguration> {
-            this.version = project.version.toString()
-            this.olderVersionsDir = project.file("api/references")
+        pluginConfiguration<VersioningPlugin, VersioningConfiguration> {
+            version = project.version.toString()
+            olderVersionsDir = project.file("api/references")
         }
     }
 
 private val Project.copyrightNotice: String
-    get() = this.rootDir.resolve("LICENSE.txt")
-        .useLines { lines: Sequence<String> ->
-            lines.first { it.startsWith("Copyright (c)") }
-        }
+    get() = rootDir.resolve("LICENSE.txt").useLines { lines: Sequence<String> ->
+        lines.first { it.startsWith("Copyright (c)") }
+    }
 
 private val Project.logoIcon: File
-    get() = this.rootDir.resolve("dokka/logo-icon.svg")
+    get() = rootDir.resolve("dokka/logo-icon.svg")
 
 private fun TaskContainer.apiReferenceJar(project: Project) {
-    val apiReferenceJar: TaskProvider<Jar> =
-        this.register<Jar>("apiReferenceJar") {
-            this.group(TaskGroup.BUILD)
-            this.description("Archives the API reference in a JAR file.")
-            this.from(dokkaHtml)
-            this.archiveClassifier.set("javadoc")
-        }
-    this.named("assemble").configure { this.dependsOn += apiReferenceJar }
+    val apiReferenceJar: TaskProvider<Jar> = register<Jar>("apiReferenceJar") {
+        group(TaskGroup.BUILD)
+        description("Archives the API reference in a JAR file.")
+        from(dokkaHtml)
+        archiveClassifier.set("javadoc")
+    }
+    named("assemble").configure { dependsOn += apiReferenceJar }
     project.includeInPublication(apiReferenceJar)
 }
 
 private val TaskContainer.dokkaHtml: TaskProvider<DokkaTask>
-    get() = this.named<DokkaTask>("dokkaHtml")
+    get() = named<DokkaTask>("dokkaHtml")
 
-private fun Project.includeInPublication(jar: TaskProvider<Jar>): Unit = this
-    .extensions
-    .getByType<PublishingExtension>()
-    .publications
-    .withType<MavenPublication>()
-    .configureEach { this.artifact(jar) }
+private fun Project.includeInPublication(jar: TaskProvider<Jar>): Unit =
+    extensions.getByType<PublishingExtension>()
+        .publications
+        .withType<MavenPublication>()
+        .configureEach { artifact(jar) }
 
 private fun TaskContainer.saveApiReference() {
     val saveApiReference: TaskProvider<Copy> =
-        this.register<Copy>("saveApiReference") {
-            this.group(TaskGroup.DOCUMENTATION)
-            this.description("Archives the API reference.")
-            this.from(dokkaHtml)
-            this.exclude("older/**")
-            this.into("api/references/${project.version}")
+        register<Copy>("saveApiReference") {
+            group(TaskGroup.DOCUMENTATION)
+            description("Archives the API reference.")
+            from(dokkaHtml)
+            exclude("older/**")
+            into("api/references/${project.version}")
         }
-    this.assembleApiReferenceForWebsite(saveApiReference)
+    assembleApiReferenceForWebsite(saveApiReference)
 }
 
 private fun TaskContainer.assembleApiReferenceForWebsite(
     saveApiReference: TaskProvider<Copy>
-): Unit = this.register("assembleApiReferenceForWebsite").configure {
-    this.group(TaskGroup.BUILD)
-    this.description("Assembles the API reference for our website.")
-    this.dependsOn += saveApiReference
+): Unit = register("assembleApiReferenceForWebsite").configure {
+    group(TaskGroup.BUILD)
+    description("Assembles the API reference for our website.")
+    dependsOn += saveApiReference
 }
 
 private fun TaskContainer.cleanDokkaHtml() {
     val dokkaHtml: TaskProvider<DokkaTask> = this.dokkaHtml
     val cleanDokkaHtml: TaskProvider<Delete> =
-        this.register<Delete>("cleanDokkaHtml") {
-            this.group(TaskGroup.BUILD)
-            this.description(
+        register<Delete>("cleanDokkaHtml") {
+            group(TaskGroup.BUILD)
+            description(
                 "Deletes the output directory of the '${dokkaHtml.name}' task."
             )
             val target: DirectoryProperty = dokkaHtml.get().outputDirectory
-            this.setDelete(target)
+            setDelete(target)
         }
-    this.named<Delete>("clean").configure { this.dependsOn += cleanDokkaHtml }
+    named<Delete>("clean").configure { dependsOn += cleanDokkaHtml }
 }
