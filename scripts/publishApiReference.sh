dcb5f6c61ea109706c6cbddf6d11a2b117878f95
Loïc Lamarque <loiclamarque777@gmail.com>
2024-03-05 12:45:04 +0100

    🔨 Parse 1st argument in API reference's publication script (#561)
    This optional argument is for specifying another branch target than the default one, which is `api-reference`.

--- scripts/publishApiReference.sh
+++ scripts/publishApiReference.sh
@@ -2,10 +2,16 @@
 
 # This script prepares the publication of the API reference of Kotools Types.
 # It should be called from the root project!
+# It accepts an argument for publishing the API reference on another branch than
+# 'api-reference', which is the default branch used if no argument is specified.
+# IMPORTANT: Make sure that the specified branch exists in the Git repository!
 
 BLUE='\033[0;36m'
 GREEN='\033[0;32m'
 NO_COLOR='\033[0m'
+
+BRANCH_TARGET=$([ -n "$1" ] && echo "$1" || echo "api-reference")
+echo "$BLUE> Branch target:$NO_COLOR $BRANCH_TARGET"
 
 echo "${BLUE}> Synchronize Gradle${NO_COLOR}"
 ./gradlew -q :unit
@@ -14,9 +20,8 @@ PROJECT_VERSION=$(./gradlew -q :version)
 echo "${BLUE}> Project version:${NO_COLOR} $PROJECT_VERSION"
 
 echo "${BLUE}> Get archived versions of API reference${NO_COLOR}"
-BRANCH_TARGET="api-reference"
 SOURCE_FOLDER="versions"
-git checkout -q $BRANCH_TARGET -- $SOURCE_FOLDER
+git checkout -q api-reference -- $SOURCE_FOLDER
 
 echo "${BLUE}> Prepare generation of latest API reference${NO_COLOR}"
 OLD_VERSIONS_DIR="api/references"
@@ -28,7 +33,7 @@ echo "${BLUE}> Generate latest API reference${NO_COLOR}"
 ./gradlew -q :assembleApiReferenceForWebsite
 
 echo "${BLUE}> Archive latest API reference${NO_COLOR}"
-git checkout -q $BRANCH_TARGET
+git checkout -q "$BRANCH_TARGET"
 git rm -qr $SOURCE_FOLDER
 mkdir $SOURCE_FOLDER
 mv $OLD_VERSIONS_DIR/* $SOURCE_FOLDER
