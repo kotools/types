5d3e6739fcd7fbbb9e5639af7358bebd1e8a4b70
Loïc Lamarque <loiclamarque777@gmail.com>
2024-03-08 18:03:32 +0100

    🔨 Update Git tag script
    This script now creates a tag from the project's version defined in the Gradle build script.

--- scripts/gitTag.sh
+++ scripts/gitTag.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-# This script tags a stable release of Kotools Types.
+# This script creates a Git annotated tag using the project's version specified.
 
-TAG_NAME=$1
-git tag "$TAG_NAME" -s -m "Stable release of Kotools Types $TAG_NAME."
+BLUE='\033[0;36m'
+GREEN='\033[0;32m'
+NO_COLOR='\033[0m'
+
+echo "${BLUE}> Synchronize Gradle${NO_COLOR}"
+./gradlew -q :unit
+
+PROJECT_VERSION=$(./gradlew -q :version)
+echo "${BLUE}> Project version:${NO_COLOR} $PROJECT_VERSION"
+
+git tag "$PROJECT_VERSION" -s -m "🔖 Kotools Types $PROJECT_VERSION" \
+  && echo "${GREEN}> Successfully created tag${NO_COLOR}"
