63f1d10ed42a858ef10e103f2c65c30b8fe23abe
Loïc Lamarque <loiclamarque777@gmail.com>
2024-03-09 11:53:56 +0100

    📝 Improve docs of `NonZeroInt.Companion.createOrNull` (#351)
    Indicates that this function may involve rounding or truncation.

--- src/commonMain/kotlin/kotools/types/number/NonZeroInt.kt
+++ src/commonMain/kotlin/kotools/types/number/NonZeroInt.kt
@@ -130,8 +130,9 @@ public sealed interface NonZeroInt : AnyInt {
         }
 
         /**
-         * Creates a [NonZeroInt] from the specified [number], or returns `null`
-         * if the [number] equals zero.
+         * Creates a [NonZeroInt] from the specified [number], which may involve
+         * rounding or truncation, or returns `null` if the [number] equals
+         * zero.
          *
          * <br>
          * <details open>
