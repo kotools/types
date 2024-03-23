58c31b768d1d557bebc1886d2af82cc38064fef3
Loïc Lamarque <loiclamarque777@gmail.com>
2024-03-23 16:41:59 +0100

    🔥 Remove sample code from `v5` Gradle project

--- v5/src/commonMain/kotlin/org/kotools/types/Temporary.kt
+++
@@ -1,7 +0,0 @@
-package org.kotools.types
-
-/** A temporary object created for testing purpose. */
-public object Temporary {
-    /** The value of this object. */
-    public const val VALUE: Boolean = true
-}
