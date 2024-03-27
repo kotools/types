9f4b6bc5de8105c387561d806eae597891f2c622
Loïc Lamarque <loiclamarque777@gmail.com>
2024-03-27 22:12:33 +0100

    ✅ Improve Java compatibility tests of `EmailAddress`

--- subprojects/java-compatibility/src/test/java/kotools/types/web/EmailAddressTest.java
+++ subprojects/java-compatibility/src/test/java/kotools/types/web/EmailAddressTest.java
@@ -1,129 +1,76 @@
 package kotools.types.web;
 
-import kotools.types.internal.ErrorMessage;
-import kotools.types.internal.ErrorMessageKt;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.util.Random;
-import java.util.Set;
-
 public class EmailAddressTest {
-    private static final Set<String> VALID_TEXTS = Set.of(
-            "contact@kotools.org",
-            "cont.act@kotools.org",
-            "cont-act@kotools.org",
-            "cont_act@kotools.org",
-            "contact.123@kotools.org",
-            "123contact@kotools.org",
-            "contact123@kotools.org",
-            "cont123act@kotools.org"
-    );
-    private static final Set<String> INVALID_TEXTS = Set.of(
-            " contact@kotools.org",
-            "contact-kotools.org",
-            "contact@ko tools. org",
-            "contact@kotools_org",
-            "cont..act@kotools_org",
-            "cont.-act@kotools_org",
-            "cont._act@kotools_org",
-            "cont-.act@kotools_org",
-            "cont--act@kotools_org",
-            "cont-_act@kotools_org",
-            "cont_.act@kotools_org",
-            "cont_-act@kotools_org",
-            "cont__act@kotools_org"
-    );
-
     @Nested
     public class Companion {
         @Test
-        public void create_should_pass_with_a_valid_String() {
-            VALID_TEXTS.forEach(EmailAddress.Companion::create);
+        public void create_should_be_compatible_with_Java() {
+            final String value = "contact@kotools.org";
+            final String type = EmailAddress.class.getSimpleName();
+            final String message = "Creating an '%s' from '%s' should pass."
+                    .formatted(type, value);
+            Assertions.assertDoesNotThrow(
+                    () -> EmailAddress.Companion.create(value),
+                    message
+            );
         }
 
         @Test
-        public void create_should_fail_with_an_invalid_String() {
-            INVALID_TEXTS.forEach(text -> {
-                final IllegalArgumentException exception =
-                        Assertions.assertThrows(
-                                IllegalArgumentException.class,
-                                () -> EmailAddress.Companion.create(text)
-                        );
-                final ErrorMessage actual =
-                        ErrorMessageKt.ErrorMessage(exception);
-                final ErrorMessage expected =
-                        ErrorMessage.Companion.invalidEmailAddress(text);
-                Assertions.assertEquals(expected, actual);
-            });
-        }
-
-        @Test
-        public void createOrNull_should_pass_with_a_valid_String() {
-            VALID_TEXTS.forEach(text -> {
-                final EmailAddress actual =
-                        EmailAddress.Companion.createOrNull(text);
-                Assertions.assertNotNull(actual);
-            });
-        }
-
-        @Test
-        public void createOrNull_should_fail_with_an_invalid_String() {
-            INVALID_TEXTS.forEach(text -> {
-                final EmailAddress actual =
-                        EmailAddress.Companion.createOrNull(text);
-                Assertions.assertNull(actual);
-            });
+        public void createOrNull_should_be_compatible_with_Java() {
+            final String value = "contact@kotools.org";
+            final EmailAddress actual =
+                    EmailAddress.Companion.createOrNull(value);
+            final String type = EmailAddress.class.getSimpleName();
+            final String message = "Creating an '%s' from '%s' should pass."
+                    .formatted(type, value);
+            Assertions.assertNotNull(actual, message);
         }
     }
 
     @Test
     public void constructor_should_be_compatible_with_Java() {
-        Assertions.assertDoesNotThrow(
-                () -> new EmailAddress("contact@kotools.org")
-        );
+        final String value = "contact@kotools.org";
+        final String type = EmailAddress.class.getSimpleName();
+        final String message = "Creating an '%s' from '%s' should pass."
+                .formatted(type, value);
+        Assertions.assertDoesNotThrow(() -> new EmailAddress(value), message);
     }
 
     @Test
-    public void structural_equality_should_pass_with_another_EmailAddress_having_the_same_string_representation() {
-        final Object[] validTexts = VALID_TEXTS.toArray();
-        final int index = new Random()
-                .nextInt(validTexts.length);
-        final String text = validTexts[index].toString();
-        final EmailAddress first = EmailAddress.Companion.create(text);
-        final EmailAddress second = EmailAddress.Companion.create(text);
-        Assertions.assertEquals(first, second);
-        final int hashCode1 = first.hashCode();
-        final int hashCode2 = second.hashCode();
-        Assertions.assertEquals(hashCode1, hashCode2);
+    public void equals_should_be_compatible_with_Java() {
+        final String value = "contact@kotools.org";
+        final EmailAddress first = EmailAddress.Companion.create(value);
+        final EmailAddress second = EmailAddress.Companion.create(value);
+        final boolean actual = first.equals(second);
+        final String message =
+                "Comparing 2 email addresses with the same value should pass.";
+        Assertions.assertTrue(actual, message);
     }
 
     @Test
-    public void structural_equality_should_fail_with_another_EmailAddress_having_another_string_representation() {
-        final Object[] validTexts = VALID_TEXTS.toArray();
-        final int index = new Random()
-                .nextInt(validTexts.length);
-        final String text1 = validTexts[index].toString();
-        final EmailAddress first = EmailAddress.Companion.create(text1);
-        final String text2 = "support.%s".formatted(text1);
-        final EmailAddress second = EmailAddress.Companion.create(text2);
-        Assertions.assertNotEquals(first, second);
-        final int hashCode1 = first.hashCode();
-        final int hashCode2 = second.hashCode();
-        Assertions.assertNotEquals(hashCode1, hashCode2);
+    public void hashCode_should_be_compatible_with_Java() {
+        final String value = "contact@kotools.org";
+        final int actual = EmailAddress.Companion.create(value)
+                .hashCode();
+        final int expected = EmailAddress.Companion.create(value)
+                .hashCode();
+        final String message = "Comparing the hash code of 2 email addresses " +
+                "having the same value should pass.";
+        Assertions.assertEquals(expected, actual, message);
     }
 
     @Test
-    public void toString_should_pass() {
-        final Object[] validTexts = VALID_TEXTS.toArray();
-        final int index = new Random()
-                .nextInt(validTexts.length);
-        final String text = validTexts[index].toString();
-        final EmailAddress address =
-                EmailAddress.Companion.createOrNull(text);
-        Assertions.assertNotNull(address);
-        final String actual = address.toString();
-        Assertions.assertEquals(text, actual);
+    public void toString_should_be_compatible_with_Java() {
+        final String expected = "contact@kotools.org";
+        final String actual = EmailAddress.Companion.create(expected)
+                .toString();
+        final String type = EmailAddress.class.getSimpleName();
+        final String message = "'%s.toString()' should return '%s'."
+                .formatted(type, expected);
+        Assertions.assertEquals(expected, actual, message);
     }
 }
