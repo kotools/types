a36d28e91437c8410914a639d693839a08697135
Loïc Lamarque <loiclamarque777@gmail.com>
2024-03-30 22:24:51 +0100

    💚 Add missing `@PathSensitive` in `ExtractCodeSamples` task (#610)

--- gradle/plugins/src/main/kotlin/org/kotools/types/samples/ExtractCodeSamples.kt
+++ gradle/plugins/src/main/kotlin/org/kotools/types/samples/ExtractCodeSamples.kt
@@ -5,6 +5,8 @@ import org.gradle.api.file.Directory
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.PathSensitive
+import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
 import org.gradle.work.DisableCachingByDefault
 import java.io.File
@@ -16,6 +18,7 @@ import java.io.File
 public abstract class ExtractCodeSamples : DefaultTask() {
     /** The directory containing the sources to extract code samples from. */
     @get:InputDirectory
+    @get:PathSensitive(PathSensitivity.NONE)
     public abstract val sourceDirectory: DirectoryProperty
 
     /** The directory that will contain the code samples. */
