d59f69e3663b84cb487a40981381cec47b2c6cf5
Loïc Lamarque <loiclamarque777@gmail.com>
2024-03-31 13:42:18 +0200

    📝 Add code samples to documentation of `Zero.toString()`

--- src/commonMain/kotlin/org/kotools/types/Zero.kt
+++ src/commonMain/kotlin/org/kotools/types/Zero.kt
@@ -14,7 +14,31 @@ public object Zero {
 
     // ----------------------- Overrides from kotlin.Any -----------------------
 
-    /** Returns the string representation of this number. */
+    /**
+     * Returns the string representation of this number.
+     *
+     * <br>
+     * <details open>
+     * <summary>
+     *     <b>Calling from Kotlin</b>
+     * </summary>
+     *
+     * Here's an example of calling this function from Kotlin code:
+     *
+     * SAMPLE: ZeroKotlinSample.toStringSample.md
+     * </details>
+     *
+     * <br>
+     * <details>
+     * <summary>
+     *     <b>Calling from Java</b>
+     * </summary>
+     *
+     * Here's an example of calling this function from Java code:
+     *
+     * SAMPLE: ZeroJavaSample.toStringSample.md
+     * </details>
+     */
     @Suppress("RedundantModalityModifier")
     final override fun toString(): String = VALUE.toString()
 }
