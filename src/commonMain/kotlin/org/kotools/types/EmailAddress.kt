db34c51fe429837d5d92297ece40cbd11fc99ecb
Loïc Lamarque <loiclamarque777@gmail.com>
2024-04-02 13:38:32 +0200

    ✨ Add structural equality to internal exceptions of `EmailAddress`
    Overrides the `equals(Any?)` and the `hashCode()` functions in the `InvalidEmailAddress` and the `InvalidEmailAddressPattern` internal exceptions.
    
    Ref: #635

--- src/commonMain/kotlin/org/kotools/types/EmailAddress.kt
+++ src/commonMain/kotlin/org/kotools/types/EmailAddress.kt
@@ -377,12 +377,23 @@ internal class InvalidEmailAddress(
     private val value: Any,
     private val pattern: Any = EmailAddress.PATTERN
 ) {
+    override fun equals(other: Any?): Boolean = other is InvalidEmailAddress
+            && this.value == other.value
+            && this.pattern == other.pattern
+
+    override fun hashCode(): Int = hashCodeOf(value, pattern)
+
     override fun toString(): String =
         "\"$value\" is an invalid email address. " +
                 "It should match the following pattern: $pattern"
 }
 
 internal class InvalidEmailAddressPattern(private val pattern: Any) {
+    override fun equals(other: Any?): Boolean =
+        other is InvalidEmailAddressPattern && this.pattern == other.pattern
+
+    override fun hashCode(): Int = hashCodeOf(pattern)
+
     @OptIn(ExperimentalKotoolsTypesApi::class)
     override fun toString(): String =
         "$pattern is an invalid email address pattern. " +
