dc3de7e957b8d4e6a0b3ebeb753da055b780935c
Loïc Lamarque <loiclamarque777@gmail.com>
2024-04-13 00:27:57 +0200

    👷 Add `org.kotools.types.multi-module-api-reference` plugin (#602)
    Creates this internal Gradle plugin for configuring the API reference's generation and documentation for a multi-module project.
    Also applies this new plugin to the root project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,8 @@
-import org.jetbrains.dokka.base.DokkaBase
-import org.jetbrains.dokka.base.DokkaBaseConfiguration
-
-buildscript {
-    dependencies.classpath(libs.dokka.base)
-}
-
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.dokka)
-}
-
-dependencies.dokkaPlugin(libs.dokka.base)
-
-tasks.dokkaHtmlMultiModule.configure {
-    // ------------------------ Specific configuration -------------------------
-    moduleName = "Kotools Types"
-    outputDirectory = layout.buildDirectory.dir("dokka")
-    pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
-        customAssets = layout.projectDirectory.file("dokka/logo-icon.svg")
-            .asFile
-            .let(::listOf)
-        footerMessage = layout.projectDirectory.file("LICENSE.txt")
-            .asFile
-            .useLines { lines: Sequence<String> ->
-                lines.first { it.startsWith("Copyright (c)") }
-            }
-    }
-    // ----------------------- Common task configuration -----------------------
-    subprojects.mapNotNull { it.tasks.findByName("restoreMainSources") }
-        .let(this::setFinalizedBy)
+    id("org.kotools.types.base")
+    id("org.kotools.types.multi-module-api-reference")
 }
 
 allprojects {
---
+++ gradle/plugins/src/main/kotlin/org/kotools/types/multi-module-api-reference.gradle.kts
@@ -0,0 +1,44 @@
+package org.kotools.types
+
+import org.jetbrains.dokka.base.DokkaBase
+import org.jetbrains.dokka.base.DokkaBaseConfiguration
+import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
+
+private val apiReferenceDirectory = layout.buildDirectory.dir("api-reference")
+
+private val cleanApiReference: TaskProvider<Delete> by tasks
+    .registering(Delete::class) {
+        // Common task configuration
+        description = "Deletes the API reference from the build directory."
+        // Specific task configuration
+        setDelete(apiReferenceDirectory)
+    }
+
+private val dokkaHtmlMultiModule: TaskProvider<DokkaMultiModuleTask> =
+    tasks.named<DokkaMultiModuleTask>("dokkaHtmlMultiModule") {
+        // Common task configuration
+        dependsOn += cleanApiReference
+        subprojects.mapNotNull { it.tasks.findByName("restoreMainSources") }
+            .takeIf(List<Task>::isNotEmpty)
+            ?.let(this::setFinalizedBy)
+        // Specific task configuration
+        moduleName = "Kotools Types"
+        outputDirectory = apiReferenceDirectory
+        pluginConfiguration<DokkaBase, DokkaBaseConfiguration> {
+            customAssets = layout.projectDirectory.file("dokka/logo-icon.svg")
+                .asFile
+                .let(::listOf)
+            footerMessage = layout.projectDirectory.file("LICENSE.txt")
+                .asFile
+                .useLines { lines: Sequence<String> ->
+                    lines.first { it.startsWith("Copyright (c)") }
+                }
+        }
+    }
+
+tasks.register("apiReferenceMultiModule").configure {
+    description =
+        "Generates the API reference for this project and its subprojects."
+    group = "recommended"
+    dependsOn += dokkaHtmlMultiModule
+}
