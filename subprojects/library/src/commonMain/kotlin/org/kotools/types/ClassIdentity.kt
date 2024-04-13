e0edbc50658c9841a9a9880dda98bfed734484a4
Loïc Lamarque <loiclamarque777@gmail.com>
2024-04-13 16:59:13 +0200

    ✨ Add `ClassIdentity` type in `types` subproject (#643)

---
+++ subprojects/library/src/commonMain/kotlin/org/kotools/types/ClassIdentity.kt
@@ -0,0 +1,7 @@
+package org.kotools.types
+
+import kotools.types.experimental.ExperimentalKotoolsTypesApi
+
+/** Represents a class identity of type [T]. */
+@ExperimentalKotoolsTypesApi
+public class ClassIdentity<T : Any> private constructor()
