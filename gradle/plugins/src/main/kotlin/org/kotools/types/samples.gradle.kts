e6ba654dab4ed9426d2e6df35fac0bd678daddfb
Loïc Lamarque <loiclamarque777@gmail.com>
2024-04-16 22:13:52 +0200

    ♻️ Refactor `org.kotools.types.samples` Gradle plugin

--- gradle/plugins/src/main/kotlin/org/kotools/types/samples.gradle.kts
+++ gradle/plugins/src/main/kotlin/org/kotools/types/samples.gradle.kts
@@ -1,8 +1,8 @@
 package org.kotools.types
 
+import org.jetbrains.dokka.gradle.AbstractDokkaLeafTask
 import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
-import org.jetbrains.dokka.gradle.DokkaTask
-import org.jetbrains.dokka.gradle.DokkaTaskPartial
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 import org.kotools.types.samples.ExtractCodeSamples
 import org.kotools.types.samples.InlineSamples
@@ -10,96 +10,76 @@ import org.kotools.types.samples.SamplesExtension
 
 private val samples: SamplesExtension = extensions.create("samples")
 
-// ---------------------------- Samples extraction -----------------------------
-
-private val samplesDirectory: Provider<Directory> =
+private val samplesBuildDirectory: Provider<Directory> =
     layout.buildDirectory.dir("samples")
+private val sourcesBackupDirectory: Provider<Directory> =
+    samplesBuildDirectory.map { it.dir("sources-backup") }
 
-private val extractKotlinSamples: TaskProvider<ExtractCodeSamples> by tasks
-    .registering(ExtractCodeSamples::class) {
-        description = "Extract Kotlin code samples from sources."
-        sourceDirectory = samples.project.map {
-            it.layout.projectDirectory.dir("src/main/kotlin")
+private val projectSources: Directory = layout.projectDirectory.dir("src")
+
+// ------------------------------ Internal tasks -------------------------------
+
+private val cleanSamples: TaskProvider<Delete> by
+tasks.registering(Delete::class) {
+    description = "Deletes the 'samples' build directory."
+    setDelete(samplesBuildDirectory)
+}
+
+private val extractSamples: TaskProvider<ExtractCodeSamples> by
+tasks.registering(ExtractCodeSamples::class) {
+    description = "Extract code samples from sources."
+    sourceDirectory =
+        samples.project.map { it.layout.projectDirectory.dir("src/main") }
+    outputDirectory = samplesBuildDirectory.map { it.dir("extracted") }
+}
+
+private val backupMainSources: TaskProvider<Copy> by
+tasks.registering(Copy::class) {
+    description = "Copies main sources into the build directory."
+    from(projectSources) { exclude("api", "*Test") }
+    into(sourcesBackupDirectory)
+}
+
+private val inlineSamples: TaskProvider<InlineSamples> by
+tasks.registering(InlineSamples::class) {
+    description = "Inlines KDoc samples in project sources."
+    dependsOn += backupMainSources
+    sourcesDirectory = projectSources
+    samplesDirectory =
+        extractSamples.flatMap(ExtractCodeSamples::outputDirectory)
+}
+
+private val restoreMainSources: TaskProvider<Copy> by
+tasks.registering(Copy::class) {
+    description = "Restores main sources backup from the build directory."
+    from(sourcesBackupDirectory)
+    into(projectSources)
+}
+
+// ----------------------------- Dokka integration -----------------------------
+
+tasks.withType<AbstractDokkaLeafTask>().configureEach {
+    dependsOn += inlineSamples
+    finalizedBy(restoreMainSources)
+}
+
+rootProject.tasks.withType<DokkaMultiModuleTask>().configureEach {
+    dependsOn += restoreMainSources
+}
+
+// ---------------------------- Kotlin integration -----------------------------
+
+private val kotlin: KotlinMultiplatformExtension = extensions.getByType()
+
+kotlin.targets
+    .map { if (it.name == "metadata") "sourcesJar" else "${it.name}SourcesJar" }
+    .map(tasks::named)
+    .forEach {
+        it.configure {
+            dependsOn += inlineSamples
+            finalizedBy(restoreMainSources)
         }
-        outputDirectory = samplesDirectory
     }
-
-private val extractJavaSamples: TaskProvider<ExtractCodeSamples> by tasks
-    .registering(ExtractCodeSamples::class) {
-        description = "Extract Java code samples from sources."
-        sourceDirectory = samples.project.map {
-            it.layout.projectDirectory.dir("src/main/java")
-        }
-        outputDirectory = samplesDirectory
-    }
-
-tasks.register<Delete>("cleanSamples").configure {
-    description = "Deletes extracted samples from the build directory."
-    setDelete(samplesDirectory)
-}
-
-// --------------------------- Main sources backup -----------------------------
-
-private val srcDirectory: Directory = layout.projectDirectory.dir("src")
-
-private val srcBackupDirectory: Provider<Directory> =
-    layout.buildDirectory.dir("srcBackup")
-
-private val backupMainSources: TaskProvider<Copy> by tasks
-    .registering(Copy::class) {
-        description = "Makes a copy of main sources in the build directory."
-        from(srcDirectory) { exclude("api", "*Test") }
-        into(srcBackupDirectory)
-    }
-
-private val restoreMainSources: TaskProvider<Copy> by tasks
-    .registering(Copy::class) {
-        description =
-            "Restores the backup of main sources from the build directory."
-        from(srcBackupDirectory)
-        into(srcDirectory)
-    }
-
-tasks.register<Delete>("cleanMainSourcesBackup").configure {
-    description = "Deletes the backup of main sources from the build directory."
-    setDelete(srcBackupDirectory)
-}
-
-// ----------------------------- Samples inlining ------------------------------
-
-private val inlineSamples: TaskProvider<InlineSamples> by tasks
-    .registering(InlineSamples::class) {
-        description = "Inlines code samples in KDoc comments."
-        setDependsOn(listOf(backupMainSources, extractJavaSamples))
-        sourcesDirectory = srcDirectory
-        samplesDirectory =
-            extractKotlinSamples.flatMap(ExtractCodeSamples::outputDirectory)
-    }
-
-// ----------------------- External tasks configuration ------------------------
-
-listOf(
-    KotlinCompilationTask::class,
-    DokkaTask::class,
-    DokkaTaskPartial::class,
-    Jar::class
-).forEach {
-    tasks.withType(it).configureEach {
-        setDependsOn(listOf(inlineSamples))
-        setFinalizedBy(listOf(restoreMainSources))
-    }
-}
-
-tasks.named("jvmSourcesJar").configure {
-    setDependsOn(listOf(inlineSamples))
-    setFinalizedBy(listOf(restoreMainSources))
-}
-
-listOf("jvmApiCheck", "apiCheck", "apiDump").forEach {
-    tasks.named(it).configure { setFinalizedBy(listOf(restoreMainSources)) }
-}
-
-restoreMainSources.configure {
-    rootProject.tasks.withType<DokkaMultiModuleTask>()
-        .let(this::setMustRunAfter)
-}
+private val kotlinCompilationTasks: TaskCollection<KotlinCompilationTask<*>> =
+    tasks.withType(KotlinCompilationTask::class)
+restoreMainSources.configure { mustRunAfter(kotlinCompilationTasks) }
