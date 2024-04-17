817324095914674a067893d5685abcad401e61f9
Loïc Lamarque <loiclamarque777@gmail.com>
2024-04-17 21:28:51 +0200

    ✨ Add `reason` constructor argument in `DeserializationError`

--- subprojects/kotlinx-serialization/src/commonMain/kotlin/org/kotools/types/kotlinx/serialization/DeserializationError.kt
+++ subprojects/kotlinx-serialization/src/commonMain/kotlin/org/kotools/types/kotlinx/serialization/DeserializationError.kt
@@ -2,15 +2,19 @@ package org.kotools.types.kotlinx.serialization
 
 import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.ExperimentalSerializationApi
+import org.kotools.types.internal.Error
 
 internal class DeserializationError(
     private val deserializer: DeserializationStrategy<*>,
-    private val decodedValue: Any
-) {
+    private val decodedValue: Any,
+    private val reason: Error? = null
+) : Error {
     @OptIn(ExperimentalSerializationApi::class)
-    val message: String
+    override val message: String
         get() {
             val serialName: String = this.deserializer.descriptor.serialName
-            return "Unable to deserialize '$serialName' from ${decodedValue}."
+            val reasonMessage: String = this.reason?.message ?: ""
+            return "Unable to deserialize '$serialName' from ${decodedValue}." +
+                    reasonMessage
         }
 }
