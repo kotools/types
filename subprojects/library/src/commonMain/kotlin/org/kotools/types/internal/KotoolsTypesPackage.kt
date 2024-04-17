d2fea005d2fbd3f8354095673b6f87f342282d09
Loïc Lamarque <loiclamarque777@gmail.com>
2024-04-17 18:20:05 +0200

    ⚰️ Remove unused internals from `types` Gradle subproject

--- subprojects/library/src/commonMain/kotlin/org/kotools/types/internal/KotoolsTypesPackage.kt
+++
@@ -1,7 +0,0 @@
-package org.kotools.types.internal
-
-internal enum class KotoolsTypesPackage(private val value: String) {
-    ROOT("org.kotools.types");
-
-    override fun toString(): String = value
-}
