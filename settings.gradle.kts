59e5788cd5fde81b1c37201c09569368a8c4b893
Loïc Lamarque <loiclamarque777@gmail.com>
2024-04-23 17:31:13 +0200

    🔥 Remove `java-compatibility` Gradle subproject
    The `samples` Gradle subproject already tests the Java compatibility of some declarations.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,11 +14,6 @@ include("${rootProject.name}-$internal")
 project(":${rootProject.name}-$internal").projectDir =
     subprojects.resolve(internal)
 
-private val javaCompatibility: String = "java-compatibility"
-include(javaCompatibility)
-project(":$javaCompatibility").projectDir =
-    subprojects.resolve(javaCompatibility)
-
 private val kotlinxSerialization: String = "kotlinx-serialization"
 include("${rootProject.name}-$kotlinxSerialization")
 project(":${rootProject.name}-$kotlinxSerialization").projectDir =
