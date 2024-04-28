c4dfde5f899f83c87d0341e88140d7305b672278
Loïc Lamarque <loiclamarque777@gmail.com>
2024-04-28 14:44:50 +0000

    Deploying to gh-pages from @ kotools/types@3a3338701b715ee45ca75a173bfb44d2f34b8a68 🚀

---
+++ older/4.5.0/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies='{":dokkaHtml/commonMain":[],":dokkaHtml/jsMain":[":dokkaHtml/commonMain"],":dokkaHtml/jvmMain":[":dokkaHtml/commonMain"],":dokkaHtml/linuxMain":[":dokkaHtml/commonMain"],":dokkaHtml/macosArm64Main":[":dokkaHtml/commonMain"],":dokkaHtml/macosMain":[":dokkaHtml/commonMain"],":dokkaHtml/windowsMain":[":dokkaHtml/commonMain"]}'
