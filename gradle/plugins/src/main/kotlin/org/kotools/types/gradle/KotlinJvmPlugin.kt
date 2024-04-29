8c81929007663ee514e68b581f336d0bf9dfa39c
Loïc Lamarque <loiclamarque777@gmail.com>
2024-04-29 19:21:34 +0200

    🔨 Add new Gradle convention plugin for Kotlin/JVM projects
    This commit adds a `org.kotools.types.kotlin.jvm` Gradle convention plugin, implemented as a binary plugin.

---
+++ gradle/plugins/src/main/kotlin/org/kotools/types/gradle/KotlinJvmPlugin.kt
@@ -0,0 +1,19 @@
+package org.kotools.types.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+
+/** Gradle plugin for configuring a Kotlin JVM project. */
+public abstract class KotlinJvmPlugin : Plugin<Project> {
+    /** Applies this plugin to the specified [project]. */
+    override fun apply(project: Project) {
+        project.pluginManager.apply("org.jetbrains.kotlin.jvm")
+        project.extensions.getByType<KotlinJvmProjectExtension>()
+            .explicitApi()
+        val taskManager = KotlinJvmTaskManager(project)
+        taskManager.configureKotlinCompile()
+        taskManager.configureTest()
+    }
+}
