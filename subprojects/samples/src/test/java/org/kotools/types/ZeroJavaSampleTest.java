debe45e5138100d26125e7bcaa914acc7555a86c
Loïc Lamarque <loiclamarque777@gmail.com>
2024-05-02 13:27:57 +0200

    ♻️ Refactor Java samples tests of `Zero` (#646)

--- subprojects/samples/src/test/java/org/kotools/types/ZeroJavaSampleTest.java
+++ subprojects/samples/src/test/java/org/kotools/types/ZeroJavaSampleTest.java
@@ -3,41 +3,37 @@ package org.kotools.types;
 import org.junit.jupiter.api.Test;
 
 class ZeroJavaSampleTest {
+    private final ZeroJavaSample sample = new ZeroJavaSample();
+
     @Test
     void equalsOverride_should_pass() {
-        final ZeroJavaSample sample = new ZeroJavaSample();
-        Assert.printsTrue(sample::equalsOverride);
+        Assert.printsTrue(this.sample::equalsOverride);
     }
 
     @Test
     void hashCodeOverride_should_pass() {
-        final ZeroJavaSample sample = new ZeroJavaSample();
-        Assert.printsTrue(sample::hashCodeOverride);
+        Assert.printsTrue(this.sample::hashCodeOverride);
     }
 
     @Test
     void toByte_should_pass() {
-        final ZeroJavaSample sample = new ZeroJavaSample();
         final String expected = "0";
-        Assert.prints(expected, sample::toByte);
+        Assert.prints(expected, this.sample::toByte);
     }
 
     @Test
     void toShort_should_pass() {
-        final ZeroJavaSample sample = new ZeroJavaSample();
-        Assert.printsTrue(sample::toShort);
+        Assert.printsTrue(this.sample::toShort);
     }
 
     @Test
     void toInt_should_pass() {
-        final ZeroJavaSample sample = new ZeroJavaSample();
-        Assert.printsTrue(sample::toInt);
+        Assert.printsTrue(this.sample::toInt);
     }
 
     @Test
     void toStringSample_should_pass() {
-        final ZeroJavaSample sample = new ZeroJavaSample();
         final String expected = "0";
-        Assert.prints(expected, sample::toStringSample);
+        Assert.prints(expected, this.sample::toStringSample);
     }
 }
