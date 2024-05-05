6c5f8efd5e49d9a16ad3422fa89231a0e09564dc
Loïc Lamarque <loiclamarque777@gmail.com>
2024-05-05 14:18:10 +0200

    ✅ Test Kotlin sample of `NotEmptyList`'s serialization processes
    Ref: #645

--- subprojects/samples/src/main/kotlin/kotools/types/collection/NotEmptyListKotlinSample.kt
+++ subprojects/samples/src/main/kotlin/kotools/types/collection/NotEmptyListKotlinSample.kt
@@ -1,9 +1,20 @@
 package kotools.types.collection
 
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
 internal class NotEmptyListKotlinSample {
     fun notEmptyListOf() {
         val integers: NotEmptyList<Int> = notEmptyListOf(1, 2, 3)
         println(integers) // [1, 2, 3]
+    } // END
+
+    fun serialization() {
+        val string: NotEmptyList<Int> = notEmptyListOf(1, 2, 3)
+        val encoded: String = Json.encodeToString(string)
+        println(encoded) // [1,2,3]
+        val decoded: NotEmptyList<Int> = Json.decodeFromString(encoded)
+        println(decoded == string) // true
     } // END
 
     fun toNotEmptyListOnCollection() {
