d695c53d73a47204fcbb70018f92e97d4f59d657
Loïc Lamarque <loiclamarque777@gmail.com>
2024-05-05 13:13:14 +0200

    ✅ Test Kotlin sample of `PositiveInt.unaryMinus()` method (#645)

---
+++ subprojects/samples/src/main/kotlin/kotools/types/experimental/PositiveIntKotlinSample.kt
@@ -0,0 +1,15 @@
+package kotools.types.experimental
+
+import kotools.types.number.NegativeInt
+import kotools.types.number.PositiveInt
+import kotools.types.number.toPositiveInt
+
+internal class PositiveIntKotlinSample {
+    @OptIn(ExperimentalKotoolsTypesApi::class)
+    fun unaryMinusOperator() {
+        val number: PositiveInt = 1.toPositiveInt()
+            .getOrThrow() // TABS: 1
+        val result: NegativeInt = -number // or number.unaryMinus()
+        println(result) // -1
+    } // END
+}
