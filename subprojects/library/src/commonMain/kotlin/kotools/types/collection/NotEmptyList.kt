a5463d3ef86d5e23b242fe45fbb4dcd6654981ae
Loïc Lamarque <loiclamarque777@gmail.com>
2024-05-06 13:40:00 +0200

    ♻️ Refactor samples of top-level declarations of `NotEmptyList`
    This commit moves Kotlin samples of top-level declarations associated with the `NotEmptyList` type to a new `NotEmptyListKtKotlinSample` class.
    
    Ref: #645

--- subprojects/library/src/commonMain/kotlin/kotools/types/collection/NotEmptyList.kt
+++ subprojects/library/src/commonMain/kotlin/kotools/types/collection/NotEmptyList.kt
@@ -22,7 +22,7 @@ import kotlin.jvm.JvmSynthetic
  *
  * Here's an example of calling this function from Kotlin code:
  *
- * SAMPLE: NotEmptyListKotlinSample.notEmptyListOf.md
+ * SAMPLE: NotEmptyListKtKotlinSample.notEmptyListOf.md
  *
  * The [NotEmptyList] type being an
  * [inline value class](https://kotlinlang.org/docs/inline-classes.html), it is
@@ -42,12 +42,12 @@ public fun <E> notEmptyListOf(head: E, vararg tail: E): NotEmptyList<E> {
  *
  * Here's an example of calling this function from Kotlin code:
  *
- * SAMPLE: NotEmptyListKotlinSample.toNotEmptyListOnCollection.md
+ * SAMPLE: NotEmptyListKtKotlinSample.toNotEmptyListOnCollection.md
  *
  * Please note that changes made to the original collection will not be
  * reflected on the resulting [NotEmptyList].
  *
- * SAMPLE: NotEmptyListKotlinSample.toNotEmptyListOnMutableCollection.md
+ * SAMPLE: NotEmptyListKtKotlinSample.toNotEmptyListOnMutableCollection.md
  *
  * The [NotEmptyList] type being an
  * [inline value class](https://kotlinlang.org/docs/inline-classes.html), it is
--- subprojects/samples/src/main/kotlin/kotools/types/collection/NotEmptyListKotlinSample.kt
+++ subprojects/samples/src/main/kotlin/kotools/types/collection/NotEmptyListKotlinSample.kt
@@ -4,11 +4,6 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 internal class NotEmptyListKotlinSample {
-    fun notEmptyListOf() {
-        val integers: NotEmptyList<Int> = notEmptyListOf(1, 2, 3)
-        println(integers) // [1, 2, 3]
-    } // END
-
     fun serialization() {
         val string: NotEmptyList<Int> = notEmptyListOf(1, 2, 3)
         val encoded: String = Json.encodeToString(string)
@@ -21,27 +16,5 @@ internal class NotEmptyListKotlinSample {
         val notEmptyList: NotEmptyList<Int> = notEmptyListOf(1, 2, 3)
         val list: List<Int> = notEmptyList.toList()
         println(list) // [1, 2, 3]
-    } // END
-
-    fun toNotEmptyListOnCollection() {
-        var collection: Collection<Int> = listOf(1, 2, 3)
-        var result: Result<NotEmptyList<Int>> = collection.toNotEmptyList()
-        println(result) // Success([1, 2, 3])
-
-        collection = emptyList()
-        result = collection.toNotEmptyList()
-        println(result) // Failure(IllegalArgumentException)
-    } // END
-
-    fun toNotEmptyListOnMutableCollection() {
-        val original: MutableCollection<Int> = mutableListOf(1, 2, 3)
-        val notEmptyList: NotEmptyList<Int> = original.toNotEmptyList()
-            .getOrThrow() // TABS: 1
-        println(original) // [1, 2, 3]
-        println(notEmptyList) // [1, 2, 3]
-
-        original.clear()
-        println(original) // []
-        println(notEmptyList) // [1, 2, 3]
     } // END
 }
