7e06077e91398624171a8153ba5d343c147d3182
Loïc Lamarque <loiclamarque777@gmail.com>
2024-05-06 13:40:00 +0200

    ♻️ Refactor samples of top-level declarations of `NotEmptyList`
    This commit moves Kotlin samples of top-level declarations associated with the `NotEmptyList` type to a new `NotEmptyListKtKotlinSample` class.
    
    Ref: #645

---
+++ subprojects/samples/src/main/kotlin/kotools/types/collection/NotEmptyListKtKotlinSample.kt
@@ -0,0 +1,30 @@
+package kotools.types.collection
+
+internal class NotEmptyListKtKotlinSample {
+    fun notEmptyListOf() {
+        val integers: NotEmptyList<Int> = notEmptyListOf(1, 2, 3)
+        println(integers) // [1, 2, 3]
+    } // END
+
+    fun toNotEmptyListOnCollection() {
+        var collection: Collection<Int> = listOf(1, 2, 3)
+        var result: Result<NotEmptyList<Int>> = collection.toNotEmptyList()
+        println(result) // Success([1, 2, 3])
+
+        collection = emptyList()
+        result = collection.toNotEmptyList()
+        println(result) // Failure(IllegalArgumentException)
+    } // END
+
+    fun toNotEmptyListOnMutableCollection() {
+        val original: MutableCollection<Int> = mutableListOf(1, 2, 3)
+        val notEmptyList: NotEmptyList<Int> = original.toNotEmptyList()
+            .getOrThrow() // TABS: 1
+        println(original) // [1, 2, 3]
+        println(notEmptyList) // [1, 2, 3]
+
+        original.clear()
+        println(original) // []
+        println(notEmptyList) // [1, 2, 3]
+    } // END
+}
--- subprojects/samples/src/test/kotlin/kotools/types/collection/NotEmptyListKotlinSampleTest.kt
+++ subprojects/samples/src/test/kotlin/kotools/types/collection/NotEmptyListKotlinSampleTest.kt
@@ -5,13 +5,6 @@ import kotlin.test.Test
 
 class NotEmptyListKotlinSampleTest {
     @Test
-    fun `notEmptyListOf(E, vararg E) should pass`() {
-        val expected = "[1, 2, 3]"
-        val sample = NotEmptyListKotlinSample()
-        assertPrints(expected, sample::notEmptyListOf)
-    }
-
-    @Test
     fun `serialization processes should pass`() {
         val expected: String = listOf("[1,2,3]", true)
             .joinToString(separator = "\n")
@@ -24,28 +17,5 @@ class NotEmptyListKotlinSampleTest {
         val expected = "[1, 2, 3]"
         val sample = NotEmptyListKotlinSample()
         assertPrints(expected, sample::toList)
-    }
-
-    @Test
-    fun `toNotEmptyList() on Collection should pass`() {
-        val exceptionMessage = "Given collection shouldn't be empty."
-        val expected: String = listOf(
-            "Success([1, 2, 3])",
-            "Failure(java.lang.IllegalArgumentException: $exceptionMessage)"
-        ).joinToString(separator = "\n")
-        val sample = NotEmptyListKotlinSample()
-        assertPrints(expected, sample::toNotEmptyListOnCollection)
-    }
-
-    @Test
-    fun `toNotEmptyList() on MutableCollection should pass`() {
-        val expected: String = listOf(
-            "[1, 2, 3]",
-            "[1, 2, 3]",
-            "[]",
-            "[1, 2, 3]"
-        ).joinToString(separator = "\n")
-        val sample = NotEmptyListKotlinSample()
-        assertPrints(expected, sample::toNotEmptyListOnMutableCollection)
     }
 }
