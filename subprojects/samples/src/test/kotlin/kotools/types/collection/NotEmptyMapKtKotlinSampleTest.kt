ceb418f7e441cf437cde021d067f9ed237e36f46
Loïc Lamarque <loiclamarque777@gmail.com>
2024-05-18 18:26:13 +0200

    ✅ Test Kotlin samples of `Map.toNotEmptyMap()` (#645)

--- subprojects/samples/src/test/kotlin/kotools/types/collection/NotEmptyMapKtKotlinSampleTest.kt
+++ subprojects/samples/src/test/kotlin/kotools/types/collection/NotEmptyMapKtKotlinSampleTest.kt
@@ -10,4 +10,27 @@ class NotEmptyMapKtKotlinSampleTest {
         val sample = NotEmptyMapKtKotlinSample()
         assertPrints(expected, sample::notEmptyMapOf)
     }
+
+    @Test
+    fun `toNotEmptyMap() on Map should pass`() {
+        val exceptionMessage = "Given map shouldn't be empty."
+        val expected: String = listOf(
+            "Success({a=1, b=2})",
+            "Failure(java.lang.IllegalArgumentException: $exceptionMessage)"
+        ).joinToString(separator = "\n")
+        val sample = NotEmptyMapKtKotlinSample()
+        assertPrints(expected, sample::toNotEmptyMapOnMap)
+    }
+
+    @Test
+    fun `toNotEmptyMap() on MutableMap should pass`() {
+        val expected: String = listOf(
+            "{a=1, b=2}",
+            "{a=1, b=2}",
+            "{}",
+            "{a=1, b=2}"
+        ).joinToString(separator = "\n")
+        val sample = NotEmptyMapKtKotlinSample()
+        assertPrints(expected, sample::toNotEmptyMapOnMutableMap)
+    }
 }
