d710a36de2c7a175f7112d9a1db5bf0a3114ec64
Loïc Lamarque <loiclamarque777@gmail.com>
2024-05-22 18:53:19 +0200

    ✅ Test Kotlin samples of `Collection.toNotEmptySet()` (#645)

--- subprojects/samples/src/main/kotlin/kotools/types/collection/NotEmptySetKtKotlinSample.kt
+++ subprojects/samples/src/main/kotlin/kotools/types/collection/NotEmptySetKtKotlinSample.kt
@@ -5,4 +5,26 @@ internal class NotEmptySetKtKotlinSample {
         val integers: NotEmptySet<Int> = notEmptySetOf(1, 2, 3, 1)
         println(integers) // [1, 2, 3]
     } // END
+
+    fun toNotEmptySetOnCollection() {
+        var collection: Collection<Int> = setOf(1, 2, 3, 1)
+        var result: Result<NotEmptySet<Int>> = collection.toNotEmptySet()
+        println(result) // Success([1, 2, 3])
+
+        collection = emptySet()
+        result = collection.toNotEmptySet()
+        println(result) // Failure(IllegalArgumentException)
+    } // END
+
+    fun toNotEmptySetOnMutableCollection() {
+        val original: MutableCollection<Int> = mutableSetOf(1, 2, 3)
+        val notEmptySet: NotEmptySet<Int> = original.toNotEmptySet()
+            .getOrThrow() // TABS: 1
+        println(original) // [1, 2, 3]
+        println(notEmptySet) // [1, 2, 3]
+
+        original.clear()
+        println(original) // []
+        println(notEmptySet) // [1, 2, 3]
+    } // END
 }
