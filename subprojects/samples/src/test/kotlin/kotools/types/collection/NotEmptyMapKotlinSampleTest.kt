3da24687bd947cdf2af4cf10d26118afedea3876
Loïc Lamarque <loiclamarque777@gmail.com>
2024-05-22 06:59:11 +0200

    ♻️ Refactor Kotlin sample tests of `NotEmptyMap` (#645)

--- subprojects/samples/src/test/kotlin/kotools/types/collection/NotEmptyMapKotlinSampleTest.kt
+++ subprojects/samples/src/test/kotlin/kotools/types/collection/NotEmptyMapKotlinSampleTest.kt
@@ -4,25 +4,24 @@ import org.kotools.types.assertPrints
 import kotlin.test.Test
 
 class NotEmptyMapKotlinSampleTest {
+    private val sample: NotEmptyMapKotlinSample = NotEmptyMapKotlinSample()
+
     @Test
     fun `serialization processes should pass`() {
         val expected: String = listOf("{\"a\":1,\"b\":2}", "true")
             .joinToString(separator = "\n")
-        val sample = NotEmptyMapKotlinSample()
-        assertPrints(expected, sample::serialization)
+        assertPrints(expected, this.sample::serialization)
     }
 
     @Test
     fun `head should pass`() {
         val expected = "(a, 1)"
-        val sample = NotEmptyMapKotlinSample()
-        assertPrints(expected, sample::head)
+        assertPrints(expected, this.sample::head)
     }
 
     @Test
     fun `tail should pass`() {
         val expected = "{b=2, c=3}"
-        val sample = NotEmptyMapKotlinSample()
-        assertPrints(expected, sample::tail)
+        assertPrints(expected, this.sample::tail)
     }
 }
