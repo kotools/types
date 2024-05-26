bc972998223754da99db524fdbe2d6d11bce00e1
Loïc Lamarque <loiclamarque777@gmail.com>
2024-05-26 14:15:32 +0200

    ✅ Test Kotlin sample of `PositiveInt`'s serialization (#645)

---
+++ subprojects/samples/src/main/kotlin/kotools/types/number/PositiveIntKotlinSample.kt
@@ -0,0 +1,15 @@
+package kotools.types.number
+
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+internal class PositiveIntKotlinSample {
+    fun serialization() {
+        val number: PositiveInt = 123.toPositiveInt()
+            .getOrThrow() // TABS: 1
+        val encoded: String = Json.encodeToString(number)
+        println(encoded) // 123
+        val decoded: PositiveInt = Json.decodeFromString(encoded)
+        println(decoded == number) // true
+    } // END
+}
