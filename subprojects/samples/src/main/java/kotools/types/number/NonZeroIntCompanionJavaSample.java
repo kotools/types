d5c875ea5e45ac8b3d0323236cc22e018557df17
Loïc Lamarque <loiclamarque777@gmail.com>
2024-05-26 13:54:59 +0200

    ✅ Test samples of `NonZeroInt.Companion.create(Number)` (#645)

---
+++ subprojects/samples/src/main/java/kotools/types/number/NonZeroIntCompanionJavaSample.java
@@ -0,0 +1,8 @@
+package kotools.types.number;
+
+class NonZeroIntCompanionJavaSample {
+    void create() {
+        final NonZeroInt number = NonZeroInt.Companion.create(23);
+        System.out.println(number); // 23
+    } // END
+}
---
+++ subprojects/samples/src/test/java/kotools/types/number/NonZeroIntCompanionJavaSampleTest.java
@@ -0,0 +1,14 @@
+package kotools.types.number;
+
+import org.junit.jupiter.api.Test;
+import org.kotools.types.Assert;
+
+class NonZeroIntCompanionJavaSampleTest {
+    @Test
+    void create_should_pass() {
+        final String expected = "23";
+        final NonZeroIntCompanionJavaSample sample =
+                new NonZeroIntCompanionJavaSample();
+        Assert.prints(expected, sample::create);
+    }
+}
