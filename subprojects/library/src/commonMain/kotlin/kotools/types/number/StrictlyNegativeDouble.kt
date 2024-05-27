17e435f3235da996843ef5300543012346623348
Loïc Lamarque <loiclamarque777@gmail.com>
2024-05-27 13:43:27 +0200

    ✅ Test samples of `StrictlyNegativeDouble.equals(Any?)` (#645)
    This commit also fixes the Java sample of this function.

--- subprojects/library/src/commonMain/kotlin/kotools/types/number/StrictlyNegativeDouble.kt
+++ subprojects/library/src/commonMain/kotlin/kotools/types/number/StrictlyNegativeDouble.kt
@@ -141,14 +141,7 @@ public class StrictlyNegativeDouble private constructor(
      *
      * Here's an example of calling this function from Kotlin code:
      *
-     * ```kotlin
-     * val number: Number = -23
-     * val first: StrictlyNegativeDouble = StrictlyNegativeDouble.create(number)
-     * val second: StrictlyNegativeDouble =
-     *     StrictlyNegativeDouble.create(number)
-     * val result: Boolean = first == second // or first.equals(second)
-     * println(result) // true
-     * ```
+     * SAMPLE: StrictlyNegativeDoubleKotlinSample.equalsOverride.md
      * </details>
      *
      * <br/>
@@ -159,15 +152,7 @@ public class StrictlyNegativeDouble private constructor(
      *
      * Here's an example of calling this function from Java code:
      *
-     * ```java
-     * final int number = -23;
-     * final StrictlyNegativeDouble first =
-     *         StrictlyNegativeDouble.Companion.create(number);
-     * final StrictlyNegativeDouble second =
-     *         StrictlyNegativeDouble.Companion.create(number);
-     * final boolean result = first == second; // or first.equals(second)
-     * System.out.println(result); // true
-     * ```
+     * SAMPLE: StrictlyNegativeDoubleJavaSample.equalsOverride.md
      * </details>
      */
     @Suppress("RedundantModalityModifier")
---
+++ subprojects/samples/src/main/java/kotools/types/number/StrictlyNegativeDoubleJavaSample.java
@@ -0,0 +1,13 @@
+package kotools.types.number;
+
+class StrictlyNegativeDoubleJavaSample {
+    void equalsOverride() {
+        final int number = -23;
+        final StrictlyNegativeDouble first =
+                StrictlyNegativeDouble.Companion.create(number); // TABS: 2
+        final StrictlyNegativeDouble second =
+                StrictlyNegativeDouble.Companion.create(number); // TABS: 2
+        final boolean result = first.equals(second);
+        System.out.println(result); // true
+    } // END
+}
