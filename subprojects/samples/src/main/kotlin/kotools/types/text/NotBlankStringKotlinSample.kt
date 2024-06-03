5cd04ccba8ef10a7e2202555886e1f1800fe2d7b
Loïc Lamarque <loiclamarque777@gmail.com>
2024-06-03 07:30:07 +0200

    ✅ Test Kotlin sample of `NotBlankString`'s serialization (#645)

--- subprojects/samples/src/main/kotlin/kotools/types/text/NotBlankStringKotlinSample.kt
+++ subprojects/samples/src/main/kotlin/kotools/types/text/NotBlankStringKotlinSample.kt
@@ -1,8 +1,19 @@
 package kotools.types.text
 
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import kotools.types.experimental.ExperimentalKotoolsTypesApi
 
 internal class NotBlankStringKotlinSample {
+    fun serialization() {
+        val string: NotBlankString = "hello world".toNotBlankString()
+            .getOrThrow() // TABS: 1
+        val encoded: String = Json.encodeToString(string)
+        println(encoded) // "hello world"
+        val decoded: NotBlankString = Json.decodeFromString(encoded)
+        println(decoded == string) // true
+    } // END
+
     @OptIn(ExperimentalKotoolsTypesApi::class)
     fun plusOperator() {
         val text: NotBlankString = "hello".toNotBlankString()
--- subprojects/samples/src/test/kotlin/kotools/types/text/NotBlankStringKotlinSampleTest.kt
+++ subprojects/samples/src/test/kotlin/kotools/types/text/NotBlankStringKotlinSampleTest.kt
@@ -5,6 +5,14 @@ import kotlin.test.Test
 
 class NotBlankStringKotlinSampleTest {
     @Test
+    fun `serialization processes should pass`() {
+        val expected: String = listOf("\"hello world\"", true)
+            .joinToString(separator = "\n")
+        val sample = NotBlankStringKotlinSample()
+        assertPrints(expected, sample::serialization)
+    }
+
+    @Test
     fun `plus(Any) should pass`() {
         val sample = NotBlankStringKotlinSample()
         assertPrints(expected = "hello world", sample::plusOperator)
