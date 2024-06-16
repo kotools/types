14f335f0b72025cba14b48e076b36448e3e0391e
Loïc Lamarque <loiclamarque777@gmail.com>
2024-06-16 13:49:53 +0200

    ✅ Update Java samples tests of `EmailAddress.Companion`

--- subprojects/samples/src/test/java/org/kotools/types/EmailAddressCompanionJavaSampleTest.java
+++ subprojects/samples/src/test/java/org/kotools/types/EmailAddressCompanionJavaSampleTest.java
@@ -3,41 +3,34 @@ package org.kotools.types;
 import org.junit.jupiter.api.Test;
 
 class EmailAddressCompanionJavaSampleTest {
+    private final EmailAddressCompanionJavaSample sample =
+            new EmailAddressCompanionJavaSample();
+
     @Test
     void patternSample_should_pass() {
-        final EmailAddressCompanionJavaSample sample =
-                new EmailAddressCompanionJavaSample();
         final String expected = "^\\S+@\\S+\\.\\S+$";
-        Assert.prints(expected, sample::patternSample);
+        Assert.prints(expected, this.sample::patternSample);
     }
 
     @Test
     void fromString_Any_should_pass() {
-        final EmailAddressCompanionJavaSample sample =
-                new EmailAddressCompanionJavaSample();
         final String expected = "success";
-        Assert.prints(expected, sample::fromString_Any);
+        Assert.prints(expected, this.sample::fromString_Any);
     }
 
     @Test
     void fromString_Any_Any_should_pass() {
-        final EmailAddressCompanionJavaSample sample =
-                new EmailAddressCompanionJavaSample();
         final String expected = "success";
-        Assert.prints(expected, sample::fromString_Any_Any);
+        Assert.prints(expected, this.sample::fromString_Any_Any);
     }
 
     @Test
     void fromStringOrNull_Any_should_pass() {
-        final EmailAddressCompanionJavaSample sample =
-                new EmailAddressCompanionJavaSample();
-        Assert.printsTrue(sample::fromStringOrNull_Any);
+        Assert.printsTrue(this.sample::fromStringOrNull_Any);
     }
 
     @Test
     void fromStringOrNull_Any_Any_should_pass() {
-        final EmailAddressCompanionJavaSample sample =
-                new EmailAddressCompanionJavaSample();
-        Assert.printsTrue(sample::fromStringOrNull_Any_Any);
+        Assert.printsTrue(this.sample::fromStringOrNull_Any_Any);
     }
 }
