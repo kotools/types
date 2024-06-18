bd1fdfd1b2dad8bcea0e6a045420114dd117d7e0
Loïc Lamarque <loiclamarque777@gmail.com>
2024-06-18 23:58:58 +0200

    ♻️ Refactor `org.kotools.types.samples.v2` Gradle plugin (#641)
    Improves the source sets configuration for Kotlin Multiplatform projects.

--- gradle/plugins/src/main/kotlin/org/kotools/types/samples.v2.gradle.kts
+++ gradle/plugins/src/main/kotlin/org/kotools/types/samples.v2.gradle.kts
@@ -5,19 +5,15 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 
 private val kotlin: KotlinMultiplatformExtension = extensions.findByType()
     ?: error("Kotlin Multiplatform plugin not found.")
-kotlin.sourceSets {
-    fun createSamples(platform: String) {
-        val main: KotlinSourceSet = this.findByName("${platform}Main") ?: return
-        val test: KotlinSourceSet = this.findByName("${platform}Test") ?: return
-        val samples: KotlinSourceSet = this.create("${platform}Samples") {
-            dependsOn(main)
-        }
-        test.dependsOn(samples)
-    }
-    this.asSequence()
-        .filterNotNull()
-        .map { it.name.substringBefore("Main") }
-        .map { it.substringBefore("Test") }
-        .toSet()
-        .forEach(::createSamples)
+private val prefixes: Set<String> = kotlin.sourceSets.mapNotNull { it?.name }
+    .filter { it.endsWith("Main") }
+    .map { it.substringBefore("Main") }
+    .toSet()
+prefixes.forEach {
+    val main: KotlinSourceSet = kotlin.sourceSets.getByName("${it}Main")
+    val test: KotlinSourceSet = kotlin.sourceSets.findByName("${it}Test")
+        ?: return@forEach
+    val samples: KotlinSourceSet =
+        kotlin.sourceSets.create("${it}Samples") { dependsOn(main) }
+    test.dependsOn(samples)
 }
