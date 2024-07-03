059f934f633a1c59b0fc9af0b7a25d6d63b9daf5
Loïc Lamarque <loiclamarque777@gmail.com>
2024-07-03 14:28:24 +0200

    📝 Update Java sample of `AnyInt(Int)` function (#641)

--- subprojects/library/src/jvmSample/java/kotools/types/experimental/AnyIntFactoryJavaSample.java
+++ subprojects/library/src/jvmSample/java/kotools/types/experimental/AnyIntFactoryJavaSample.java
@@ -1,11 +1,16 @@
 package kotools.types.experimental;
 
+import kotools.types.number.AnyInt;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("NewClassNamingConvention")
 class AnyIntFactoryJavaSample {
     @Test
     void constructorLikeInt() {
-        AnyIntFactory.create(1);
+        final AnyInt number = AnyIntFactory.create(1);
+        final String actual = number.toString();
+        final String expected = "1";
+        Assertions.assertEquals(expected, actual);
     }
 }
