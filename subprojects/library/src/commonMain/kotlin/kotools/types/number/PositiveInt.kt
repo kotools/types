440978819e842bbd6dd40350fa37773e9714b5bb
Loïc Lamarque <loiclamarque777@gmail.com>
2024-07-06 22:04:24 +0200

    🐛 Fix Kotlin sample of `PositiveInt` type's serialization (#641)

--- subprojects/library/src/commonMain/kotlin/kotools/types/number/PositiveInt.kt
+++ subprojects/library/src/commonMain/kotlin/kotools/types/number/PositiveInt.kt
@@ -58,7 +58,7 @@ public fun Number.toPositiveInt(): Result<PositiveInt> {
  * Here's an example of Kotlin code that encodes and decodes this type using the
  * [JavaScript Object Notation (JSON) format from kotlinx.serialization](https://kotlinlang.org/api/kotlinx.serialization/kotlinx-serialization-json/kotlinx.serialization.json/-json):
  *
- * SAMPLE: PositiveIntKotlinSample.serialization.md
+ * SAMPLE: [kotools.types.number.PositiveIntCommonSample.serialization]
  * </details>
  */
 @OptIn(InternalKotoolsTypesApi::class)
--- subprojects/library/src/commonSample/kotlin/kotools/types/number/PositiveIntCommonSample.kt
+++ subprojects/library/src/commonSample/kotlin/kotools/types/number/PositiveIntCommonSample.kt
@@ -2,14 +2,17 @@ package kotools.types.number
 
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
-internal class PositiveIntKotlinSample {
+internal class PositiveIntCommonSample {
+    @Test
     fun serialization() {
         val number: PositiveInt = 123.toPositiveInt()
             .getOrThrow()
         val encoded: String = Json.encodeToString(number)
-        println(encoded) // 123
+        assertEquals(expected = "123", actual = encoded)
         val decoded: PositiveInt = Json.decodeFromString(encoded)
-        println(decoded == number) // true
-    } // END
+        assertEquals(expected = number, actual = decoded)
+    }
 }
