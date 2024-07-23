47ab8685ed957eaf77ae816e31d547d3425b043b
Loïc Lamarque <loiclamarque777@gmail.com>
2024-07-24 01:48:11 +0200

    🐛 Fix publication artifacts of `types-internal` subproject
    This subproject now properly includes the Javadoc archive in its publication artifacts.

--- subprojects/internal/build.gradle.kts
+++ subprojects/internal/build.gradle.kts
@@ -1,16 +1,21 @@
 plugins {
-    alias(libs.plugins.kotools.types.dev.tasks)
-    alias(libs.plugins.kotools.types.kotlin.multiplatform)
+    alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.dokka)
-    alias(libs.plugins.kotools.types.documentation)
     `maven-publish`
     signing
+    alias(libs.plugins.kotools.types.dev.tasks)
+    alias(libs.plugins.kotools.types.kotlin.multiplatform)
+    alias(libs.plugins.kotools.types.documentation)
     alias(libs.plugins.kotools.types.publication)
 }
+
+// ----------------------------- Plugin extensions -----------------------------
 
 devTasks.list(tasks.checkJs, tasks.checkJvm)
 
 documentation.excludeFromParentApiReference = true
+
+// ------------------------------- Dependencies --------------------------------
 
 dependencies {
     commonMainImplementation(libs.kotlinx.serialization.core)
