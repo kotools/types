2073a7560ddf132797adaba914daf7f3a5a4038b
Loïc Lamarque <loiclamarque777@gmail.com>
2024-08-15 15:57:54 +0200

    ✨ Add `ZeroAsByteSerializer` type (#690)

---
+++ subprojects/kotlinx-serialization/src/commonMain/kotlin/org/kotools/types/kotlinx/serialization/ZeroAsByteSerializer.kt
@@ -0,0 +1,21 @@
+package org.kotools.types.kotlinx.serialization
+
+import org.kotools.types.Zero
+
+/**
+ * Class responsible for serializing the [Zero] type as [Byte].
+ *
+ * @constructor Creates an instance of [ZeroAsByteSerializer].
+ *
+ * <br>
+ * <details>
+ * <summary>
+ *     <b>Calling from Kotlin</b>
+ * </summary>
+ *
+ * Here's an example of calling this constructor from Kotlin code:
+ *
+ * SAMPLE: [org.kotools.types.kotlinx.serialization.ZeroAsByteSerializerCommonSample.primaryConstructor]
+ * </details>
+ */
+public class ZeroAsByteSerializer
---
+++ subprojects/kotlinx-serialization/src/commonSample/kotlin/org/kotools/types/kotlinx/serialization/ZeroAsByteSerializerCommonSample.kt
@@ -0,0 +1,10 @@
+package org.kotools.types.kotlinx.serialization
+
+import kotlin.test.Test
+
+internal class ZeroAsByteSerializerCommonSample {
+    @Test
+    fun primaryConstructor() {
+        ZeroAsByteSerializer()
+    }
+}
