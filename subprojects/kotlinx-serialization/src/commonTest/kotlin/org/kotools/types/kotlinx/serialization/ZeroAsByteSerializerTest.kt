15322df1609bf0aa7a7dfbf2eb88258632be1f85
Loïc Lamarque <loiclamarque777@gmail.com>
2024-08-15 17:27:29 +0200

    ✅ Add missing unit tests for `ZeroAsByteSerializer` type (#690)

--- subprojects/kotlinx-serialization/src/commonTest/kotlin/org/kotools/types/kotlinx/serialization/ZeroAsByteSerializerTest.kt
+++ subprojects/kotlinx-serialization/src/commonTest/kotlin/org/kotools/types/kotlinx/serialization/ZeroAsByteSerializerTest.kt
@@ -1,16 +1,40 @@
 package org.kotools.types.kotlinx.serialization
 
 import kotools.types.experimental.ExperimentalKotoolsTypesApi
+import kotools.types.internal.simpleNameOf
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 @OptIn(ExperimentalKotoolsTypesApi::class)
 class ZeroAsByteSerializerTest {
+    @Test
+    fun constructorShouldPass() {
+        ZeroAsByteSerializer()
+    }
+
+    @Test
+    fun equalsShouldPassWithZeroAsByteSerializer() {
+        val serializer = ZeroAsByteSerializer()
+        val other = ZeroAsByteSerializer()
+        val actual: Boolean = serializer == other
+        assertTrue(actual)
+    }
+
     @Test
     fun equalsShouldFailWithAnotherTypeThanZeroAsByteSerializer() {
         val serializer = ZeroAsByteSerializer()
         val other: Any = "Oops"
         val actual: Boolean = serializer == other
         assertFalse(actual)
+    }
+
+    @Test
+    fun toStringShouldPass() {
+        val serializer = ZeroAsByteSerializer()
+        val actual: String = serializer.toString()
+        val expected: String = simpleNameOf(serializer::class)
+        assertEquals(expected, actual)
     }
 }
