77bbc4eeb66b1ca46d6d356a12a7e83943261d70
Loïc Lamarque <loiclamarque777@gmail.com>
2024-12-17 07:39:13 +0100

    ➖ Remove `org.jetbrains.kotlin.jvm` Gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ allprojects {
 }
 
 plugins {
-    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.dokka)
     alias(libs.plugins.kotools.types.documentation)
