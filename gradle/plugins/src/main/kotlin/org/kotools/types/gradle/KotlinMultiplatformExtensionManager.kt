041c89eae2cc69254780bbbd1ff8c03fef448c94
Loïc Lamarque <loiclamarque777@gmail.com>
2024-12-19 08:43:21 +0100

    🔥 Remove useless Yarn resolutions for Kotlin/JS (#740)

--- gradle/plugins/src/main/kotlin/org/kotools/types/gradle/KotlinMultiplatformExtensionManager.kt
+++ gradle/plugins/src/main/kotlin/org/kotools/types/gradle/KotlinMultiplatformExtensionManager.kt
@@ -50,9 +50,6 @@ internal class KotlinMultiplatformExtensionManager(
         yarn.lockFileDirectory =
             this.project.rootProject.layout.projectDirectory.asFile
         yarn.resolution("braces", "3.0.3")
-        yarn.resolution("follow-redirects", "1.15.6")
-        yarn.resolution("rollup", "2.79.2")
-        yarn.resolution("webpack", "5.94.0")
     }
 
     private fun configureKotlinJvmTarget(kotlin: KotlinMultiplatformExtension) {
