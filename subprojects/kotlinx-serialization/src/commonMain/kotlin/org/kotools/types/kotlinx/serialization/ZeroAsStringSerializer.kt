8b8a1d1399f3e30fde7ba03c37307d015dda288e
Loïc Lamarque <loiclamarque777@gmail.com>
2025-01-08 12:36:04 +0100

    ✨ Override `ZeroAsStringSerializer.toString()` method (#699)

--- subprojects/kotlinx-serialization/src/commonMain/kotlin/org/kotools/types/kotlinx/serialization/ZeroAsStringSerializer.kt
+++ subprojects/kotlinx-serialization/src/commonMain/kotlin/org/kotools/types/kotlinx/serialization/ZeroAsStringSerializer.kt
@@ -1,9 +1,12 @@
 package org.kotools.types.kotlinx.serialization
 
+import kotools.types.internal.simpleNameOf
 import org.kotools.types.ExperimentalKotoolsTypesApi
 import org.kotools.types.Zero
 import org.kotools.types.internal.ExperimentalSince
 import org.kotools.types.internal.KotoolsTypesVersion
+import org.kotools.types.internal.Warning
+import kotlin.reflect.KClass
 
 /**
  * Class responsible for serializing the [Zero] type as [String].
@@ -23,4 +26,24 @@ import org.kotools.types.internal.KotoolsTypesVersion
  */
 @ExperimentalKotoolsTypesApi
 @ExperimentalSince(KotoolsTypesVersion.V5_0_0)
-public class ZeroAsStringSerializer
+public class ZeroAsStringSerializer {
+    // ------------------------------ Conversions ------------------------------
+
+    /**
+     * Returns the string representation of this serializer, corresponding to
+     * its [simple name][KClass.simpleName].
+     *
+     * <br>
+     * <details>
+     * <summary>
+     *     <b>Calling from Kotlin</b>
+     * </summary>
+     *
+     * Here's an example of calling this method from Kotlin code:
+     *
+     * SAMPLE: [org.kotools.types.kotlinx.serialization.ZeroAsStringSerializerCommonSample.toStringOverride]
+     * </details>
+     */
+    @Suppress(Warning.FINAL)
+    final override fun toString(): String = simpleNameOf(this::class)
+}
