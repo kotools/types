eea0cc2b75b950cae02fef65315ff05c3fa32dcb
Loïc Lamarque <loiclamarque777@gmail.com>
2025-01-08 10:28:52 +0100

    ♻️ Refactor `KotoolsTypesVersion` internal type
    This commit removes useless documentation from the `KotoolsTypesVersion` enumeration, and reorders its values for improving backward compatibility.

--- subprojects/internal/src/commonMain/kotlin/org/kotools/types/internal/KotoolsTypesVersion.kt
+++ subprojects/internal/src/commonMain/kotlin/org/kotools/types/internal/KotoolsTypesVersion.kt
@@ -5,33 +5,14 @@ import kotools.types.internal.InternalKotoolsTypesApi
 /** Represents a version of Kotools Types. */
 @InternalKotoolsTypesApi
 public enum class KotoolsTypesVersion {
-    /** Not released yet. */
     Unreleased,
-
-    /** Version 4.5.3. */
-    V4_5_3,
-
-    /** Version 4.5.2. */
-    V4_5_2,
-
-    /** Version 4.5.1. */
-    V4_5_1,
-
-    /** Version 4.5.0. */
-    V4_5_0,
-
-    /** Version 4.4.0. */
-    V4_4_0,
-
-    /** Version 4.1.0. */
-    V4_1_0,
-
-    /** Version 4.0.0. */
-    V4_0_0,
-
-    /** Version 3.0.0. */
+    V1_1_0,
     V3_0_0,
-
-    /** Version 1.1.0. */
-    V1_1_0
+    V4_0_0,
+    V4_1_0,
+    V4_4_0,
+    V4_5_0,
+    V4_5_1,
+    V4_5_2,
+    V4_5_3
 }
