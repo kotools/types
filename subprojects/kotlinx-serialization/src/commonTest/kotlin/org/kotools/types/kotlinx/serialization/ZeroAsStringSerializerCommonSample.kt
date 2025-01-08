29dc4de2f2da2b0ad4822141f90ec37b7130bc76
Loïc Lamarque <loiclamarque777@gmail.com>
2025-01-08 12:36:04 +0100

    ✨ Override `ZeroAsStringSerializer.toString()` method (#699)

--- subprojects/kotlinx-serialization/src/commonTest/kotlin/org/kotools/types/kotlinx/serialization/ZeroAsStringSerializerCommonSample.kt
+++ subprojects/kotlinx-serialization/src/commonTest/kotlin/org/kotools/types/kotlinx/serialization/ZeroAsStringSerializerCommonSample.kt
@@ -2,11 +2,22 @@ package org.kotools.types.kotlinx.serialization
 
 import org.kotools.types.ExperimentalKotoolsTypesApi
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 @OptIn(ExperimentalKotoolsTypesApi::class)
 class ZeroAsStringSerializerCommonSample {
     @Test
     fun primaryConstructor() {
         ZeroAsStringSerializer()
+    }
+
+    // ------------------------------ Conversions ------------------------------
+
+    @Test
+    fun toStringOverride() {
+        val serializer = ZeroAsStringSerializer()
+        val serializerAsString: String = serializer.toString()
+        val expected: String = checkNotNull(serializer::class.simpleName)
+        assertEquals(expected, serializerAsString)
     }
 }
