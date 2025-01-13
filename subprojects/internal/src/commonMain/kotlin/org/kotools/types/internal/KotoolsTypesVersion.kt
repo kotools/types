0428412395267ef199ec60e80a2730a076fc6fdd
Loïc Lamarque <loiclamarque777@gmail.com>
2025-01-13 12:36:28 +0100

    💚 Add missing documentation in `KotoolsTypesVersion`

--- subprojects/internal/src/commonMain/kotlin/org/kotools/types/internal/KotoolsTypesVersion.kt
+++ subprojects/internal/src/commonMain/kotlin/org/kotools/types/internal/KotoolsTypesVersion.kt
@@ -5,14 +5,33 @@ import kotools.types.internal.InternalKotoolsTypesApi
 /** Represents a version of Kotools Types. */
 @InternalKotoolsTypesApi
 public enum class KotoolsTypesVersion {
+    /** Version 1.0.0. */
     V1_1_0,
+
+    /** Version 3.0.0. */
     V3_0_0,
+
+    /** Version 4.0.0. */
     V4_0_0,
+
+    /** Version 4.1.0. */
     V4_1_0,
+
+    /** Version 4.4.0. */
     V4_4_0,
+
+    /** Version 4.5.0. */
     V4_5_0,
+
+    /** Version 4.5.1. */
     V4_5_1,
+
+    /** Version 4.5.2. */
     V4_5_2,
+
+    /** Version 4.5.3. */
     V4_5_3,
+
+    /** Version 5.0.0. */
     V5_0_0
 }
