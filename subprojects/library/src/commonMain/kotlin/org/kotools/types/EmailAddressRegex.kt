76e9316e88d214664ea48bc223cb84791d8e8a86
Loïc Lamarque <loiclamarque777@gmail.com>
2025-03-31 11:56:55 +0200

    ✨ Add `EmailAddressRegex` type (#809)

---
+++ subprojects/library/src/commonMain/kotlin/org/kotools/types/EmailAddressRegex.kt
@@ -0,0 +1,12 @@
+package org.kotools.types
+
+import org.kotools.types.internal.ExperimentalSince
+import org.kotools.types.internal.KotoolsTypesVersion
+
+/**
+ * Represents a regular expression used for validating
+ * [email addresses][EmailAddress].
+ */
+@ExperimentalKotoolsTypesApi
+@ExperimentalSince(KotoolsTypesVersion.V5_0_1)
+public class EmailAddressRegex private constructor()
