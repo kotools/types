229691ff89fc9f97aa6c2ebd6b1fb0c9c44e8f99
Loïc Lamarque <loiclamarque777@gmail.com>
2025-08-14 08:10:54 +0200

    🎨 Group declarations from settings of `plugins` included build

--- gradle/plugins/settings.gradle.kts
+++ gradle/plugins/settings.gradle.kts
@@ -1,4 +1,8 @@
+// ------------------------------- Root project --------------------------------
+
 rootProject.name = "gradle-plugins"
+
+// ----------------------------- Version catalogs ------------------------------
 
 dependencyResolutionManagement.versionCatalogs.register("libs") {
     val files: ConfigurableFileCollection = files("../libs.versions.toml")
