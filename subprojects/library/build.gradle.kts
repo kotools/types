c87d61e036085314f0cb941794b5d5770f32a845
Loïc Lamarque <loiclamarque777@gmail.com>
2025-09-24 19:59:21 +0200

    👷 Use `convention.documentation.module` plugin in `types` project

--- subprojects/library/build.gradle.kts
+++ subprojects/library/build.gradle.kts
@@ -1,35 +1,25 @@
 import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
 
-// ---------------------------------- Plugins ----------------------------------
-
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.dokka)
     alias(libs.plugins.kotlinx.binary.compatibility.validator)
     alias(libs.plugins.kotlinx.serialization)
     `maven-publish`
     signing
     alias(libs.plugins.kotools.types.kotlin.multiplatform)
-    alias(libs.plugins.kotools.types.documentation)
+    alias(libs.plugins.convention.documentation.module)
     alias(libs.plugins.kotools.samples.multiplatform)
     alias(libs.plugins.kotools.types.publication)
 }
 
-// ----------------------------- Plugin extensions -----------------------------
-
 apiValidation.apiDumpDirectory = "src/api"
 
-documentation {
-    moduleName = "Kotools Types"
-    packages = layout.projectDirectory.file("packages.md").asFile
-}
+documentation.packages = layout.projectDirectory.file("packages.md").asFile
 
 kotlin.jvm(KotlinJvmTarget::withJava)
 kotlin.sourceSets.configureEach {
     languageSettings.optIn("org.kotools.types.internal.InternalKotoolsTypesApi")
 }
-
-// ------------------------------- Dependencies --------------------------------
 
 dependencies {
     commonMainImplementation(project(":types-internal"))
