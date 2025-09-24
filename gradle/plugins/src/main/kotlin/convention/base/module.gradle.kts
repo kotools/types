2c51334e1bae143cf8ab0f138cc57f63e9f1dddf
Loïc Lamarque <loiclamarque777@gmail.com>
2025-09-25 00:45:36 +0200

    👷 Update `convention.base.module` Gradle plugin

--- gradle/plugins/src/main/kotlin/convention/base/module.gradle.kts
+++ gradle/plugins/src/main/kotlin/convention/base/module.gradle.kts
@@ -14,14 +14,9 @@ tasks.named(BasePlugin.CLEAN_TASK_NAME).configure {
     group = TaskGroup.Module.toString()
 }
 
-tasks.named(BasePlugin.ASSEMBLE_TASK_NAME).configure {
-    group = TaskGroup.Module.toString()
-}
-
 private val coordinates: TaskProvider<Task> by tasks.registering
 coordinates.configure {
-    description =
-        "Prints the coordinates (group, artifact and version) of this project."
+    description = "Prints the coordinates (group, name and version)."
     group = TaskGroup.Module.toString()
     doLast { println("${project.group}:${project.name}:$version") }
 }
