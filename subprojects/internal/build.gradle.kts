ab2c182808e5fe446001ecde5e02dcda5ae2ab0a
Loïc Lamarque <loiclamarque777@gmail.com>
2025-09-24 20:39:17 +0200

    👷 Use `convention.documentation.module` plugin in `types-internal`

--- subprojects/internal/build.gradle.kts
+++ subprojects/internal/build.gradle.kts
@@ -1,19 +1,12 @@
-// ---------------------------------- Plugins ----------------------------------
-
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.dokka)
     `maven-publish`
     signing
     alias(libs.plugins.kotools.types.kotlin.multiplatform)
-    alias(libs.plugins.kotools.types.documentation)
+    alias(libs.plugins.convention.documentation.module)
     alias(libs.plugins.kotools.types.publication)
 }
 
-// ----------------------------- Plugin extensions -----------------------------
-
 documentation.excludeFromParentApiReference = true
-
-// ------------------------------- Dependencies --------------------------------
 
 dependencies.commonMainImplementation(libs.kotlinx.serialization.core)
