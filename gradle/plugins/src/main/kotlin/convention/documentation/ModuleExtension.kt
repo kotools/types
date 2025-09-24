d9037105a8cb596b72bfa13bc922c70ab23283cb
Loïc Lamarque <loiclamarque777@gmail.com>
2025-09-24 23:20:00 +0200

    🚚 Move `DocumentationModuleExtension` type to dedicated file

---
+++ gradle/plugins/src/main/kotlin/convention/documentation/ModuleExtension.kt
@@ -0,0 +1,26 @@
+package convention.documentation
+
+import org.gradle.api.provider.ListProperty
+import org.gradle.api.provider.Property
+import java.io.File
+
+/**
+ * The extension responsible for configuring the
+ * `convention.documentation.module` Gradle plugin.
+ */
+public interface DocumentationModuleExtension {
+    /**
+     * Flag for excluding this subproject from the API reference of the parent
+     * project. Set to `false` by default.
+     */
+    public val excludeFromParentApiReference: Property<Boolean>
+
+    /**
+     * Links to use for resolving this project's dependencies in the
+     * documentation.
+     */
+    public val externalLinks: ListProperty<String>
+
+    /** The file containing module and package documentation. */
+    public val packages: Property<File>
+}
--- gradle/plugins/src/main/kotlin/convention/documentation/module.gradle.kts
+++ gradle/plugins/src/main/kotlin/convention/documentation/module.gradle.kts
@@ -9,27 +9,6 @@ import org.jetbrains.dokka.gradle.DokkaTaskPartial
 
 // ----------------------------- Plugin extension ------------------------------
 
-/**
- * The extension responsible for configuring the
- * `convention.documentation.module` Gradle plugin.
- */
-public interface DocumentationModuleExtension {
-    /**
-     * Flag for excluding this subproject from the API reference of the parent
-     * project. Set to `false` by default.
-     */
-    public val excludeFromParentApiReference: Property<Boolean>
-
-    /**
-     * Links to use for resolving this project's dependencies in the
-     * documentation.
-     */
-    public val externalLinks: ListProperty<String>
-
-    /** The file containing module and package documentation. */
-    public val packages: Property<File>
-}
-
 private val extension: DocumentationModuleExtension =
     extensions.create("documentation")
 
