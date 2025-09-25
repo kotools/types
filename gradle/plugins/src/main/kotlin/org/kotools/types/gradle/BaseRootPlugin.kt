47b3886ac965fe315d8cb76b4d4a8f09e490b24a
Loïc Lamarque <loiclamarque777@gmail.com>
2025-09-25 20:02:51 +0200

    ♻️ Refactor `BaseRootPlugin` type

--- gradle/plugins/src/main/kotlin/org/kotools/types/gradle/BaseRootPlugin.kt
+++ gradle/plugins/src/main/kotlin/org/kotools/types/gradle/BaseRootPlugin.kt
@@ -3,7 +3,6 @@ package org.kotools.types.gradle
 import convention.base.TaskGroup
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.BasePlugin
 import org.gradle.api.tasks.diagnostics.TaskReportTask
 import org.gradle.kotlin.dsl.named
 
@@ -12,17 +11,17 @@ public class BaseRootPlugin : Plugin<Project> {
     /** Applies this plugin to the specified [project]. */
     override fun apply(project: Project) {
         project.repositories.mavenCentral()
-        project.pluginManager.withPlugin("base") {
-            project.tasks.named<TaskReportTask>("tasks")
-                .configure { this.displayGroup = TaskGroup.Root.toString() }
-            project.tasks.named(BasePlugin.CLEAN_TASK_NAME)
-                .configure { this.group = TaskGroup.Root.toString() }
-            project.tasks.named("check")
-                .configure { this.group = TaskGroup.Root.toString() }
-            project.tasks.named(BasePlugin.ASSEMBLE_TASK_NAME)
-                .configure { this.group = TaskGroup.Root.toString() }
-            project.tasks.named("build")
-                .configure { this.group = TaskGroup.Root.toString() }
-        }
+        project.tasks.named<TaskReportTask>("tasks")
+            .configure { this.displayGroup = TaskGroup.Root.toString() }
+        project.withBasePlugin()
+    }
+}
+
+private fun Project.withBasePlugin() {
+    val project: Project = this
+    project.pluginManager.withPlugin("base") {
+        project.tasks
+            .named { it in setOf("clean", "check", "assemble", "build") }
+            .configureEach { this.group = TaskGroup.Root.toString() }
     }
 }
