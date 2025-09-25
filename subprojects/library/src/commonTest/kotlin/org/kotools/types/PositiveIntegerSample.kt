1816dbe796bde12fe859678e6cdb3315a451f6aa
Loïc Lamarque <loiclamarque777@gmail.com>
2025-09-25 22:31:20 +0200

    ✨ Add `PositiveInteger.Companion.orNull(String)` method

---
+++ subprojects/library/src/commonTest/kotlin/org/kotools/types/PositiveIntegerSample.kt
@@ -0,0 +1,23 @@
+package org.kotools.types
+
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+@OptIn(ExperimentalKotoolsTypesApi::class)
+class PositiveIntegerSample {
+    // ------------------------------- Companion -------------------------------
+
+    @Test
+    fun orNullString() {
+        val text: String = Byte.MAX_VALUE.toString()
+        val integer: PositiveInteger? = PositiveInteger.orNull(text)
+        assertNotNull(integer)
+    }
+
+    @Test
+    fun orNullStringSigned() {
+        val text: String = '+' + Short.MAX_VALUE.toString()
+        val integer: PositiveInteger? = PositiveInteger.orNull(text)
+        assertNotNull(integer)
+    }
+}
