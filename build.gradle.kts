815b28cd3b68e7a704f7177940ff7c9437164e40
Loïc Lamarque <loiclamarque777@gmail.com>
2025-09-25 18:07:05 +0200

    👷 Use `org.kotools.types.base.root` Gradle plugin in root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.convention.base.root)
+    alias(libs.plugins.kotools.types.base.root)
     alias(libs.plugins.convention.documentation.root)
     alias(libs.plugins.convention.publication.root)
 }
