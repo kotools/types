7b0d0b5b1073dc3ea7e234e97f2e70e9975b36de
Loïc Lamarque <loiclamarque777@gmail.com>
2025-09-28 18:47:11 +0200

    🔥 Remove `PositiveInteger.Companion` type (#856)

--- subprojects/library/src/commonTest/kotlin/org/kotools/types/PositiveIntegerSample.kt
+++ subprojects/library/src/commonTest/kotlin/org/kotools/types/PositiveIntegerSample.kt
@@ -34,32 +34,4 @@ class PositiveIntegerSample {
             .toPositiveIntegerOrNull()
         assertNotNull(integer)
     }
-
-    // ------------------------------- Companion -------------------------------
-
-    @Test
-    fun orNullString() {
-        val text: String = Byte.MAX_VALUE.toString()
-        val integer: PositiveInteger? = PositiveInteger.orNull(text)
-        assertNotNull(integer)
-    }
-
-    @Test
-    fun orNullStringSigned() {
-        val text: String = '+' + Short.MAX_VALUE.toString()
-        val integer: PositiveInteger? = PositiveInteger.orNull(text)
-        assertNotNull(integer)
-    }
-
-    @Test
-    fun orThrowString() {
-        val text: String = Int.MAX_VALUE.toString()
-        PositiveInteger.orThrow(text)
-    }
-
-    @Test
-    fun orThrowStringSigned() {
-        val text: String = '+' + Int.MAX_VALUE.toString()
-        PositiveInteger.orThrow(text)
-    }
 }
