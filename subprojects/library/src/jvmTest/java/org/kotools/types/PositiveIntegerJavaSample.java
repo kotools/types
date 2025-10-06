2be9b52c903897815c01d0c14a83eb1ddd8a1679
Loïc Lamarque <loiclamarque777@gmail.com>
2025-10-06 12:09:45 +0200

    ✨ Override `PositiveInteger.toString()` method (#661)

--- subprojects/library/src/jvmTest/java/org/kotools/types/PositiveIntegerJavaSample.java
+++ subprojects/library/src/jvmTest/java/org/kotools/types/PositiveIntegerJavaSample.java
@@ -7,6 +7,17 @@ import org.kotools.types.internal.Warning;
 @SuppressWarnings(Warning.TEST_JAVA_CLASS_NAME)
 public class PositiveIntegerJavaSample {
     @Test
+    void toStringOverride() {
+        final String text = "123456789";
+        final PositiveInteger integer = PositiveInteger.of(text);
+        Assertions.assertNotNull(integer);
+        final String result = integer.toString();
+        Assertions.assertEquals(text, result);
+    }
+
+    // ------------------------------- Companion -------------------------------
+
+    @Test
     void of() {
         final PositiveInteger actual = PositiveInteger.of("123456789");
         Assertions.assertNotNull(actual);
