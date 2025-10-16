b9f7f2d152bf9b4dca10f09da284b5b2fd7da193
Loïc Lamarque <loiclamarque777@gmail.com>
2025-10-16 23:47:34 +0200

    ➕ Add `com.gradleup.nmcp.aggregation` Gradle plugin (#833)
    This plugin helps for publishing to Central Portal.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,16 @@ plugins {
     alias(libs.plugins.convention.base.root)
     alias(libs.plugins.convention.documentation.root)
     alias(libs.plugins.convention.publication.root)
+    alias(libs.plugins.maven.central.publishing)
+}
+
+nmcpAggregation {
+    centralPortal {
+        username = System.getenv("SONATYPE_PORTAL_USERNAME")
+        password = System.getenv("SONATYPE_PORTAL_PASSWORD")
+        publishingType = "USER_MANAGED"
+    }
+    publishAllProjectsProbablyBreakingProjectIsolation()
 }
 
 dependencies.dokkaHtmlMultiModulePlugin(libs.dokka.versioning)
