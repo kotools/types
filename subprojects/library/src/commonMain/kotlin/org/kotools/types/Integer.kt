7be8b679f090eaf0e7b6840c69349b4888e5a0cd
Loïc Lamarque <loiclamarque777@gmail.com>
2025-10-19 22:28:53 +0200

    🏷️ Add `Integer` type (#881)

---
+++ subprojects/library/src/commonMain/kotlin/org/kotools/types/Integer.kt
@@ -0,0 +1,12 @@
+package org.kotools.types
+
+/**
+ * Represents an integer.
+ *
+ * Contrarily to the Kotlin integer types ([Byte], [Short], [Int] and [Long]),
+ * this type has no maximum and minimum values. It can hold greater values than
+ * [Long.MAX_VALUE] and lesser values than [Long.MIN_VALUE].
+ */
+@ExperimentalKotoolsTypesApi
+// TODO: @ExperimentalSince(KotoolsTypesVersion.Unreleased)
+public interface Integer
