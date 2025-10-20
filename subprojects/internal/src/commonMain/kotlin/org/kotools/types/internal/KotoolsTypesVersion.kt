df4feacba836dbd0fc18779f4fbb06cfcd47973f
Loïc Lamarque <loiclamarque777@gmail.com>
2025-10-20 02:00:17 +0200

    🎨 Reorder entries of `KotoolsTypesVersion` enum

--- subprojects/internal/src/commonMain/kotlin/org/kotools/types/internal/KotoolsTypesVersion.kt
+++ subprojects/internal/src/commonMain/kotlin/org/kotools/types/internal/KotoolsTypesVersion.kt
@@ -5,33 +5,33 @@ package org.kotools.types.internal
 public enum class KotoolsTypesVersion {
     Unreleased,
 
-    /** Version 1.0.0. */
-    V1_1_0,
-
-    /** Version 3.0.0. */
-    V3_0_0,
-
-    /** Version 4.0.0. */
-    V4_0_0,
-
-    /** Version 4.1.0. */
-    V4_1_0,
-
-    /** Version 4.4.0. */
-    V4_4_0,
-
-    /** Version 4.5.1. */
-    V4_5_1,
-
-    /** Version 4.5.2. */
-    V4_5_2,
-
-    /** Version 4.5.3. */
-    V4_5_3,
+    /** Version 5.0.1. */
+    V5_0_1,
 
     /** Version 5.0.0. */
     V5_0_0,
 
-    /** Version 5.0.1. */
-    V5_0_1
+    /** Version 4.5.3. */
+    V4_5_3,
+
+    /** Version 4.5.2. */
+    V4_5_2,
+
+    /** Version 4.5.1. */
+    V4_5_1,
+
+    /** Version 4.4.0. */
+    V4_4_0,
+
+    /** Version 4.1.0. */
+    V4_1_0,
+
+    /** Version 4.0.0. */
+    V4_0_0,
+
+    /** Version 3.0.0. */
+    V3_0_0,
+
+    /** Version 1.0.0. */
+    V1_1_0
 }
