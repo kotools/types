9cbe7b0c3fdbdbfe374f807712b3c150558a1466
Loïc Lamarque <loiclamarque777@gmail.com>
2025-10-22 17:40:20 +0200

    ✅ Update tests of `Integer` type (#880)

---
+++ subprojects/library/src/commonTest/kotlin/org/kotools/types/numbers/IntegerTests.kt
@@ -0,0 +1,87 @@
+package org.kotools.types.numbers
+
+import org.kotools.types.ExperimentalKotoolsTypesApi
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+@OptIn(ExperimentalKotoolsTypesApi::class)
+class IntegerInstanceCreationTest {
+    @Test
+    fun withMaximumLong() {
+        // Given
+        val number: Long = Long.MAX_VALUE
+        // When
+        val actual = Integer(number)
+        // Then
+        assertEquals(expected = "$number", actual = "$actual")
+    }
+
+    @Test
+    fun withMinimumLong() {
+        // Given
+        val number: Long = Long.MIN_VALUE
+        // When
+        val actual = Integer(number)
+        // Then
+        assertEquals(expected = "$number", actual = "$actual")
+    }
+
+    @Test
+    fun withDecimalText() {
+        // Given
+        val text: String = Long.MAX_VALUE.toString()
+        // When
+        val actual = Integer(text)
+        // Then
+        assertEquals(expected = text, actual = "$actual")
+    }
+
+    @Test
+    fun withPlusSignedDecimalText() {
+        // Given
+        val number: Long = Long.MAX_VALUE
+        val text = "+$number"
+        // When
+        val actual = Integer(text)
+        // Then
+        assertEquals(expected = "$number", actual = "$actual")
+    }
+
+    @Test
+    fun withMinusSignedDecimalText() {
+        // Given
+        val text: String = Long.MIN_VALUE.toString()
+        // When
+        val actual = Integer(text)
+        // Then
+        assertEquals(expected = text, actual = "$actual")
+    }
+
+    @Test
+    fun withBlankText() {
+        // Given
+        val text = ""
+        // When
+        val actual: IllegalArgumentException = assertFailsWith {
+            Integer(text)
+        }
+        // Then
+        val expected = "Integer should not be blank"
+        assertEquals(expected, actual.message)
+    }
+
+    @Test
+    fun withNonDecimalText() {
+        // Given
+        val text = "hello"
+        // When
+        val actual: IllegalArgumentException = assertFailsWith {
+            Integer(text)
+        }
+        // Then
+        val expected = "Integer can only contain an optional + or - sign, " +
+                "followed by a sequence of digits, was: $text"
+        assertEquals(expected, actual.message)
+    }
+}
