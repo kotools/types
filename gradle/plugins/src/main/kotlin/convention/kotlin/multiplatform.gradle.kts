a4e1ba2e06f77ea44bef5137bece34518c2fddac
Loïc Lamarque <loiclamarque777@gmail.com>
2025-10-23 18:27:15 +0200

    📌 Set Java 17 compatibility with flags
    Removes the usage of JVM toolchains.

--- gradle/plugins/src/main/kotlin/convention/kotlin/multiplatform.gradle.kts
+++ gradle/plugins/src/main/kotlin/convention/kotlin/multiplatform.gradle.kts
@@ -1,5 +1,6 @@
 package convention.kotlin
 
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.gradle.plugin.KotlinJsCompilerType
@@ -7,6 +8,7 @@ import org.jetbrains.kotlin.gradle.targets.js.ir.KotlinJsIrLink
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 import org.jetbrains.kotlin.gradle.targets.jvm.tasks.KotlinJvmTest
 import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
+import kotlin.jvm.optionals.getOrNull
 
 plugins { id("org.jetbrains.kotlin.multiplatform") }
 
@@ -27,12 +29,27 @@ rootProject.extensions.findByType<YarnRootExtension>()?.run {
     resolution("webpack", "5.94.0")
 }
 
+private val javaVersion: Int = extensions.getByType<VersionCatalogsExtension>()
+    .named("libs")
+    .findVersion("java")
+    .getOrNull()
+    ?.requiredVersion
+    ?.toInt()
+    ?: error("Java version not found in version catalog")
 kotlin.jvm {
+    this.compilations.configureEach {
+        this.compilerOptions.configure {
+            this.jvmTarget = JvmTarget.fromTarget("$javaVersion")
+            this.freeCompilerArgs.add("-Xjdk-release=$javaVersion")
+        }
+    }
     testRuns.configureEach {
         executionTask.configure(KotlinJvmTest::useJUnitPlatform)
     }
 }
-kotlin.jvmToolchain(17)
+tasks.withType<JavaCompile>().configureEach {
+    this.options.release = javaVersion
+}
 
 // Inspired from https://kotlinlang.org/docs/native-target-support.html.
 // Tier 1
