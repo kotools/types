835a2e229214e37b3f0a0010f7bb7e6bc75cc339
Loïc Lamarque <loiclamarque777@gmail.com>
2025-10-25 14:06:20 +0200

    📝 Add Evolution ideas in documentation of `Integer` type (#880)

--- subprojects/library/src/commonMain/kotlin/org/kotools/types/Integer.kt
+++ subprojects/library/src/commonMain/kotlin/org/kotools/types/Integer.kt
@@ -42,6 +42,22 @@ import org.kotools.types.internal.Warning
  * [minus][Integer.minus] (`-`) and [times][Integer.times] (`*`).
  * - **Conversions:** [toString][Integer.toString].
  * </details>
+ *
+ * <br>
+ * <details>
+ * <summary>
+ *     <b>Evolution ideas</b>
+ * </summary>
+ *
+ * ### Evolution ideas
+ *
+ * See the following discussions for contributing to the evolution of this type:
+ *
+ * - [Extension properties for instance creation](https://github.com/kotools/types/discussions/887).
+ *
+ * If you have an idea that is not listed above, feel free to
+ * [open a discussion](https://github.com/kotools/types/discussions/new?category=ideas).
+ * </details>
  */
 @ExperimentalKotoolsTypesApi
 @ExperimentalSince(KotoolsTypesVersion.Unreleased)
