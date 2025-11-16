5e2ebac883c793a95072a31dca06258ef787eb9f
Loïc Lamarque <loiclamarque777@gmail.com>
2025-11-16 10:12:12 +0100

    👷 Update POM config in `convention.publication.module` plugin (#902)

--- gradle/plugins/src/main/kotlin/convention/publication/module.gradle.kts
+++ gradle/plugins/src/main/kotlin/convention/publication/module.gradle.kts
@@ -12,32 +12,28 @@ pluginManager.withPlugin("maven-publish") {
     val publications: NamedDomainObjectCollection<MavenPublication> =
         publishing.publications.withType()
     publications.configureEach {
-        pom {
-            name.set("Kotools Types")
-            description.set(
-                "Multiplatform library providing explicit types for Kotlin."
-            )
-            val gitRepository = "https://github.com/kotools/types"
-            url.set(gitRepository)
-            licenses {
-                license {
-                    name.set("MIT")
-                    url.set("https://opensource.org/licenses/MIT")
+        this.pom {
+            this.name = "Kotools Types"
+            this.description = "Explicit types for Kotlin Multiplatform."
+            this.url = "https://github.com/kotools/types"
+            this.licenses {
+                this.license {
+                    this.name = "MIT"
+                    this.url = "https://opensource.org/licenses/MIT"
                 }
             }
-            issueManagement {
-                system.set("GitHub")
-                url.set("$gitRepository/issues")
-            }
-            scm {
-                connection.set("$gitRepository.git")
-                url.set(gitRepository)
-            }
-            developers {
-                developer {
-                    name.set(System.getenv("GIT_USER"))
-                    email.set(System.getenv("GIT_EMAIL"))
+            this.developers {
+                this.developer {
+                    this.name = "Loïc Lamarque"
+                    this.email = "contact@kotools.org"
+                    this.organization = "Kotools"
+                    this.organizationUrl =
+                        this@pom.url.map { it.removeSuffix("/types") }
                 }
+            }
+            this.scm {
+                this.url = this@pom.url
+                this.connection = this.url.map { "${it}.git" }
             }
         }
     }
