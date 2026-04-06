7a82c514e0d7f18e6544baf2d39e53c9306ccd52
Loïc Lamarque <loiclamarque777@gmail.com>
2026-04-06 14:43:07 +0200

    📝 Update documentation of `Decimal` type (#925)

--- subprojects/library/src/commonTest/kotlin/org/kotools/types/DecimalSample.kt
+++ subprojects/library/src/commonTest/kotlin/org/kotools/types/DecimalSample.kt
@@ -8,6 +8,22 @@ class DecimalSample {
     @Suppress("SimplifyBooleanWithConstants")
     @Test
     fun representationProblem() {
-        check(1.1 + 2.2 == 3.3000000000000003) // instead of being exactly 3.3
+        check(1.1 + 2.2 == 3.3000000000000003) // instead of exactly 3.3
+    }
+
+    @Test
+    fun precisionProblem() {
+        val price = 0.1
+        val quantity = 3
+        val total: Double = price * quantity
+        check(total == 0.30000000000000004) // instead of exactly 0.3
+    }
+
+    @Suppress("DIVISION_BY_ZERO")
+    @Test
+    fun divisionByZeroProblem() {
+        val x: Double = 1.0 / 0.0 // passes instead of throwing exception
+        val y: Double = x + 10 // error silently propagates
+        check(y == Double.POSITIVE_INFINITY)
     }
 }
