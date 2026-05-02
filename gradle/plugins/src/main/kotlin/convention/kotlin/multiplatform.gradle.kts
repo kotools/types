aad679be70f9af784f26152a2265c7541c7e5edf
Loïc Lamarque <loiclamarque777@gmail.com>
2026-05-02 23:24:10 +0200

    🔥 Remove intermediate Kotlin source sets (#938)

--- gradle/plugins/src/main/kotlin/convention/kotlin/multiplatform.gradle.kts
+++ gradle/plugins/src/main/kotlin/convention/kotlin/multiplatform.gradle.kts
@@ -3,7 +3,6 @@ package convention.kotlin
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinJsCompilerType
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.targets.js.ir.KotlinJsIrLink
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension
 import org.jetbrains.kotlin.gradle.targets.jvm.tasks.KotlinJvmTest
@@ -55,24 +54,6 @@ kotlin.targets.configureEach {
 kotlin.applyDefaultHierarchyTemplate()
 kotlin.sourceSets {
     configureEach { languageSettings.optIn("kotlin.RequiresOptIn") }
-    val commonMain: KotlinSourceSet by this.getting
-    val commonTest: KotlinSourceSet by this.getting
-    val jsNativeMain: KotlinSourceSet by this.creating {
-        this.dependsOn(commonMain)
-    }
-    val jvmNativeTest: KotlinSourceSet by this.creating {
-        this.dependsOn(commonTest)
-    }
-    val jsMain: KotlinSourceSet by this.getting { this.dependsOn(jsNativeMain) }
-    val jvmTest: KotlinSourceSet by this.getting {
-        this.dependsOn(jvmNativeTest)
-    }
-    val nativeMain: KotlinSourceSet by this.getting {
-        this.dependsOn(jsNativeMain)
-    }
-    val nativeTest: KotlinSourceSet by this.getting {
-        this.dependsOn(jvmNativeTest)
-    }
 }
 
 // ----------------------------------- Tasks -----------------------------------
