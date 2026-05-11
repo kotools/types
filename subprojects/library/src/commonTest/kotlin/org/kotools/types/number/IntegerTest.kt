4c404248158e3fa05256f208b1076ee9c2e402f5
Loïc Lamarque <loiclamarque777@gmail.com>
2026-05-11 22:17:02 +0200

    ✅ Add test for `Integer` conversion to `String` (#941)

--- subprojects/library/src/commonTest/kotlin/org/kotools/types/number/IntegerTest.kt
+++ subprojects/library/src/commonTest/kotlin/org/kotools/types/number/IntegerTest.kt
@@ -4,6 +4,7 @@ import org.kotools.types.ExperimentalKotoolsTypesApi
 import kotlin.random.Random
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 
 @OptIn(ExperimentalKotoolsTypesApi::class)
 class IntegerTest {
@@ -20,4 +21,22 @@ class IntegerTest {
             val expected: String = value.toString()
             assertEquals(expected, actual)
         }
+
+    // ------------------------- Conversion to String --------------------------
+
+    @Test
+    fun stringRepresentationIsCanonical(): Unit = repeat(times = 1_000) {
+        val value: Long = Random.nextLong()
+        val integer: Integer = Integer.of(value)
+
+        val actual: String = integer.toString()
+
+        val expected: String = value.toString()
+        assertEquals(expected, actual)
+        val zero = 0L
+        if (value == zero) return@repeat assertEquals(expected = "0", actual)
+        if (value > zero) assertFalse("\"$actual\" must not have leading +.") {
+            actual.startsWith('+')
+        }
+    }
 }
